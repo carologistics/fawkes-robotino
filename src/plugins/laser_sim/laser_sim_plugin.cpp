666dc9bd44c7cdf33c61e89288594f1f2583b1d1
Daniel Ewert <daniel.ewert@ima-zlw-ifu.rwth-aachen.de>
2012-06-14 20:17:58 +0200

    lasersim: simple simulator for a laser
    allows to add artificial obstacles, currently only by editing the source code.
    could be usefull anyways

---
+++ src/plugins/laser_sim/laser_sim_plugin.cpp
@@ -0,0 +1,46 @@
+
+/***************************************************************************
+ *  lasersim_plugin.cpp - Empty example
+ *
+ *  Created: Do June 14th, 18:07:14 CEST 2012
+ *  Copyright  2012  Daniel Ewert
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <core/plugin.h>
+
+#include "laser_sim_thread.h"
+
+using namespace fawkes;
+
+/** Template! Makes the robotino move forward for 3 seconds
+ * @author Daniel Ewert
+ */
+class LaserSimPlugin : public fawkes::Plugin
+{
+ public:
+  /** Constructor.
+   * @param config Fawkes configuration
+   */
+  LaserSimPlugin(Configuration *config)
+    : Plugin(config)
+  {
+    thread_list.push_back(new LaserSim());
+  }
+};
+
+PLUGIN_DESCRIPTION("simple laser simulation")
+EXPORT_PLUGIN(LaserSimPlugin)
---
+++ src/plugins/laser_sim/laser_sim_thread.h
@@ -0,0 +1,74 @@
+/***************************************************************************
+ *  laser_cluster_detector_thread.h - reads laserdata and finds adjacent clusters
+ *
+ *  Created: Mi 23. Mai 17:44:14 CEST 2012
+ *  Copyright  2012 Daniel Ewert 
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#ifndef __PLUGINS_LASER_SIM_THREAD_H_
+#define __PLUGINS_LASER_SIM_THREAD_H_
+
+#include <core/threading/thread.h>
+#include <aspect/blocked_timing.h>
+#include <aspect/logging.h>
+#include <aspect/configurable.h>
+#include <aspect/blackboard.h>
+
+#include <vector>
+#include <utility>
+#include <cstdlib>
+
+namespace fawkes
+{
+class Laser360Interface;
+}
+
+class LaserSim:
+		public fawkes::Thread,
+		public fawkes::BlockedTimingAspect,
+		public fawkes::LoggingAspect,
+		public fawkes::ConfigurableAspect,
+		public fawkes::BlackBoardAspect
+{
+
+public:
+	LaserSim();
+
+	virtual void init();
+	virtual void loop();
+	virtual bool prepare_finalize_user();
+	virtual void finalize();
+
+	/** Stub to see name in backtrace for easier debugging. @see Thread::run() */
+protected:
+	virtual void run()
+	{
+		Thread::run();
+	}
+
+private:
+	void add_cluster_at(unsigned int angle, unsigned int size, float distance);
+	float add_noise(float base);
+
+private:
+	fawkes::Laser360Interface *laser_if_;
+
+	float laser_scan_[360];
+	unsigned int num_scans_;
+};
+
+#endif
