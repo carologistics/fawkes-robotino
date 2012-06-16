1ad05e49c8999d0520f38c97c24e33a420a64802
Victor Mataré <matare@lih.rwth-aachen.de>
2012-06-16 22:51:57 +0200

    fetch_puck: move sideways till puck is at 0°

--- src/lua/skills/robotino/fetch_puck.lua
+++ src/lua/skills/robotino/fetch_puck.lua
@@ -25,69 +25,81 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "fetch_puck"
 fsm                = SkillHSM:new{name=name, start="SEE_PUCK", debug=false}
-depends_skills     = { "grab_puck" }
+depends_skills     = { "grab_puck", "relgoto", "motor_move" }
 depends_interfaces = {
---	 {v = "omnipuck", type="Position3DInterface"},
---	 {v = "navigator", type="NavigatorInterface"}
+	 {v = "omnipuck", type="Position3DInterface", id="Omnipuck1"},
+	 {v = "pose", type="Position3DInterface", id="Pose"},
+	 {v = "navigator", type="NavigatorInterface", id="Navigator"}
 }
 
 documentation      = [==[Move to puck pickup position]==]
-local puck_loc
+local TIMEOUT = 5
+local ORI_OFFSET = 0.15
 
 -- Initialize as skill module
 skillenv.skill_module(...)
 
+function get_puck_loc()
+	if omnipuck:visibility_history() >= 5 then
+		fsm.vars.puck_loc = {
+			x = omnipuck.translation(0)
+			y = omnipuck.translation(1)
+		}
+		return true
+	end
+	return false
+end
+
 function no_puck()
-	-- TODO: stub
+	if os.time() - fsm.vars.start_time > TIMEOUT and not fsm.vars.puck_loc then
+		return true
+	end
 	return false
 end
 
 function puck()
-	-- TODO: stub
-	return true
-end
-
-function no_puck_in_front()
-	-- TODO: stub
-	return false
+	return get_puck_loc()
 end
 
 function puck_in_front()
-	-- TODO: stub
-	return true
-end
-
-function final_position_reached()
-	-- TODO: stub
-	-- return navigator:final()
-end
-
-function navigation_error()
-	-- TODO: stub
-	-- return not navigator:error_code() == 0
+	if get_puck_loc() and math.abs(math.atan2(fsm.vars.puck_loc.y, fsm.vars.puck_loc.x)) < ORI_OFFSET then
+		return true
+	end
+	return false
 end
 
 fsm:add_transitions{
 	closure={motor=motor},
 	{"SEE_PUCK", "FAILED", cond=no_puck, desc="No puck seen by OmniVision"},
 	{"SEE_PUCK", "APPROACH_PUCK", cond=puck},
-	{"APPROACH_PUCK", "ARRIVED", cond=final_position_reached},
+	{"APPROACH_PUCK", "ARRIVED", skill=relgoto, fail_to="SEE_PUCK"},
 	{"APPROACH_PUCK", "FAILED", cond=navigation_error, desc="Target not reachable"},
-	{"ARRIVED", "SEE_PUCK", cond=no_puck_in_front, desc="Puck gone after approach"},
+	{"ARRIVED", "SEE_PUCK", cond=no_puck, desc="Puck gone after approach"},
 	{"ARRIVED", "SKILL_GRAB_PUCK", cond=puck_in_front},
-	{"SKILL_GRAB_PUCK", "FINAL", skill=grab_puck, fail_to="SEE_PUCK"}
+	{"SKILL_GRAB_PUCK", "FINAL", skill=grab_puck, fail_to="FAILED"}
 }
 
 function SEE_PUCK:init()
-	-- TODO: stub
-	-- store relative puck coordinates
-	-- puck_loc.x = omnipuck:translation(0)
-	-- puck_loc.y = omnipuck:translation(1)
-	-- puck_loc.w = omnipuck:translation(3)
+	self.fsm.vars.start_time = os.time()
 end
 
 function APPROACH_PUCK:init()
-	-- TODO: stub
-	-- drive to puck using navigator
+	local ori = 2*math.acos(pose:rotation(3))
+	if math.abs(ori) < ORI_OFFSET
+		or math.abs(ori - math.pi) < ORI_OFFSET then
+		self.args.rel_y = self.fsm.puck_loc.y
+	elseif math.abs(ori - 0.5*math.pi) < ORI_OFFSET
+		or math.abs(ori - 1.5*math.pi) < ORI_OFFSET then
+		self.args.rel_x = self.fsm.puck_loc.x
+	end
 end
+
+function ARRIVED:init()
+	self.fsm.vars.start_time = os.time()
+end
+
+function SKILL_GRAB_PUCK:init()
+	self.args.puck_loc = self.fsm.vars.puck_loc
+end
+
 
