88a3a3e42b81ff7235627dc2b0b60101f60e92e5
Jens Wittmeyer <J.Wittmeyer@wzl.rwth-aachen.de>
2012-06-17 23:22:43 +0200

    changes for the new determin_signal skill

--- src/lua/skills/robotino/express_good_agent.lua
+++ src/lua/skills/robotino/express_good_agent.lua
@@ -24,11 +24,11 @@ module(..., agentenv.module_init)
 -- Crucial agent information
 name               = "express_good_agent"
 fsm                = AgentHSM:new{name=name, debug=true, start="INIT"}
-depends_skills     = {"goto","move_under_rfid","determine_signal_rock","deliver_puck","leave_area_with_puck","fetch_puck", "leave_area"}
+depends_skills     = {"goto","move_under_rfid","determine_signal","deliver_puck","leave_area_with_puck","fetch_puck", "leave_area","take_puck_to"} --TODO goto durch den skill von victor/sebastian ersetzten
 depends_interfaces = {
    { v="worldmodel", type="WorldModel"},
-   { v="switch_4_omni",type = "SwitchInterface" , id = "OmnivisionSwitch"}, --todo
-   { v="puck",type = "Position3DInterface", id = "OmniPuck1"},
+   { v="omnivisionSwitch",type = "SwitchInterface" , id = "omnivisionSwitch"}, --todo
+   { v="OmniPuck1",type = "Position3DInterface", id = "OmniPuck1"},
    { v="light",type ="RobotinoAmpelInterface" } 
   
 }
@@ -43,7 +43,7 @@ agentenv.agent_module(...)
 --local Skill = AgentSkillExecJumpState
 
 function exp_exist()
-	return puck:visibility_history() > 10	
+	return OmniPuck1:visibility_history() > 10	
 
 end
 
@@ -52,30 +52,15 @@ function m_exp_does_exist()
 	return worldmodel:getExpressMachine() ~=nil
 
 end
-function not_prossesing()
-
-	return not  light:state() ==light.YELLOW_GREEN  ---yellow_green() gibt true zurück wenn es processing ist sonst false 
-
+function no_change()
+	return light:state() == light.NO_CHANGE
 end
 function green()
-
-	if  light:state() ==light.GREEN then
-	
-		self.fsm.vars.greenchecked = self.fsm.vars.greenchecked +1
-	end
-
-
-	return ( light:state() ==light.GREEN and self.fsm.vars.greenchecked < 3)--true wenn NUR das grüne licht lechted
-
-end
-
-function finished()
 	return  light:state() ==light.GREEN
 end
 
-function green_to_often()
-	return self.fsm.vars.greenchecked >2
-end
+
+
 function not_outOfOrder()
 
 	return not  light:state() ==light.RED
@@ -91,37 +76,31 @@ end
 fsm:add_transitions{
    closure={},
 
-   {"INIT", "TURN_TO_EGC", cond = true, desc="initialized/restarted"},
-   {"TURN_TO_EGC","WAIT_PUCK", fail_to ="INIT" ,skills={{"goto"}}},
+   {"INIT", "MOVE_TO_EGC", cond = true, desc="initialized/restarted"},
+   {"MOVE_TO_EGC","WAIT_PUCK", fail_to ="INIT" ,skills={{"goto"}}},
    {"WAIT_PUCK","FETCH_PUCK", cond = exp_exist},
    {"FETCH_PUCK" , "M_EXP_EXIST" , fail_to  ="INIT" , skills = {{"fetch_puck"}} },
    {"M_EXP_EXIST","GOTO_M_EXP",cond = m_exp_does_exist},
-   {"GOTO_M_EXP","CHECK_OUT_OF_ORDER" , fail_to = "INIT" , skills = {{"goto"}} } ,
-   {"CHECK_OUT_OF_ORDER_SKILL", "CHECK_OUT_OF_ORDER", fail_to = "INIT", skills = {{"determine_signal_rock"}} ,
+   {"GOTO_M_EXP","CHECK_OUT_OF_ORDER_SKILL" , fail_to = "INIT" , skills = {{"take_puck_to"}} } ,
+   {"CHECK_OUT_OF_ORDER_SKILL", "CHECK_OUT_OF_ORDER", fail_to = "INIT", skills = {{"determine_signal"}} ,
    {"CHECK_OUT_OF_ORDER", "MOVE_UNDER_RFID", cond = not_outOfOrder },
    {"CHECK_OUT_OF_ORDER", "GOTO_M_EXP", cond = outOfOrder},
    {"MOVE_UNDER_RFID","CHECK_LIGHT", fail_to = "INIT" ,skills = {{"move_under_rfid"}} },
-   {"CHECK_LIGHT", "DECIDE" , fail_to = "INIT" ,skills = {{"determine_signal_rock" }} }, 
----evtl modifizieren für exp good um zeit zu sparen (wenn NUR gelb an ist dann ist sie automatisch am gelb blinken , da es keine möglichkeit gibt, dass beim exp good auf ein weiteres gewarted wird ) 
-   
-   {"DESICE" , "WAIT_TO_CHECK",cond = green},
-   {"DESICE" , "LEAVE_AREA" , cond = green_to_often},
-   {"LEAVE_AREA" , "INIT" , fail_to  = "FAILED" skills = {{ "leave_area"}} },
-   {"WAIT_TO_CHECK", "CHECK_LIGHT",  wait_sec = 0.5},
-   {"DECIDE" , "INIT", cond = not_prossesing },
-   {"DECIDE" , "RE_CHECK_LIGHT" wait_sec = 8},
-   {"RE_CHECK_LIGHT", "FINISHED" , fail_to = "INIT" ,skills = {{"determine_signal_rock" }} },
-   {"FINISHED" , "LEAVE_AREA_WITH_PUCK",cond = finished},
-   {"FINISHED" , "INIT" , cond =true },
+   {"CHECK_LIGHT", "DECIDE" , fail_to = "INIT" ,skills = {{"determine_signal" }} }, 
+   {"DECIDE","LEAVE_AREA_WITH_PUCK",cond = green},
+  -- {"DECIDE","",cond = yellow}, gibt es bei exp nicht (wird auch nicht ausgewerted)
+  -- {"DECIDE","",cond = red}, kann nicht zurück gegeben werden da ooo schon gescheckt wurde
+  --{"DECIDE","",cond = yellow_flashing}, unwarscheinlich da vorher gecheckt wird ob es die richtige machine ist (sonst weltbild fehlerhaft)
+   {"DECIDE","MOVE_TO_EGC",cond = no_change}, --kein puck dabei
    {"LEAVE_AREA_WITH_PUCK" , "GOTO_DEL" , fail_to = "INIT" , skills = {{ "leave_area_with_puck"}} },
-   {"GOTO_DEL" , "DELIVER",fail_to  = "INIT" , skills = {{"goto" }} },
+   {"GOTO_DEL" , "DELIVER",fail_to  = "INIT" , skills = {{"take_puck_to" }} },
    {"DELIVER" , "INIT" , fail_to = "INIT" , skills = {{"deliver_puck"}} }
 
 
 }
 function INIT:init()
 --init everything if needed
-self.fsm.args.greenchecked = 0
+self.fsm.vars.greenchecked = 0
 
 end
 
@@ -135,7 +114,7 @@ end
 
 function CHECK_OUT_OF_ORDER_SKILL:init()
 
-	self.skills[1].args = {kind = "OUTOFORDER"}
+	self.skills[1].args = {mode = "OUTOFORDER"}
 end
 
 function CHECK_OUT_OF_ORDER:init()
@@ -150,7 +129,7 @@ end
 
 function CHECK_LIGHT:init()
 
-	self.skills[1].args = { kind = "EXP"}
+	self.skills[1].args = { mode = "EXP"}
 
 
 function GOTO_DEL:init()
@@ -168,7 +147,7 @@ function GOTO_M_EXP:init()
 
 end
 
-function TURN_TO_PUCK:init() 
+function MOVE_TO_PUCK:init() 
 	self.skills[1].args = {goto_name = "EGI"}
 end
 
