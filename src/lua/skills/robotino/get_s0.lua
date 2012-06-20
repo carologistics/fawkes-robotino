0ad8a9ba1b933e74006a67c15fbd5c8d420bb978
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2012-06-20 12:24:18 -0500

    Skills: Added get_s0

---
+++ src/lua/skills/robotino/get_s0.lua
@@ -0,0 +1,50 @@
+
+----------------------------------------------------------------------------
+--  test_agent.lua
+--
+--  Created: Thu Aug 14 14:32:47 2008
+--  Copyright  2008  Tim Niemueller [www.niemueller.de]
+--
+----------------------------------------------------------------------------
+
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Library General Public License for more details.
+--
+--  Read the full text in the LICENSE.GPL file in the doc directory.
+
+-- Initialize module
+module(..., skillenv.module_init)
+
+-- Crucial skill information
+name               = "get_s0"
+fsm                = SkillHSM:new{name=name, start="GOTO_IS", debug=false}
+depends_skills     = {"goto","fetch_puck","leave_area_with_puck"}
+depends_interfaces = {
+	
+}
+
+documentation      = [==[test der bisherigen skills später eigener skill mit puck_aufname_location und puck_abgabe_location]==]
+--constants
+local start = "Is"
+-- Initialize as skill module
+skillenv.skill_module(...)
+
+
+fsm:add_transitions{
+	closure={motor=motor},
+	{"GOTO_IS", "SKILL_FETCH_PUCK", skill=goto, fail_to="FAILED"},
+	{"SKILL_FETCH_PUCK", "SKILL_LEAVE_AREA", skill=fetch_puck, fail_to="FAILED"},
+	{"SKILL_LEAVE_AREA", "FINAL", skill=leave_area_with_puck, fail_to="FAILED"},
+}
+
+function GOTO_IS:init()
+	self.fsm.args = {goto_name=start}
+end
+
