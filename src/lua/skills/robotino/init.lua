fb7ca5f716e0af4becae104f3443afbfb224e034
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2012-08-23 15:46:38 +0200

    skills: latest robotino skill space
    Found on robotino-ima-2.
    
    This concludes the behavior commits all based on the old BE. Time for
    Victor to convert the skills for our next tests.

--- src/lua/skills/robotino/init.lua
+++ src/lua/skills/robotino/init.lua
@@ -4,7 +4,6 @@
 --
 --  Created: Wed May 30 11:51:30 2012
 --  Copyright  2012  Tim Niemueller [www.niemueller.de]
---
 ----------------------------------------------------------------------------
 
 --  This program is free software; you can redistribute it and/or modify
@@ -24,24 +23,23 @@ module(..., fawkes.modinit.register_all);
 
 print("Initializing Lua skill space for Robotino")
 
--- Generic skills for all platforms
---skillenv.use_skill("skills.generic.relgoto")
---skillenv.use_skill("skills.generic.goto")
-
 -- Robotino specific skills
+skillenv.use_skill("skills.robotino.grab_puck")
 skillenv.use_skill("skills.robotino.motor_move")
 skillenv.use_skill("skills.robotino.relgoto")
 skillenv.use_skill("skills.robotino.fetch_puck")
 skillenv.use_skill("skills.robotino.goto")
 skillenv.use_skill("skills.robotino.take_puck_to")
-skillenv.use_skill("skills.robotino.leave_area")
-skillenv.use_skill("skills.robotino.leave_area_with_puck")
-skillenv.use_skill("skills.robotino.leave_IS")
-skillenv.use_skill("skills.robotino.get_s0")
 skillenv.use_skill("skills.robotino.move_under_rfid")
 skillenv.use_skill("skills.robotino.determine_signal")
 skillenv.use_skill("skills.robotino.deliver_puck")
+skillenv.use_skill("skills.robotino.finish_puck_at")
 skillenv.use_skill("skills.robotino.deposit_puck")
---skillenv.use_skill("skills.robotino.deliver")
+skillenv.use_skill("skills.robotino.leave_area_with_puck")
+skillenv.use_skill("skills.robotino.leave_IS")
+skillenv.use_skill("skills.robotino.get_s0")
+skillenv.use_skill("skills.robotino.leave_area")
+skillenv.use_skill("skills.robotino.move_to_next_deliver")
+skillenv.use_skill("skills.robotino.deliver")
 skillenv.use_skill("skills.robotino.finish_puck_at")
 
