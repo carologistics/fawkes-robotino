8b98f3241304e304733f0df19ca8cb1014ca9d2b
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2012-08-23 15:44:26 +0200

    skills: finish_puck_at latest version
    Found on robotin-ima-2

--- src/lua/skills/robotino/finish_puck_at.lua
+++ src/lua/skills/robotino/finish_puck_at.lua
@@ -25,8 +25,10 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "finish_puck_at"
 fsm                = SkillHSM:new{name=name, start="SKILL_TAKE_PUCK", debug=true}
-depends_skills     = { "take_puck_to", "determine_signal", "deposit_puck", "leave_area", "move_under_rfid" }
-depends_interfaces = { v="Pose", type="Position3DInterface", id="Pose" }
+depends_skills     = { "take_puck_to", "determine_signal", "deposit_puck", "leave_area", "move_under_rfid","motor_move" }
+depends_interfaces = {{ v="Pose", type="Position3DInterface", id="Pose" },
+		      { v="light", type="RobotinoAmpelInterface", id="light" },
+		}
 
 documentation      = [==[Go to target without losing teh puck]==]
 
@@ -45,16 +47,26 @@ function end_deliver()
 	return mpos.delivery_goto[fsm.vars.goto_name].d_skill == "deliver"
 end
 
+function is_ampel_yellow()
+	return light:state() == light.YELLOW
+end
+
+function is_not_yellow()
+	return not is_ampel_yellow()
+end
+
 fsm:add_transitions{
 	{ "SKILL_TAKE_PUCK", "DECIDE_ENDSKILL", skill=take_puck_to, fail_to="FAILED" },
 	{ "DECIDE_ENDSKILL", "SKILL_RFID", cond=end_rfid, desc="move under rfid" },
-	{ "DECIDE_ENDSKILL", "SKILL_DEPOSIT", cond=end_deposit },
---	{ "DECIDE_ENDSKILL", "SKILL_DELIVER", cond=end_deliver, desc="deliver" },
+	{ "DECIDE_ENDSKILL", "SKILL_DELIVER", cond=end_deliver, desc="deliver" },
 	{ "SKILL_RFID", "SKILL_DETERMINE_SIGNAL", skill=move_under_rfid, fail_to="FAILED" },
-	{ "SKILL_DETERMINE_SIGNAL", "SKILL_DEPOSIT", skill=determine_signal, fail_to="FAILED" },
+	{ "SKILL_DETERMINE_SIGNAL", "DECIDE", skill=determine_signal, fail_to="FAILED" },
+	{ "DECIDE", "SKILL_DEPOSIT", cond=is_ampel_yellow },
+	{ "DECIDE", "SKILL_DRIVE_LEFT", cond=is_not_yellow},
+	{ "SKILL_DRIVE_LEFT", "FINAL", skill=motor_move,fail_to="FAILED"},
 	{ "SKILL_DEPOSIT", "SKILL_LEAVE", skill=deposit_puck, fail_to="FAILED" },
-	{ "SKILL_LEAVE", "FINAL", skill=leave_area, fail_to="FAILED" }
---	{ "SKILL_DELIVER", "FINAL", skill=deliver, fail_to="FAILED" }
+	{ "SKILL_LEAVE", "FINAL", skill=leave_area, fail_to="FAILED" },
+	{ "SKILL_DELIVER", "FINAL", skill=deliver, fail_to="FAILED" }
 }
 
 function SKILL_TAKE_PUCK:init()
@@ -71,6 +83,9 @@ function SKILL_TAKE_PUCK:init()
 		end
 	end
 	self.args = { goto_name = self.fsm.vars.goto_name }
+end
+function SKILL_DRIVE_LEFT:init()
+	self.args = {x=0,y=-0.5,ori=0}
 end
 
 
