acdff508e21917c7f2b36ee904abbaa91e46cc47
Victor Mataré <matare@lih.rwth-aachen.de>
2013-01-14 18:03:57 +0100

    skills:get_s0: use ppgoto

--- src/lua/skills/robotino/get_s0.lua
+++ src/lua/skills/robotino/get_s0.lua
@@ -25,25 +25,25 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "get_s0"
 fsm                = SkillHSM:new{name=name, start="GOTO_IS", debug=false}
-depends_skills     = {"goto","fetch_puck","leave_IS"}
+depends_skills     = {"ppgoto","fetch_puck","leave_IS"}
 depends_interfaces = {
    
 }
 
 documentation      = [==[test der bisherigen skills später eigener skill mit puck_aufname_location und puck_abgabe_location]==]
 --constants
-local start = "Is"
+local start = "Ins"
 -- Initialize as skill module
 skillenv.skill_module(_M)
 
 fsm:define_states{ export_to=_M,
-   {"GOTO_IS", SkillJumpState, skills={{goto}}, final_to="SKILL_FETCH_PUCK", fail_to="FAILED"},
+   {"GOTO_IS", SkillJumpState, skills={{ppgoto}}, final_to="SKILL_FETCH_PUCK", fail_to="FAILED"},
    {"SKILL_FETCH_PUCK", SkillJumpState, skills={{fetch_puck}}, final_to="SKILL_LEAVE_AREA",
       fail_to="FAILED"},
    {"SKILL_LEAVE_AREA", SkillJumpState, skills={{leave_IS}}, final_to="FINAL", fail_to="FAILED"}
 }
 
 function GOTO_IS:init()
-   self.skills[1].goto_name = start
+   self.skills[1].place = start
 end
 
