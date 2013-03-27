a477d0d58f2e9f6276e3e33c340cb51777cbc11c
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2013-03-27 15:00:44 +0100

    Omnivision: Added Activator Plugin
    With this Plugin you can permantly enable/disable the Omnivision
    algorithm. (it just sends the needed message to trigger the alogrithm)

---
+++ src/plugins/omnivision_activator/omnivision_activator_plugin.cpp
@@ -0,0 +1,47 @@
+
+/***************************************************************************
+ *  plugin_template_plugin.cpp - Empty example
+ *
+ *  Created: Mi 23. Mai 18:07:14 CEST 2012
+ *  Copyright  2012  Daniel Ewert
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <core/plugin.h>
+
+#include "omnivision_activator_thread.h"
+#include <interfaces/SwitchInterface.h>
+
+using namespace fawkes;
+
+/** Activates the Omnivison
+ * @author Johannes Rothe
+ */
+class OmnivisionActivatorPlugin : public fawkes::Plugin
+{
+ public:
+  /** Constructor.
+   * @param config Fawkes configuration
+   */
+  OmnivisionActivatorPlugin(Configuration *config)
+    : Plugin(config)
+  {
+    thread_list.push_back(new OmnivisionActivatorThread());
+  }
+};
+
+PLUGIN_DESCRIPTION("Activates the Omnivision Thread")
+EXPORT_PLUGIN(OmnivisionActivatorPlugin)
