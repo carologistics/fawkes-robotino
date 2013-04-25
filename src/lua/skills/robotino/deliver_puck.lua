ece2065c7d86e9caca33ae7628d279a5ff3a7710
Victor Mataré <matare@lih.rwth-aachen.de>
2013-04-25 18:37:19 +0200

    deliver_puck: fix errors and behavior

--- src/lua/skills/robotino/deliver_puck.lua
+++ src/lua/skills/robotino/deliver_puck.lua
@@ -33,12 +33,14 @@ depends_interfaces = {
 
 documentation     = [==[delivers already fetched puck to specified location]==]
 -- Constants
-local THRESHOLD_DISTANCE = 0.07
+local THRESHOLD_DISTANCE = 0.075
 local DELIVERY_GATES = { "D2", "D1", "D3" }
-local ANGLES = { 0, 0.2*math.pi, -0.2*math.pi}
+local ANGLES = { 0, 0.17*math.pi, -0.34*math.pi}
 
 -- Initialize as skill module
 skillenv.skill_module(_M)
+
+local tfm = require('tf_module')
 
 function have_puck()
 local curDistance = sensor:distance(8)
@@ -53,14 +55,14 @@ function ampel_green()
 end
 
 fsm:define_states{ export_to=_M,
-   closure = {have_puck=have_puck, idx=fsm.vars.cur_gate_idx,
-      dg=DELIVERY_GATES, ampel_green=ampel_green},
+   closure = {have_puck=have_puck, dg=DELIVERY_GATES, ampel_green=ampel_green},
    {"CHECK_PUCK", JumpState},
    {"SKILL_DETERMINE_SIGNAL", SkillJumpState, skills={{watch_signal}},
       final_to="DECIDE_DELIVER", fail_to="FAILED"},
    {"DECIDE_DELIVER", JumpState},
    {"TURN_TO_NEXT", SkillJumpState, skills={{motor_move}}, final_to="SKILL_DETERMINE_SIGNAL",
       fail_to="FAILED"},
+   {"TAKE_PUCK_WAIT", JumpState},
    {"TAKE_PUCK_TO", SkillJumpState, skills={{take_puck_to}}, final_to="MOVE_UNDER_RFID", fail_to="FAILED"},
    {"MOVE_UNDER_RFID", SkillJumpState, skills={{move_under_rfid}}, final_to="LEAVE_AREA",
       fail_to="FAILED"},
@@ -71,10 +73,11 @@ fsm:define_states{ export_to=_M,
 fsm:add_transitions{
    {"CHECK_PUCK", "FAILED", cond="not have_puck()", desc="No puck seen by Infrared"},
    {"CHECK_PUCK", "SKILL_DETERMINE_SIGNAL", cond=have_puck},
-   {"DECIDE_DELIVER", "MOVE_UNDER_RFID", cond="ampel_green() and vars.cur_gate_idx == 2", desc="Gate 2 green"},
-   {"DECIDE_DELIVER", "TAKE_PUCK_TO", cond="ampel_green() and vars.cur_gate_idx ~= 2", desc="Gate 1 or 3 green"},
-   {"DECIDE_DELIVER", "TURN_TO_NEXT", cond="idx < #dg"},
-   {"DECIDE_DELIVER", "FAILED", cond="(not ampel_green()) and (idx >= #dg)"}
+   {"DECIDE_DELIVER", "MOVE_UNDER_RFID", cond="ampel_green() and vars.cur_gate_idx == 1", desc="Gate 2 green"},
+   {"DECIDE_DELIVER", "TAKE_PUCK_WAIT", cond="ampel_green() and vars.cur_gate_idx ~= 1", desc="Gate 1 or 3 green"},
+   {"TAKE_PUCK_WAIT", "TAKE_PUCK_TO", timeout=1},
+   {"DECIDE_DELIVER", "TURN_TO_NEXT", cond="vars.cur_gate_idx < #dg"},
+   {"DECIDE_DELIVER", "FAILED", cond="(not ampel_green()) and (vars.cur_gate_idx >= #dg)"}
 }
 
 function CHECK_PUCK:init()
@@ -83,7 +86,11 @@ end
 
 function TURN_TO_NEXT:init()
    self.fsm.vars.cur_gate_idx = self.fsm.vars.cur_gate_idx + 1
-   self.skills[1].ori = ANGLES[self.fsm.vars.cur_gate_idx]
+   --turn_bl = tfm.transform({x=0, y=0, ori=ANGLES[self.fsm.vars.cur_gate_idx]}, "/map", "/base_link")
+   turn_bl = {}
+   turn_bl.ori = ANGLES[self.fsm.vars.cur_gate_idx]
+   print(turn_bl.ori)
+   self.skills[1].ori = turn_bl.ori
 end
 
 function TAKE_PUCK_TO:init()
