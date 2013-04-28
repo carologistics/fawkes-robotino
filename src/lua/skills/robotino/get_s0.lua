19eeb453565b57c07a0c3adfab74ae6ffd2d46f7
Victor Mataré <matare@lih.rwth-aachen.de>
2013-04-28 10:02:23 +0200

    get_s0: wait, cancel ppgoto before fetch_puck

--- src/lua/skills/robotino/get_s0.lua
+++ src/lua/skills/robotino/get_s0.lua
@@ -27,7 +27,7 @@ name               = "get_s0"
 fsm                = SkillHSM:new{name=name, start="GOTO_IS", debug=false}
 depends_skills     = {"ppgoto","fetch_puck","leave_IS"}
 depends_interfaces = {
-   
+   {v = "ppnavi", type = "NavigatorInterface"}   
 }
 
 documentation      = [==[test der bisherigen skills später eigener skill mit puck_aufname_location und puck_abgabe_location]==]
@@ -37,11 +37,25 @@ local start = "Ins"
 skillenv.skill_module(_M)
 
 fsm:define_states{ export_to=_M,
-   {"GOTO_IS", SkillJumpState, skills={{ppgoto}}, final_to="SKILL_FETCH_PUCK", fail_to="FAILED"},
+   {"GOTO_IS", SkillJumpState, skills={{ppgoto}}, final_to="WAIT", fail_to="FAILED"},
+   {"WAIT", JumpState},
    {"SKILL_FETCH_PUCK", SkillJumpState, skills={{fetch_puck}}, final_to="SKILL_LEAVE_AREA",
       fail_to="FAILED"},
    {"SKILL_LEAVE_AREA", SkillJumpState, skills={{leave_IS}}, final_to="FINAL", fail_to="FAILED"}
 }
+
+fsm:add_transitions{
+   {"WAIT", "SKILL_FETCH_PUCK", timeout=3}
+}
+
+function SKILL_FETCH_PUCK:init()
+   ppnavi:msgq_enqueue_copy(ppnavi.StopMessage:new())
+end
+
+--fsm:add_transitions{
+--   {"GOTO_IS", "SKILL_FETCH_PUCK", timeout=1},
+--   {"SKILL_FETCH_PUCK", "SKILL_LEAVE_AREA", timeout=1}
+--}
 
 function GOTO_IS:init()
    self.skills[1].place = start
