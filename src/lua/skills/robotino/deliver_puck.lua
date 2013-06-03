186c9fab126003e48b9fe7fcc6539a008d228680
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2013-06-03 18:23:48 +0200

    deliver_puck: now uses the config
    for the front sensor distance

--- src/lua/skills/robotino/deliver_puck.lua
+++ src/lua/skills/robotino/deliver_puck.lua
@@ -34,15 +34,18 @@ depends_interfaces = {
 }
 
 documentation     = [==[delivers already fetched puck to specified location]==]
+
+-- Initialize as skill module
+skillenv.skill_module(_M)
+
 -- Constants
 local THRESHOLD_DISTANCE = 0.05
+-- you can find the config value in /cfg/host.yaml
+local THRESHOLD_DISTANCE = config:get_float("/skills/deliver_puck/front_sensor_dist")
 local DELIVERY_GATES = { "D1", "D2", "D3" }
 local MOVES = { {y=-0.35}, {y=-0.35}, {y=0.7} }
 local MAX_TRIES = 3
 local MAX_ORI_ERR = 0.15
-
--- Initialize as skill module
-skillenv.skill_module(_M)
 
 local tfm = require("tf_module")
 
