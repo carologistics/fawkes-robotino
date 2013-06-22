f368fe0ed791193c13ac64d2f44ba0d52f3d4de8
Tobias Neumann <tobias.neumann@alumni.fh-aachen.de>
2013-06-22 16:35:06 +0200

    get_s0: add move to right by fail of fetch puck
    if fetch_puck failes the robot moves right one time
    by a second fail the skill fails

--- src/lua/skills/robotino/get_s0.lua
+++ src/lua/skills/robotino/get_s0.lua
@@ -25,27 +25,33 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "get_s0"
 fsm                = SkillHSM:new{name=name, start="GOTO_IS", debug=false}
-depends_skills     = {"ppgoto","fetch_puck","leave_IS"}
+depends_skills     = {"ppgoto","fetch_puck","leave_IS", "motor_move"}
 depends_interfaces = {
-   {v = "ppnavi", type = "NavigatorInterface"}   
+   {v = "ppnavi", type = "NavigatorInterface"}
+   {v = "motor", type = "MotorInterface", id="Robotino" }
 }
 
 documentation      = [==[test der bisherigen skills später eigener skill mit puck_aufname_location und puck_abgabe_location]==]
 --constants
 local start = "Ins"
+local MOVE_RIGHT_DISTANCE = 0.5
+local MOVE_RIGHT_MAX_MOVEMENTS = 1
+local move_right_count = 0
 -- Initialize as skill module
 skillenv.skill_module(_M)
 
 fsm:define_states{ export_to=_M,
-   {"GOTO_IS", SkillJumpState, skills={{ppgoto}}, final_to="WAIT", fail_to="FAILED"},
-   {"WAIT", JumpState},
+   {"GOTO_IS", SkillJumpState, skills={{ppgoto}}, final_to="SKILL_FETCH_PUCK", fail_to="FAILED"},
    {"SKILL_FETCH_PUCK", SkillJumpState, skills={{fetch_puck}}, final_to="SKILL_LEAVE_AREA",
-      fail_to="FAILED"},
+      fail_to="CHECK_RETRY"},
+   {"CHECK_RETRY", JumpState},
+   {"MOVE_RIGHT", SkillJumpState, skills={{motor_move}}, final_to="SKILL_FETCH_PUCK", fail_to="FAILED"},
    {"SKILL_LEAVE_AREA", SkillJumpState, skills={{leave_IS}}, final_to="FINAL", fail_to="FAILED"}
 }
 
 fsm:add_transitions{
-   {"WAIT", "SKILL_FETCH_PUCK", timeout=3}
+   {"CHECK_RETRY", "MOVE_RIGHT", cond="move_right_count <= MOVE_RIGHT_MAX_MOVEMENTS"},
+   {"CHECK_RETRY", "FAILED", cond="move_right_count > MOVE_RIGHT_MAX_MOVEMENTS"}
 }
 
 function SKILL_FETCH_PUCK:init()
@@ -61,3 +67,10 @@ function GOTO_IS:init()
    self.skills[1].place = start
 end
 
+function MOVE_RIGHT:init()
+   self.skills[1].y = MOVE_RIGHT_DISTANCE
+end
+
+function CHECK_RETRY:init()
+   move_right_count = move_right_count + 1
+end
