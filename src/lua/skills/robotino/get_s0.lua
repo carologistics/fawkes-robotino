5f9c030a84c07a5a5f694c7c09ce56fc23c6d008
Tobias Neumann <tobias.neumann@alumni.fh-aachen.de>
2013-06-22 17:16:14 +0200

    get_s0: add global_motor_move

--- src/lua/skills/robotino/get_s0.lua
+++ src/lua/skills/robotino/get_s0.lua
@@ -25,7 +25,7 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "get_s0"
 fsm                = SkillHSM:new{name=name, start="GOTO_IS", debug=false}
-depends_skills     = {"ppgoto","fetch_puck","leave_IS", "motor_move"}
+depends_skills     = {"ppgoto", "fetch_puck", "leave_IS", "motor_move", "global_motor_move"}
 depends_interfaces = {
    {v = "ppnavi", type = "NavigatorInterface"},
    {v = "motor", type = "MotorInterface", id="Robotino" }
@@ -41,7 +41,8 @@ skillenv.skill_module(_M)
 
 fsm:define_states{ export_to=_M,
    closure = {MOVE_RIGHT_MAX_MOVEMENTS=MOVE_RIGHT_MAX_MOVEMENTS},
-   {"GOTO_IS", SkillJumpState, skills={{ppgoto}}, final_to="SKILL_FETCH_PUCK", fail_to="FAILED"},
+   {"GOTO_IS", SkillJumpState, skills={{ppgoto}}, final_to="SKILL_GLOBAL_MOTOR_MOVE", fail_to="FAILED"},
+   {"SKILL_GLOBAL_MOTOR_MOVE", SkillJumpState, skills={{global_motor_move}}, final_to="SKILL_FETCH_PUCK", fail_to="FAILED"},
    {"SKILL_FETCH_PUCK", SkillJumpState, skills={{fetch_puck}}, final_to="SKILL_LEAVE_AREA",
       fail_to="CHECK_RETRY"},
    {"CHECK_RETRY", JumpState},
@@ -66,6 +67,10 @@ end
 function GOTO_IS:init()
    self.fsm.vars.move_right_count = 0
    self.skills[1].place = start
+end
+
+function SKILL_GLOBAL_MOTOR_MOVE:init()
+   self.skills[1].place = self.fsm.vars.start
 end
 
 function SKILL_FETCH_PUCK:init()
