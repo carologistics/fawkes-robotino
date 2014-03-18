7f7bd068c78ad5c372b2bde4e2d8e822689e6005
Victor Mataré <matare@lih.rwth-aachen.de>
2014-03-18 01:17:35 +0100

    machine_signal: correctly update "best" signal iface

--- src/plugins/machine_signal/machine_signal_thread.cpp
+++ src/plugins/machine_signal/machine_signal_thread.cpp
@@ -487,9 +487,19 @@ void MachineSignalThread::loop()
   while (known_signals_.size() > MAX_SIGNALS)
     known_signals_.pop_back();
 
+  signal_state_t_ best_signal; // Goes into the backward compatible bb interface
+
   // Then sort geometrically
-  if (at_delivery) known_signals_.sort(sort_signal_states_by_x_);
-  else known_signals_.sort(sort_signal_states_by_area_);
+  if (at_delivery) {
+    // Ordering is critical here
+    best_signal = *(known_signals_.begin());
+    known_signals_.sort(sort_signal_states_by_x_);
+  }
+  else {
+    // Ordering is critical here
+    known_signals_.sort(sort_signal_states_by_area_);
+    best_signal = *(known_signals_.begin());
+  }
 
   // Update blackboard with the current information
   std::list<signal_state_t_>::iterator known_signal = known_signals_.begin();
@@ -504,12 +514,12 @@ void MachineSignalThread::loop()
 
     known_signal++;
   }
-  bb_signal_compat_->set_red(known_signals_.begin()->red);
-  bb_signal_compat_->set_yellow(known_signals_.begin()->yellow);
-  bb_signal_compat_->set_green(known_signals_.begin()->green);
+  bb_signal_compat_->set_red(best_signal.red);
+  bb_signal_compat_->set_yellow(best_signal.yellow);
+  bb_signal_compat_->set_green(best_signal.green);
   bb_signal_compat_->set_visibility_history(
-    known_signals_.begin()->unseen > 1 ? -1 : known_signals_.begin()->visibility);
-  bb_signal_compat_->set_ready(known_signals_.begin()->visibility >= (long int) buflen_/2);
+    known_signals_.begin()->unseen > 1 ? -1 : best_signal.visibility);
+  bb_signal_compat_->set_ready(best_signal.visibility >= (long int) buflen_/2);
   bb_signal_compat_->write();
 
   delete rois_R;
