7d38b93f0354ae3b8d2d021e92d0268a32b71eb2
Randolph Maassen <randolph.maassen@alumni.fh-aachen.de>
2014-06-16 20:02:01 +0200

    tag_vision: changed plugin to publish unified units in data (meters)

--- src/plugins/tag_vision/tag_vision_thread.cpp
+++ src/plugins/tag_vision/tag_vision_thread.cpp
@@ -24,6 +24,7 @@
 #include <interfaces/Position3DInterface.h>
 
 #include <opencv/cv.h>
+#include <math.h>
 
 #define CFG_PREFIX "/plugins/tag_vision/"
 #define IMAGE_CAHNNELS 3
@@ -281,7 +282,7 @@ void TagVisionThread::update_blackboard(size_t marker_count){
                 tag_interfaces[i]->set_translation(1,0);
                 tag_interfaces[i]->set_translation(2,0);
                 tag_interfaces[i]->set_translation(3,0);
-
+				tag_interfaces[i]->write();
             }
         }
         else{
@@ -290,32 +291,27 @@ void TagVisionThread::update_blackboard(size_t marker_count){
             if(tag_interfaces[i]==NULL){
                 create_tag_interface(i);
             }
-            //temporary the rotation quaternion in wxyz
-            double quat[4];
             //temp mat to get cv data
             CvMat mat;
             //angles in heading attitude bank
             double rot[3];
             //create the mat
-            cvInitMatHeader(&mat, 4, 1, CV_64F, quat);
+            cvInitMatHeader(&mat, 3, 1, CV_64F, rot);
             //get the quaternion of this pose in the mat
-            markers[i].pose.GetQuaternion(&mat);
+            markers[i].pose.GetEuler(&mat);
             //get the temporary quaternion in wxyz order to calculate angles
-            quat[1] = CV_MAT_ELEM(mat, double, 1, 0);
-            quat[2] = CV_MAT_ELEM(mat, double, 2, 0);
-            quat[3] = CV_MAT_ELEM(mat, double, 3, 0);
-            quat[0] = CV_MAT_ELEM(mat, double, 0, 0);
-                //calculate the angles from the quaternion
-            markers[i].pose.QuatToEul(quat,rot);
+            rot[0] = CV_MAT_ELEM(mat, double, 0, 0);
+            rot[1] = CV_MAT_ELEM(mat, double, 1, 0);
+            rot[2] = CV_MAT_ELEM(mat, double, 2, 0);
             //publish the angles
-            tag_interfaces[i]->set_rotation(ROT::X,rot[ROT::X]);
-            tag_interfaces[i]->set_rotation(ROT::Y,rot[ROT::Y]);
-            tag_interfaces[i]->set_rotation(ROT::Z,rot[ROT::Z]);
+            tag_interfaces[i]->set_rotation(ROT::X,rot[ROT::X]*M_PI/180);
+            tag_interfaces[i]->set_rotation(ROT::Y,rot[ROT::Y]*M_PI/180);
+            tag_interfaces[i]->set_rotation(ROT::Z,rot[ROT::Z]*M_PI/180);
             tag_interfaces[i]->set_rotation(ROT::W,0);
             //publish the translation
-            tag_interfaces[i]->set_translation(0,markers[i].pose.translation[0]);
-            tag_interfaces[i]->set_translation(1,markers[i].pose.translation[1]);
-            tag_interfaces[i]->set_translation(2,markers[i].pose.translation[2]);
+            tag_interfaces[i]->set_translation(1,markers[i].pose.translation[0]/1000);
+            tag_interfaces[i]->set_translation(2,markers[i].pose.translation[1]/1000);
+            tag_interfaces[i]->set_translation(0,markers[i].pose.translation[2]/1000);
 
             tag_interfaces[i]->set_frame(fv_cam_info.frame.c_str());
             tag_interfaces[i]->set_visibility_history(1);
