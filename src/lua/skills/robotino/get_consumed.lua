41d42a8290a31222414829d41493212eaab0b292
Matthias Loebach <matthias.loebach@ima-zlw-ifu.rwth-aachen.de>
2014-07-14 15:01:35 +0200

    get_consumed: added get_rid_of_puck to skill

--- src/lua/skills/robotino/get_consumed.lua
+++ src/lua/skills/robotino/get_consumed.lua
@@ -22,8 +22,8 @@ module(..., skillenv.module_init)
 
 -- Crucial skill information
 name               = "get_consumed"
-fsm                = SkillHSM:new{name=name, start="GOTO_MASCHINE", debug=false}
-depends_skills     = {"motor_move", "ppgoto", "global_motor_move", "fetch_puck"}
+fsm                = SkillHSM:new{name=name, start="GOTO_MACHINE", debug=false}
+depends_skills     = {"motor_move", "ppgoto", "global_motor_move", "fetch_puck", "get_rid_of_puck"}
 depends_interfaces = {
    {v = "ppnavi", type = "NavigatorInterface"},
    {v = "sensor", type="RobotinoSensorInterface"},
@@ -35,6 +35,20 @@ documentation      = [==[Drive to Maschine and ged a consumed puck]==]
 -- Initialize as skill module
 skillenv.skill_module(_M)
 graph = fawkes.load_yaml_navgraph("navgraph-llsf.yaml")
+-- Constants
+local LOSTPUCK_DIST = 0.08
+local PUCK_SENSOR_INDEX = 8
+if config:exists("/hardware/robotino/puck_sensor/trigger_dist") then
+   LOSTPUCK_DIST = config:get_float("/hardware/robotino/puck_sensor/trigger_dist")
+else
+   printf("NO CONFIG FOR /hardware/robotino/puck_sensor/trigger_dist FOUND! Using default value\n");
+end
+if config:exists("/hardware/robotino/puck_sensor/index") then
+   -- you can find the config value in /cfg/host.yaml
+   PUCK_SENSOR_INDEX = config:get_uint("/hardware/robotino/puck_sensor/index")
+else
+   printf("NO CONFIG FOR /hardware/robotino/puck_sensor/index FOUND! Using default value\n");
+end
 
 
 --fawkes.load_yaml_navgraph already searches in the cfg directory
@@ -49,17 +63,20 @@ function have_puck()
     return false
 end
 
-fsm:define_states{ export_to=_M,
-   {"GOTO_MASCHINE", SkillJumpState, skills={{ppgoto}}, final_to="ADJUST_POS", fail_to="FAILED"},
+fsm:define_states{ export_to=_M, closure={sensor = sensor, LOSTPUCK_DIST = LOSTPUCK_DIST, PUCK_SENSOR_INDEX = PUCK_SENSOR_INDEX},
+   {"GOTO_MACHINE", SkillJumpState, skills={{ppgoto}}, final_to="ADJUST_POS", fail_to="FAILED"},
    {"ADJUST_POS", SkillJumpState, skills={{global_motor_move}}, final_to="DRIVE_TO_SIDE", fail_to="FAILED"},
-   {"DRIVE_TO_SIDE", SkillJumpState, skills={{motor_move}}, final_to="GRAP_CONSUMED",
-      fail_to="FAILED"},
+   {"DRIVE_TO_SIDE", SkillJumpState, skills={{motor_move}}, final_to="GRAP_CONSUMED", fail_to="FAILED"},
    {"GRAP_CONSUMED", SkillJumpState, skills={{fetch_puck}}, final_to="LEAVE_TURN", fail_to="FAILED"},
-   {"LEAVE_TURN", SkillJumpState, skills={{motor_move}}, final_to="FINAL",
-      fail_to="FAILED"},
+   {"LEAVE_TURN", SkillJumpState, skills={{motor_move}}, final_to="FINAL", fail_to="FAILED"},
+   {"GET_RID_OF_PUCK", SkillJumpState, skills={{get_rid_of_puck}}, final_to="GOTO_MACHINE", fail_to="FAILED"}
 }
 
-function GOTO_MASCHINE:init()
+fsm:add_transitions{
+   {"GOTO_MACHINE", "GET_RID_OF_PUCK", cond = "sensor:distance(PUCK_SENSOR_INDEX) <= LOSTPUCK_DIST and sensor:distance(PUCK_SENSOR_INDEX) > 0", desc = "Picked up another puck while driving, escape"}
+}
+
+function GOTO_MACHINE:init()
    self.skills[1].place = graph:closest_node_to(self.fsm.vars.place, "highway_exit"):name()
 end
 
