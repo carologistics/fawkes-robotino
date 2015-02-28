7227e890b3f3c9d748d85a814cf88d867484529b
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2015-02-28 10:47:03 +0100

    ax12_gripper: add skill to use with the ax12_gripper
    The skill is based on the gripper.lua skill found in the
    fawkes-robotino-repository and lets the user interact with the
    ax12_gripper plugin by making use of the AX12GripperInterface.
    The combination of the interface and the skill lets the user close the
    gripper until a defined maximum load is given on each servo resulting in
    the particular servo stopping its motion.
    This lets the gripper try to grab a puck which isn't straight in front
    of the gripper but lays apart of it in a small offset.

---
+++ src/lua/skills/robotino/ax12gripper.lua
@@ -0,0 +1,140 @@
+
+----------------------------------------------------------------------------
+--  gripper.lua - Skill to open or close Robotino AX12 gripper
+--
+--  Created: Sat Feb 28 10:46:33 2015
+--  Copyright  2014  Sebastian Reuter
+--             2014  Tim Niemueller
+--             2015  Nicolas Limpert
+----------------------------------------------------------------------------
+
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Library General Public License for more details.
+--
+--  Read the full text in the LICENSE.GPL file in the doc directory.
+
+-- Initialize module
+module(..., skillenv.module_init)
+
+-- Crucial skill information
+name               = "ax12gripper"
+fsm                = SkillHSM:new{name=name, start="CHECK_WRITER", debug=true}
+depends_skills     = nil
+depends_interfaces = {
+   {v = "gripper_if", type = "AX12GripperInterface", id="Gripper AX12"}
+}
+
+documentation      = [==[Skill to open and close AX12 - gripper.
+]==]
+
+fsm.vars.right_fully_loaded = false
+fsm.vars.left_fully_loaded  = false
+
+-- Initialize as skill module
+skillenv.skill_module(_M)
+
+-- States
+fsm:define_states{
+   export_to=_M,
+   closure={gripper_if=gripper_if, right_fully_loaded=right_fully_loaded, left_fully_loaded=left_fully_loaded},
+   {"CHECK_WRITER", JumpState},
+   {"COMMAND", JumpState},
+   {"CLOSE_GRIPPER_WAIT", JumpState},
+   -- {"CLOSE_GRIPPER", JumpState},
+   {"STOP_GRIPPER_MOVEMENT", JumpState},
+   {"STOP_GRIPPER_LEFT", JumpState},
+   {"STOP_GRIPPER_RIGHT", JumpState},
+}
+
+function is_right_full_loaded()
+   fsm.vars.right_fully_loaded = gripper_if:right_load() > 150
+   print("load_right: " .. gripper_if:right_load())
+   return fsm.vars.right_fully_loaded
+end
+
+function is_left_full_loaded()
+   fsm.vars.left_fully_loaded = gripper_if:left_load() > 150
+   print("load_left: " .. gripper_if:left_load())
+   return fsm.vars.left_fully_loaded
+end
+
+-- Transitions
+fsm:add_transitions{
+   {"CHECK_WRITER", "FAILED", precond="not gripper_if:has_writer()", desc="No writer for gripper"},
+   {"CHECK_WRITER", "COMMAND", cond=true},
+   -- {"COMMAND", "FINAL", cond="not vars.error"},
+   {"COMMAND", "FAILED", cond="vars.error"},
+   {"COMMAND", "CLOSE_GRIPPER_WAIT", cond="vars.close_load"},
+   {"CLOSE_GRIPPER_WAIT", "STOP_GRIPPER_MOVEMENT", timeout=0.15},
+   {"STOP_GRIPPER_MOVEMENT", "STOP_GRIPPER_RIGHT", cond=is_right_full_loaded},
+   {"STOP_GRIPPER_MOVEMENT", "STOP_GRIPPER_LEFT", cond=is_left_full_loaded},
+   
+   {"STOP_GRIPPER_RIGHT", "FINAL", cond=is_left_full_loaded},
+   {"STOP_GRIPPER_LEFT", "FINAL", cond=is_right_full_loaded},
+   
+   -- {"STOP_GRIPPER_MOVEMENT", "FINAL", cond="vars.left_fully_loaded and vars.right_fully_loaded"},
+   {"STOP_GRIPPER_MOVEMENT", "FAILED", cond="vars.error"},
+}
+
+function CLOSE_GRIPPER_WAIT:init()
+   self.fsm.vars.right_fully_loaded = false
+   self.fsm.vars.left_fully_loaded = false
+   gripper_if:msgq_enqueue_copy(gripper_if.CloseLoadMessage:new())
+   -- print("right: " .. self.fsm.vars.right_fully_loaded .. " left: " .. self.fsm.vars.left_fully_loaded)
+end
+
+function COMMAND:init()
+   -- if self.fsm.vars.close then
+   --    theCloseMessage = gripper_if.CloseMessage:new()
+   --    theCloseMessage:set_offset(self.fsm.vars.offset)
+   --    gripper_if:msgq_enqueue_copy(theCloseMessage)
+   -- elseif self.fsm.vars.open then
+   if self.fsm.vars.open then
+--      gripper_if:msgq_enqueue_copy(gripper_if.OpenMessage:new())
+      theOpenMessage = gripper_if.OpenMessage:new()
+      theOpenMessage:set_offset(self.fsm.vars.offset)
+      gripper_if:msgq_enqueue_copy(theOpenMessage)
+   elseif self.fsm.vars.close then
+      print("close")
+      theCloseMessage = gripper_if.CloseMessage:new()
+      theCloseMessage:set_offset(self.fsm.vars.offset)
+      gripper_if:msgq_enqueue_copy(theCloseMessage)
+   elseif self.fsm.vars.close_load then
+      print("close load")
+   else
+      self.fsm:set_error("No known command")
+      self.fsm.vars.error = true
+   end
+end
+
+function STOP_GRIPPER_LEFT:init()
+   print("------- STOP LEFT --------")
+   gripper_if:msgq_enqueue_copy(gripper_if.StopLeftMessage:new())
+end
+
+function STOP_GRIPPER_LEFT:exit()
+   print("------- STOP LEFT -> RIGHT --------")
+   gripper_if:msgq_enqueue_copy(gripper_if.StopRightMessage:new())
+end
+
+function STOP_GRIPPER_RIGHT:init()
+   print("------- STOP RIGHT --------")
+   gripper_if:msgq_enqueue_copy(gripper_if.StopRightMessage:new())
+end
+
+function STOP_GRIPPER_RIGHT:exit()
+   print("------- STOP RIGHT -> RIGHT --------")
+   gripper_if:msgq_enqueue_copy(gripper_if.StopLeftMessage:new())
+end
+
+-- function STOP_GRIPPER_MOVEMENT:init()
+--    print("------- STOP --------")
+--    gripper_if:msgq_enqueue_copy(gripper_if.StopMessage:new())
+-- end
