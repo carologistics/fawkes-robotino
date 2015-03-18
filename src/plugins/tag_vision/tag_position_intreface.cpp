a5daee4ec686117818b9d9544c8ffe91d30e7cf5
Randolph Maassen <randolph.maassen@alumni.fh-aachen.de>
2015-03-18 15:53:53 +0100

    Tag Vision: Updated class about interface handling
    The tag position interface class has now basic interface handling about
    the visibility history and handling of the marker id.

--- src/plugins/tag_vision/tag_position_intreface.cpp
+++ src/plugins/tag_vision/tag_position_intreface.cpp
@@ -1,5 +1,45 @@
 #include "tag_position_intreface.h"
 
-TagPositionIntreface::TagPositionIntreface()
+TagPositionIntreface::TagPositionIntreface(fawkes::Position3DInterface *position_interface)
 {
+  this->interface_ = position_interface;
+  this->visibility_history_ = 0;
+  this->marker_id_ = 0;
+  this->touched_ = false;
+}
+
+TagPositionIntreface::~TagPositionIntreface()
+{
+
+}
+
+void TagPositionIntreface::set_position(alvar::Pose)
+{
+
+}
+
+void TagPositionIntreface::set_marker_id(u_int32_t new_id)
+{
+  // apply the new id
+  this->marker_id_ = new_id;
+  // reset the interface, the visibility history is 0
+  this->visibility_history_ = 0;
+}
+
+void TagPositionIntreface::write()
+{
+  // update the visibility history according to the marker, weather this interface got a new pose
+  if(this->touched_){
+    this->visibility_history_++;
+  }
+  else
+  {
+    this->visibility_history_--;
+  }
+  // set the new visibility history
+  this->interface_->set_visibility_history(this->visibility_history_);
+  // write out the interface
+  this->interface_->write();
+  // reset the update marker
+  this->touched_ = false;
 }
--- src/plugins/tag_vision/tag_position_intreface.h
+++ src/plugins/tag_vision/tag_position_intreface.h
@@ -1,10 +1,44 @@
 #ifndef TAG_POSITION_INTREFACE_H
 #define TAG_POSITION_INTREFACE_H
 
+#include <string>
+
+#include <interfaces/Position3DInterface.h>
+
+#include <alvar/Pose.h>
+
 class TagPositionIntreface
 {
 public:
-  TagPositionIntreface();
+  // constructor
+  TagPositionIntreface(fawkes::Position3DInterface *position_interface);
+  // destructor
+  ~TagPositionIntreface();
+
+  // update the position of the interface
+  void set_position(alvar::Pose);
+
+  // write the interface on the blackboard
+  void write();
+
+  // get the marker id
+  u_int32_t marker_id() {return this->marker_id_;}
+
+  // set the marker id
+  void set_marker_id(u_int32_t new_id);
+
+private:
+  // the interface to handle
+  fawkes::Position3DInterface *interface_;
+
+  // the visibility history for the interface to handle
+  int32_t visibility_history_;
+
+  // marker weather the interface was updated since last write
+  bool touched_;
+
+  // the id of the marker the interface represents
+  u_int32_t marker_id_;
 };
 
 #endif // TAG_POSITION_INTREFACE_H
