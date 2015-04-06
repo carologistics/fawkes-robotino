819ad42496848c56cce163bd3e373e79d5ecc63d
Tobias Neumann <tobias.neumann@alumni.fh-aachen.de>
2015-04-06 17:12:46 +0200

    skills: change drive_test depents on ppgoto_waypoints

--- src/lua/skills/robotino/drive_test.lua
+++ src/lua/skills/robotino/drive_test.lua
@@ -3,8 +3,8 @@
 --  drive_test.lua
 --
 --  Created: Sat Jun 14 15:13:19 2014
---  Copyright  2014  Frederik Zwilling
---             2014  Tobias Neumann
+--  Copyright  2014       Frederik Zwilling
+--             2014-2015  Tobias Neumann
 --
 ----------------------------------------------------------------------------
 
@@ -26,11 +26,8 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "drive_test"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=false}
-depends_skills     = {"ppgoto"}
-depends_interfaces = {
-   {v = "ppnavi", type = "NavigatorInterface"},
-   {v = "motor", type = "MotorInterface", id="Robotino"}
-}
+depends_skills     = {"ppgoto_waypoints"}
+depends_interfaces = { }
 
 documentation      = [==[Drives between the given list of navgraph-points
 
@@ -42,25 +39,17 @@ skillenv.skill_module(_M)
 
 fsm:define_states{ export_to=_M,
    {"INIT", JumpState},
-   {"GOTO", SkillJumpState, skills={{ppgoto}}, final_to="GOTO", fail_to="FAILED"},
+   {"GOTO", SkillJumpState, skills={{ppgoto_waypoints}}, final_to="GOTO", fail_to="FAILED"},
 }
 
 fsm:add_transitions{
-   {"INIT", "GOTO", timeout=1}
+   {"INIT", "GOTO", cond=true}
 }
 
 function INIT:init() 
-   fsm.vars.table_size = table.getn( self.fsm.vars.pps )             -- get list of targets
-   fsm.vars.table_pos  = 1
+
 end
 
 function GOTO:init()
-   if fsm.vars.table_pos > fsm.vars.table_size then                  -- if at end, start restart
-      fsm.vars.table_pos = 1
-   end
-   fsm.vars.next_target = self.fsm.vars.pps[fsm.vars.table_pos]      -- get next target
-   
-   fsm.vars.table_pos = fsm.vars.table_pos + 1                       -- increment list position
-
-   self.skills[1].place = fsm.vars.next_target
+   self.skills[1].wp = self.fsm.vars.pps
 end
