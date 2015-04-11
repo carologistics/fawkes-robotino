f372e4af0fad144205aa991886400fc396aa9d22
Randolph Maassen <randolph.maassen@alumni.fh-aachen.de>
2015-04-11 16:23:20 +0200

    skills: drive_tag added
    drive_tag drives closer to a tag and than alignes to it. for longer
    drives it uses relgoto to have colision detection. Call this skill with
    the id of the tag you want to drive to (eg. drive_tag{id=2})

---
+++ src/lua/skills/robotino/drive_tag.lua
@@ -0,0 +1,142 @@
+----------------------------------------------------------------------------
+--  drive_tag.lua - moves to a tag using colli for longer distance
+--
+--  Copyright  2015 The Carologistics Team
+----------------------------------------------------------------------------
+
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Library General Public License for more details.
+--
+--  Read the full text in the LICENSE.GPL file in the doc directory.
+
+
+-- Initialize module
+module(..., skillenv.module_init)
+
+-- Crucial skill information
+name               = "drive_tag"
+fsm                = SkillHSM:new{name=name, start="INIT"}
+depends_skills     = {"relgoto", "align_tag"}
+depends_interfaces = {
+   {v = "tag_0", type = "Position3DInterface"},
+   {v = "tag_1", type = "Position3DInterface"},
+   {v = "tag_2", type = "Position3DInterface"},
+   {v = "tag_3", type = "Position3DInterface"},
+   {v = "tag_4", type = "Position3DInterface"},
+   {v = "tag_5", type = "Position3DInterface"},
+   {v = "tag_6", type = "Position3DInterface"},
+   {v = "tag_7", type = "Position3DInterface"},
+   {v = "tag_8", type = "Position3DInterface"},
+   {v = "tag_9", type = "Position3DInterface"},
+   {v = "tag_10", type = "Position3DInterface"},
+   {v = "tag_11", type = "Position3DInterface"},
+   {v = "tag_12", type = "Position3DInterface"},
+   {v = "tag_13", type = "Position3DInterface"},
+   {v = "tag_14", type = "Position3DInterface"},
+   {v = "tag_15", type = "Position3DInterface"},
+   {v = "tag_info", type = "TagVisionInterface", id="/tag-vision/info"},
+}
+
+documentation      = [==[Drives the robot that the tag with the given id. Uses relgoto for collision detection on longer distances
+@param id The id of the tag to drive to
+]==]
+
+-- Initialize as skill module
+skillenv.skill_module(_M)
+
+-- Constant
+-- use drive when father than this distnance in m
+local drive_bound = 1
+-- align tag with these parameters
+local align_tag_param = { x = 0.4, y = 0, ori = 0 }
+
+function printtable(table)
+   for k,v in pairs(table) do
+      print(k .. "\t\t= " .. v)
+      if(k == "ori") then
+         print("degree\t= " .. v*180/math.pi)
+      end
+   end
+end
+
+function get_tag_with_id(wanted_id)
+   local i=1
+   -- get the correct id
+   for j=0,15 do
+      id = tag_info:tag_id(j)
+      -- stop when id is found
+      if id == wanted_id then
+         break
+      end
+      i = i+1
+   end
+   local tags = { tag_0, tag_1, tag_2, tag_3, tag_4, tag_5, tag_6, tag_7, tag_8, tag_9, tag_10, tag_11, tag_12, tag_13, tag_14, tag_15 }
+   return tags[i]
+end
+
+function get_tag_distance(tag)
+   -- in the camera frame of reference the forward distance is z(2) and the lateral distance is x(0)
+   return math.sqrt((tag:translation(2)*tag:translation(2)) + (tag:translation(0)*tag:translation(0)))
+end
+
+function id_not_found(self)
+   local found = false
+   for i=1,15 do
+      id=tag_info:tag_id(i)
+      if id == wanted_id then
+         found = true
+         break
+      end
+   end
+   return found
+end
+
+function tag_distance_long(self)
+   tag = get_tag_with_id(self.fsm.vars.id)
+   distance = get_tag_distance(tag)
+   print("distance: " .. distance)
+   return distance > drive_bound
+end
+
+function tag_distance_short(self)
+   return not tag_distance_long(self)
+end
+
+fsm:define_states { export_to=_M,
+   {"INIT", JumpState},
+   {"DRIVE", SkillJumpState, skills={{relgoto}}, final_to="FINAL", fail_to="FAILED"},
+   {"ALIGN", SkillJumpState, skills={{align_tag}}, final_to="FINAL", fail_to="FAILED"},
+}
+
+fsm:add_transitions {
+   {"INIT", "FAILED", cond=id_not_found, desc="Tag with the given id not found"},
+   {"INIT", "DRIVE", cond=tag_distance_long, desc="Far away tag, use collision detection drive"},
+   {"INIT", "ALIGN", cond=tag_distance_short, desc="Near tag, align"},
+}
+
+function INIT:init()
+   self.fsm.vars.id = self.fsm.vars.id or 0
+end
+
+function DRIVE:init()
+   -- relgoto
+   tag = get_tag_with_id(self.fsm.vars.id)
+   print("x: " .. tag:translation(2) - align_tag_param.x)
+   print("y: " .. tag:translation(0) - align_tag_param.y)
+   self.skills[1].x = tag:translation(2) - align_tag_param.x
+   self.skills[1].y = tag:translation(0) - align_tag_param.y
+end
+
+function ALIGN:init()
+   self.skills[1].x = align_tag_param.x
+   self.skills[1].y = align_tag_param.y
+   self.skills[1].ori = align_tag_param.ori
+
+end
