b3c8ee5f8c2e82c1ade75aea89b1d0194746dd8d
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2015-04-11 19:25:49 +0200

    new skill: move_to_conveyor
    If the robot stands in front of the MPS, aligns
    for the tag, then approaches the MPS and either
    puts or picks the puck
    
    Parameter:
    put_puck=true to put a puck on the conveyor
    pick_puck=true to pick a puck from the conveyor

---
+++ src/lua/skills/robotino/move_to_conveyor.lua
@@ -0,0 +1,89 @@
+
+----------------------------------------------------------------------------
+--  move_to_conveyor.lua
+--
+--  Created Sat Apr 11
+--  Copyright  2015  Johannes Rothe
+--
+----------------------------------------------------------------------------
+
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Library General Public License for more details.
+--
+--  Read the full text in the LICENSE.GPL file in the doc directory.
+
+-- Initialize module
+module(..., skillenv.module_init)
+
+-- Crucial skill information
+name               = "move_to_conveyor"
+fsm                = SkillHSM:new{name=name, start="SKILL_ALIGN_TAG", debug=true}
+depends_skills     = {"motor_move", "align_tag", "ax12gripper"}
+depends_interfaces = {}
+
+documentation      = [==[Align the robot and then moves to the conveyor
+@param pick_puck True if you want to pick a puck from the conveyor
+@param put_puck  True if you want to put a puck on the conveyor
+]==]
+
+
+-- Initialize as skill module
+skillenv.skill_module(_M)
+
+local TAG_OFFSET_Y = 0.025 --TODO if the tags are misaligned you there should be an parametric offset or another solution
+local ALIGN_DISTANCE = 0.35
+
+fsm:define_states{ export_to=_M, closure={},
+   {"SKILL_ALIGN_TAG", SkillJumpState, skills={{align_tag}},
+      final_to="DECIDE_OPEN", fail_to="FAILED"},
+   {"DECIDE_OPEN", JumpState},
+   --TODO align by laserlines and (if implemented) the conveyor detection
+   {"OPEN_GRIPPER", SkillJumpState, skills={{ax12gripper}},
+      final_to="DRIVE_FORWARD", fail_to="FAILED"},
+   {"DRIVE_FORWARD", SkillJumpState, skills={{motor_move}},
+      final_to="GRIPPER", fail_to="FAILED"},
+   {"GRIPPER", SkillJumpState, skills={{ax12gripper}},
+      final_to="TIMEOUT", fail_to="FAILED"},
+   {"TIMEOUT", JumpState},
+   {"MOVE_BACK", SkillJumpState, skills={{motor_move}},
+      final_to="FINAL", fail_to="FAILED"},
+}
+
+fsm:add_transitions{
+   {"DECIDE_OPEN", "DRIVE_FORWARD", cond="fsm.vars.put_puck", desc="Has a puck in front"},
+   {"DECIDE_OPEN", "OPEN_GRIPPER", cond="fsm.vars.pick_puck", desc="Open gripper before driving"},
+   {"TIMEOUT", "MOVE_BACK", timeout=1}
+}
+
+function SKILL_ALIGN_TAG:init()
+   self.skills[1].x = ALIGN_DISTANCE
+   self.skills[1].y = TAG_OFFSET_Y
+   self.skills[1].ori = 0
+end
+
+function DRIVE_FORWARD:init()
+   self.skills[1].x = 0.15
+end
+
+function OPEN_GRIPPER:init()
+   self.skills[1].open = true
+end
+
+function GRIPPER:init()
+   if self.fsm.vars.pick_puck then
+      self.skills[1].close = true
+   elseif self.fsm.vars.put_puck then
+      self.skills[1].open = true
+   end
+end
+
+function MOVE_BACK:init()
+   self.skills[1].x = -0.15
+end
