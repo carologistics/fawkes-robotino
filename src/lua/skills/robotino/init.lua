8d3209a846f2a16305428a0748ccee2ca4ba0505
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2015-04-16 17:47:18 +0200

    init.lua: add align_tag

--- src/lua/skills/robotino/init.lua
+++ src/lua/skills/robotino/init.lua
@@ -54,6 +54,7 @@ skillenv.use_skill("skills.robotino.get_produced")
 skillenv.use_skill("skills.robotino.drive_test")
 skillenv.use_skill("skills.robotino.store_puck")
 skillenv.use_skill("skills.robotino.get_stored_puck")
+skillenv.use_skill("skills.robotino.align_tag")
 
 -- Uncomment to use gripper for technical challenge
 --skillenv.use_skill("skills.robotino.gripper")
