4ba6c2dbed0f36dc0a2ff37a8018c0aa1b2d66c3
Randolph Maassen <randolph.maassen@alumni.fh-aachen.de>
2015-04-18 21:26:39 +0200

    drive_tag: providing good values for relgoto

--- src/lua/skills/robotino/drive_tag.lua
+++ src/lua/skills/robotino/drive_tag.lua
@@ -53,7 +53,7 @@ skillenv.skill_module(_M)
 
 -- Constant
 -- use drive when father than this distnance in m
-local drive_bound = 1
+local drive_bound = 0.7
 -- align tag with these parameters
 local align_tag_param = { x = 0.4, y = 0, ori = 0 }
 
@@ -101,7 +101,7 @@ end
 function tag_distance_long(self)
    tag = get_tag_with_id(self.fsm.vars.id)
    distance = get_tag_distance(tag)
-   print("distance: " .. distance)
+   --print("distance: " .. distance)
    return distance > drive_bound
 end
 
@@ -111,13 +111,14 @@ end
 
 fsm:define_states { export_to=_M,
    {"INIT", JumpState},
-   {"DRIVE", SkillJumpState, skills={{relgoto}}, final_to="FINAL", fail_to="FAILED"},
+   {"DRIVE", SkillJumpState, skills={{relgoto}}, final_to="ALIGN", fail_to="FAILED"},
    {"ALIGN", SkillJumpState, skills={{align_tag}}, final_to="FINAL", fail_to="FAILED"},
 }
 
 fsm:add_transitions {
    {"INIT", "FAILED", cond=id_not_found, desc="Tag with the given id not found"},
    {"INIT", "DRIVE", cond=tag_distance_long, desc="Far away tag, use collision detection drive"},
+   --{"DRIVE", "ALIGN", cond=tag_distance_short, desc="Close enough to the tag, aligning is enough"},
    {"INIT", "ALIGN", cond=tag_distance_short, desc="Near tag, align"},
 }
 
@@ -125,18 +126,41 @@ function INIT:init()
    self.fsm.vars.id = self.fsm.vars.id or 0
 end
 
+function get_yaw(qx, qy, qz, qw)
+   return math.atan2(2*qy*qw-2*qx*qz , 1 - 2*qy*qy - 2*qz*qz)
+end
+
+
 function DRIVE:init()
    -- relgoto
    tag = get_tag_with_id(self.fsm.vars.id)
-   print("x: " .. tag:translation(2) - align_tag_param.x)
-   print("y: " .. tag:translation(0) - align_tag_param.y)
-   self.skills[1].x = tag:translation(2) - align_tag_param.x
-   self.skills[1].y = tag:translation(0) - align_tag_param.y
+   local yaw = get_yaw(tag:rotation(0), tag:rotation(1), tag:rotation(2), tag:rotation(3))
+   -- correct rotation
+   yaw = yaw - (math.pi/2)
+   -- the forward distance is the z trnaslation in the camera frame of reference
+   forward_distance = tag:translation(2)
+   -- the lateral distance is the x translation in the camera frame of reference
+   lateral_distance = tag:translation(0)
+   --get the distance to drive
+   distance = { x = forward_distance ,--- self.fsm.vars.x,
+               y = -lateral_distance ,--- self.fsm.vars.y,
+               ori = -yaw}
+   --print("original distance")
+   --printtable(distance)
+   distance.x = distance.x - ((align_tag_param.x+0.3) * math.cos(distance.ori) + align_tag_param.y * (-1 * math.sin(distance.ori)))
+   distance.y = distance.y - ((align_tag_param.x+0.3) * math.sin(distance.ori) + align_tag_param.y * math.cos(distance.ori))
+
+   print("x: " .. distance.x)
+   print("y: " .. distance.y)
+   self.skills[1].x = distance.x
+   self.skills[1].y = distance.y
+   self.skills[1].ori = distance.ori
 end
 
 function ALIGN:init()
    self.skills[1].x = align_tag_param.x
    self.skills[1].y = align_tag_param.y
    self.skills[1].ori = align_tag_param.ori
+   self.skills[1].tag_id = self.fsm.vars.id
 
 end
