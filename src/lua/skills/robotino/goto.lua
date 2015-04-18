c67358fb2894affc3b868bf8cc06f1a5e17412ca
Tobias Neumann <tobias.neumann@alumni.fh-aachen.de>
2015-04-18 16:05:41 +0200

    skills: changed semantic of goto
    the skill goto just calls a relgoto with transformed global coordinates
    changes:
    - checkes are removed (this is the job of the local-planer)
    - if a place is given ppgoto will not be called. Instead the x, y and ori
      is extracted and relgoto is called
    
    also keep the old goto as goto-old.lua (without named in the init.lua)

--- src/lua/skills/robotino/goto.lua
+++ src/lua/skills/robotino/goto.lua
@@ -4,6 +4,7 @@
 --
 --  Created: Thu Aug 14 14:32:47 2008
 --  modified by Victor Mataré
+--              2015  Tobias Neumann
 --
 ----------------------------------------------------------------------------
 
@@ -24,101 +25,81 @@ module(..., skillenv.module_init)
 
 -- Crucial skill information
 name               = "goto"
-fsm                = SkillHSM:new{name=name, start="CHECK_PARAMS"}
-depends_skills     = { "relgoto", "ppgoto" }
+fsm                = SkillHSM:new{name=name, start="INIT"}
+depends_skills     = { "relgoto" }
 depends_interfaces = {
    {v = "pose", type="Position3DInterface", id="Pose"}
 }
 
-documentation      = [==[Move to a known (named) location.
-@param goto_name Name of the place we want to go to.
-@
+documentation      = [==[Move to a known location via place or x, y, ori.
+if place is set, this will be used and x, y and ori will be ignored
 
-Available names:
-================
-M1-M10 = m1-m10:              Machines 1-10,
-R1, R2:                       Recycling gates 1 and 2,
-T = TEST = Test:              Test station,
-EGI = ExpressGoodInsertion:   Express good insertion area,
-D1-D3 = Delivery1-Delivery3:  Delivery gates 1-3.
+@param place  Name of the place we want to go to.
+@param x      x we want to drive to
+@param y      y we want to drive to
+@param ori    ori we want to drive to
+
 ]==]
 
-local MAX_TRANSERR = 0.16
-local MAX_ROTERR = 0.16
-local MAX_POS_MISS = 4
 -- Initialize as skill module
 skillenv.skill_module(_M)
 
 local tf_mod = require 'tf_module'
 
-
-function pose_ok(self)
-   return (math.abs(self.fsm.vars.x - pose:translation(0)) <= MAX_TRANSERR
-    and math.abs(self.fsm.vars.y - pose:translation(1)) <= MAX_TRANSERR
-    and math.abs(math.abs(self.fsm.vars.ori) - 2*math.acos(pose:rotation(3))) <= MAX_ROTERR)
-end
-
-function missed_too_often(self)
-   return (self.fsm.vars.num_pos_missed >= MAX_POS_MISS)
+function check_navgraph(self)
+  return self.fsm.vars.place ~= nil and not navgraph
 end
 
 fsm:define_states{ export_to=_M,
-   closure={missed_too_often = missed_too_often,
-      pose_ok = pose_ok},
-   {"CHECK_PARAMS", JumpState},
-   {"DO_RELGOTO", SkillJumpState, skills={{relgoto}}, final_to="WAIT_POSE", fail_to="FAILED"},
-   {"DO_PPGOTO", SkillJumpState, skills={{ppgoto}}, final_to="WAIT_POSE", fail_to="FAILED"},
-   {"WAIT_POSE", JumpState},
-   {"CHECK_POSE", JumpState},
-   {"MISSED", JumpState}
+  closure={check_navgraph=check_navgraph},
+  {"INIT",          JumpState},
+  {"SKILL_RELGOTO", SkillJumpState, skills={{relgoto}}, final_to="FINAL", fail_to="FAILED"},
 }
-
 
 fsm:add_transitions{
-   {"CHECK_PARAMS", "DO_RELGOTO", cond="not vars.place"},
-   {"CHECK_PARAMS", "DO_PPGOTO", cond="fsm.vars.place"},
-   {"CHECK_PARAMS", "FAILED", cond="fsm.vars.goto_name", desc="goto_name param is deprecated!"},
-   {"WAIT_POSE", "CHECK_POSE", timeout=0.5},
-   {"CHECK_POSE", "FINAL", cond=pose_ok, desc="Pose reached" },
-   {"CHECK_POSE", "MISSED", cond="not pose_ok(self)"},
-   {"MISSED", "DO_RELGOTO", cond="not missed_too_often(self)"},
-   {"MISSED", "FAILED", cond=missed_too_often, desc="Posed missed"}
+  {"INIT",  "FAILED",         precond=check_navgraph, desc="no navgraph"},
+  {"INIT",  "FAILED",         cond="not vars.target_valid",                 desc="target invalid"},
+  {"INIT",  "SKILL_RELGOTO",  cond=true},
 }
 
-function CHECK_PARAMS:init()
-   self.fsm.vars.num_pos_missed=0
+function INIT:init()
+  self.fsm.vars.target_valid = true
 
-   if self.fsm.vars.place then
-      return
-   else
-      self.fsm.vars.x   = self.fsm.vars.x
-                       or self.fsm.vars.goto_x
-                       or pose:translation(0)
-      self.fsm.vars.y   = self.fsm.vars.y
-                       or self.fsm.vars.goto_y
-                       or pose:translation(1)
-      self.fsm.vars.ori = self.fsm.vars.ori
-                       or self.fsm.vars.goto_ori
-                       or 2*math.acos(pose:translation(3))
-   end
+  -- if a place is given, get the point from the navgraph and use this instead of x, y, ori
+  if self.fsm.vars.place ~= nil then
+    self.fsm.vars.node = navgraph:node(self.fsm.vars.place)
+    if self.fsm.vars.node:is_valid() then
+      self.fsm.vars.x = self.fsm.vars.node:x()
+      self.fsm.vars.y = self.fsm.vars.node:y()
+      if self.fsm.vars.node:has_property("orientation") then
+        self.fsm.vars.ori   = self.fsm.vars.node:property_as_float("orientation");
+      else
+        self.fsm.vars.ori   = nil   -- if orientation is not set, we don't care
+      end
+    else
+      self.fsm.vars.target_valid = false
+    end
+  end 
+
+  if self.fsm.vars.target_valid then
+    local rel_pos = tf_mod.transform({
+                      x = self.fsm.vars.x,
+                      y = self.fsm.vars.y,
+                      ori = self.fsm.vars.ori or 0},
+                      "/map", "/base_link")
+
+    self.fsm.vars.rel_x   = rel_pos.x
+    self.fsm.vars.rel_y   = rel_pos.y
+    if self.fsm.vars.ori == nil then
+      self.fsm.vars.rel_ori = nil
+    else
+      self.fsm.vars.rel_ori = rel_pos.ori
+    end
+  end
 end
 
-function MISSED:init()
-   self.fsm.vars.num_pos_missed = self.fsm.vars.num_pos_missed + 1
-end
-
-function DO_RELGOTO:init()
-   local rel_pos = tf_mod.transform({
-         x = self.fsm.vars.x,
-         y = self.fsm.vars.y,
-         ori = self.fsm.vars.ori},
-      "/map", "/base_link")
-
-   self.skills[1].x = rel_pos.x
-   self.skills[1].y = rel_pos.y
-   self.skills[1].ori = rel_pos.ori
-end
-
-function DO_PPGOTO:init()
-   self.skills[1].place = self.fsm.vars.place
+function SKILL_RELGOTO:init()
+  self.skills[1].x    = self.fsm.vars.rel_x
+  self.skills[1].y    = self.fsm.vars.rel_y
+  self.skills[1].ori  = self.fsm.vars.rel_ori
 end
