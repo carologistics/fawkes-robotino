cc0e59f331d8c52df689db1b63dc204fc608d0ed
Tobias Neumann <tobias.neumann@alumni.fh-aachen.de>
2015-04-20 15:29:59 +0200

    skills: add drive_to_global
    as a clear difference for the usages there are new skills for drive_to
    drive_to_local: to drive to with the local planer
    drive_to_global: to drive to with the global planer

---
+++ src/lua/skills/robotino/drive_to_global.lua
@@ -0,0 +1,60 @@
+
+----------------------------------------------------------------------------
+--  drive_to_global.lua
+--
+--  Created: Sat Jul 12 13:25:47 2014
+--  Copyright  2008       Tim Niemueller [www.niemueller.de]
+--             2014-2015  Tobias Neumann
+--
+----------------------------------------------------------------------------
+
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Library General Public License for more details.
+--
+--  Read the full text in the LICENSE.GPL file in the doc directory.
+
+-- Initialize module
+module(..., skillenv.module_init)
+
+-- Crucial skill information
+name               = "drive_to_global"
+fsm                = SkillHSM:new{name=name, start="DRIVE_TO", debug=true}
+depends_skills     = { "drive_to" }
+depends_interfaces = { }
+
+documentation      = [==[Drive to point with colisoin avoidance and last part with global_motor_move
+
+Parameters:
+      place:        Where to drive to
+      x:            x coordinate to drive to (if place is not set)
+      y:            y coordinate to drive to (if place is not set)
+      ori:          alternativ orientation
+      just_ori:     if true => after ppgoto finished, global_motor_move is just called with ori (for exploration)
+]==]
+
+-- Initialize as skill module
+skillenv.skill_module(_M)
+
+fsm:define_states{ export_to=_M,
+  closure={navgraph=navgraph, node_is_valid=node_is_valid},
+  {"DRIVE_TO",  SkillJumpState, skills={{drive_to}},  final_to="FINAL", fail_to="FAILED"},
+}
+
+fsm:add_transitions{
+}
+
+function DRIVE_TO:init()
+  self.skills[1].x        = self.fsm.vars.x
+  self.skills[1].y        = self.fsm.vars.y
+  self.skills[1].ori      = self.fsm.vars.ori
+  self.skills[1].place    = self.fsm.vars.place
+  self.skills[1].just_ori = self.fsm.vars.just_ori
+
+end
