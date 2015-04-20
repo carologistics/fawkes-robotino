5fb2e42e296ec123587f7165f688d8060ceef5d1
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2015-04-20 13:01:35 +0200

    skills-go15: add shelf_pick
    The skill shelf_pick is used to pick a workpiece from the shelf mounted
    on an MPS-Station. It accepts the parameter "slot" which can either be
    LEFT, MIDDLE or RIGHT.

--- src/lua/skills/robotino/shelf_pick.lua
+++ src/lua/skills/robotino/shelf_pick.lua
@@ -26,8 +26,10 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "shelf_pick"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
-depends_skills     = { }
-depends_interfaces = { }
+depends_skills     = {"motor_move", "ax12gripper"}
+depends_interfaces = {
+   {v = "line1", type = "LaserLineInterface"},
+}
 
 documentation      = [==[ shelf_pick
 
@@ -48,10 +50,61 @@ skillenv.skill_module(_M)
 
 fsm:define_states{ export_to=_M,
    {"INIT",       JumpState,  },
-   {"DUMMY_WAIT", JumpState,  },
+   {"GOTO_SHELF", SkillJumpState, skills={{motor_move}, {ax12gripper}}, final_to="WAIT_FOR_LASERLINE", fail_to="FAILED"},
+   {"APPROACH_SHELF", SkillJumpState, skills={{motor_move}}, final_to="GRAB_PRODUCT", fail_to="FAILED"},
+   {"GRAB_PRODUCT", SkillJumpState, skills={{ax12gripper}}, final_to="LEAVE_SHELF", fail_to="FAILED"},
+   {"LEAVE_SHELF", SkillJumpState, skills={{motor_move}}, final_to="CENTER_PUCK", fail_to="FAILED"},
+   {"CENTER_PUCK", SkillJumpState, skills={{ax12gripper}}, final_to="FINAL", fail_to="FAILED"},
+   {"WAIT_FOR_LASERLINE", JumpState,  },
 }
 
 fsm:add_transitions{
-   {"INIT",       "DUMMY_WAIT", cond=true},
-   {"DUMMY_WAIT", "FINAL",      timeout=10},
+   {"INIT",       "GOTO_SHELF", cond=true},
+   {"GOTO_SHELF", "FAILED", cond="vars.error"},
+   {"WAIT_FOR_LASERLINE", "APPROACH_SHELF", cond=true}
 }
+
+
+function GOTO_SHELF:init()
+   local dest_x = 0.2
+   local dest_y = line1:end_point_1(1) + 0.075
+   local line_offset
+   if line1:end_point_1(1) < line1:end_point_2(1) then
+      line_offset = line1:end_point_1(1) + 0.075
+   else
+      line_offset = line1:end_point_2(1) + 0.075
+   end
+   if self.fsm.vars.slot == "LEFT" then
+      dest_y = line_offset + 0.17
+   elseif self.fsm.vars.slot == "MIDDLE" then
+      dest_y = line_offset + 0.1
+   elseif self.fsm.vars.slot == "RIGHT" then
+      dest_y = line_offset
+   else
+      dest_x = 0
+      dest_y = 0
+      self.fsm:set_error("no shelf side set")
+      self.fsm.vars.error = true
+   end
+   
+   self.skills[1].x = dest_x
+   self.skills[1].y = dest_y
+--   self.skills[1].y = -0.02 -(self.fsm.vars.slot * 0.094)
+end
+
+function APPROACH_SHELF:init()
+   self.skills[1].x = line1:point_on_line(0) - 0.08
+   self.skills[2].open = true
+end
+
+function GRAB_PRODUCT:init()
+   self.skills[1].close = true
+end
+
+function LEAVE_SHELF:init()
+   self.skills[1].x = -0.1
+end
+
+function CENTER_PUCK:init()
+   self.skills[1].center = true
+end
