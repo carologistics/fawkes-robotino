b04bde342937dd88100ed793ee9389c4116d1734
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2015-04-20 13:04:11 +0200

    skills-go15: add shelf_put
    The skill shelf_put is used to store a workpiece on a shelf.
    This skill is neither finished nor tested.

--- src/lua/skills/robotino/shelf_put.lua
+++ src/lua/skills/robotino/shelf_put.lua
@@ -1,6 +1,6 @@
 
 ----------------------------------------------------------------------------
---  shelf_put.lua
+--  shelf_pick.lua
 --
 --  Created: Thu Aug 14 14:32:47 2008
 --  Copyright  2008  Tim Niemueller [www.niemueller.de]
@@ -26,18 +26,20 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "shelf_put"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
-depends_skills     = { }
-depends_interfaces = { }
+depends_skills     = {"motor_move", "ax12gripper"}
+depends_interfaces = {
+   {v = "line1", type = "LaserLineInterface"},
+}
 
 documentation      = [==[ shelf_put
 
                           This skill does:
                           - drives to the given Navgraphpunkt (ppgoto)
                           - aligns to the machine             (align_mps)
-                          - Puts puck onto Shelf              (SKILL-TODO)
+                          - Picks of Shelf                    (SKILL-TODO)
 
                           @param nav_point  string  the name of the navgraph point to drive to
-                          @param slot       string  the slot to put the puck of; options ( LEFT | MIDDLE | RIGHT )
+                          @param slot       string  the slot to pick the puck of; options ( LEFT | MIDDLE | RIGHT )
                           @param tag_id     int     the tag_id to variafy the alignmend to the correct machine
 
                           TODO this skill
@@ -48,10 +50,33 @@ skillenv.skill_module(_M)
 
 fsm:define_states{ export_to=_M,
    {"INIT",       JumpState,  },
+   {"GOTO_SHELF", SkillJumpState, skills={{motor_move}}, final_to="APPROACH_SHELF", fail_to="FAILED"},
+   {"APPROACH_SHELF", SkillJumpState, skills={{motor_move}}, final_to="STORE_PRODUCT", fail_to="FAILED"},
+   {"STORE_PRODUCT", SkillJumpState, skills={{ax12gripper}}, final_to="LEAVE_SHELF", fail_to="FAILED"},
+   {"LEAVE_SHELF", SkillJumpState, skills={{motor_move}}, final_to="FINAL", fail_to="FAILED"},
    {"DUMMY_WAIT", JumpState,  },
 }
 
 fsm:add_transitions{
-   {"INIT",       "DUMMY_WAIT", cond=true},
+   {"INIT",       "GOTO_SHELF", cond=true},
    {"DUMMY_WAIT", "FINAL",      timeout=10},
 }
+
+
+function GOTO_SHELF:init()
+   self.skills[1].x = 0.2
+   self.skills[1].y = line1:end_point_1()[1] + self.fsm.vars.slot * 0.1
+--   self.skills[1].y = -0.02 -(self.fsm.vars.slot * 0.094)
+end
+
+function APPROACH_SHELF:init()
+   self.skills[1].x = 0.14
+end
+
+function STORE_PRODUCT:init()
+   self.skills[1].open = true
+end
+
+function LEAVE_SHELF:init()
+   self.skills[1].x = -0.1
+end
