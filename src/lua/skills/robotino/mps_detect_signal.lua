a95f6298a07385e51d3b7594cc907e88b4b20ccd
Victor Mataré <matare@lih.rwth-aachen.de>
2015-04-23 22:52:59 +0200

    skills: super-awesome mps_detect_signal:
    move around until visibility history increases

--- src/lua/skills/robotino/mps_detect_signal.lua
+++ src/lua/skills/robotino/mps_detect_signal.lua
@@ -1,11 +1,16 @@
 
 ----------------------------------------------------------------------------
+--  explore_signal.lua - Position and detect signal
+--
+--  Copyright  2015 Victor Mataré
+----------------------------------------------------------------------------
+
 --  This program is free software; you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
---  the Free Software Foundation; either version 2 of the License,  or
+--  the Free Software Foundation; either version 2 of the License, or
 --  (at your option) any later version.
 --
---  This program is distributed in the hope that it will be useful, 
+--  This program is distributed in the hope that it will be useful,
 --  but WITHOUT ANY WARRANTY; without even the implied warranty of
 --  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --  GNU Library General Public License for more details.
@@ -13,12 +18,12 @@
 --  Read the full text in the LICENSE.GPL file in the doc directory.
 
 -- Initialize module
-module(...,  skillenv.module_init)
+module(..., skillenv.module_init)
 
 -- Crucial skill information
 name               = "mps_detect_signal"
-fsm                = SkillHSM:new{name=name,  start="INIT",  debug=true}
-depends_skills     = nil
+fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
+depends_skills     = {"mps_align", "mps_detect_signal" }
 depends_interfaces = {
    { v="bb_signal", type ="RobotinoLightInterface", id = "/machine-signal/best" },
    { v="bb_output", type ="RobotinoLightInterface", id = "Light determined" },
@@ -27,44 +32,77 @@ depends_interfaces = {
    { v="bb_sw_laser", type="SwitchInterface", id="laser-lines" }
 }
 
-
-
-documentation      = [==[
-writes ampel data into the light interface 
+documentation      = [==[Detect signal at a certain MPS
 
 Parameters:
       place: navgraph name of the machine node, e.g. C-CS1.
+      wait_for: GREEN|YELLOW|RED, i.e. become FINAL iff the specified signal is seen.
 ]==]
 
+
+-- Tunables
+local MIN_VIS_HIST=25
+local TIMEOUT=120
+local ALIGN_POS = {
+   {x=0.5, y=0, ori=0},
+   {x=0.6, y=0, ori=0.3},
+   {x=0.6, y=0, ori=-0.3},
+   {x=0.4, y=0, ori=0.2},
+   {x=0.4, y=0, ori=-0.2},
+   {x=0.5, y=0, ori=0.25},
+   {x=0.5, y=0, ori=-0.25}
+}
 
 -- Initialize as skill module
 skillenv.skill_module(_M)
 
-
 function done()
-   return bb_signal:is_ready() and bb_signal:visibility_history() > 25
+   return bb_signal:is_ready() and bb_signal:visibility_history() >= MIN_VIS_HIST
 end
 
 function invalid_node()
    return not navgraph:node(fsm.vars.place):is_valid()
 end
 
+function desired_signal()
+   local green = (fsm.vars.wait_for == "GREEN" and bb_signal.ON) or bb_signal.OFF
+   local yellow = (fsm.vars.wait_for == "YELLOW" and bb_signal.ON) or bb_signal.OFF
+   local red = (fsm.vars.wait_for == "RED" and bb_signal.ON) or bb_signal.OFF
 
-fsm:define_states{ export_to=_M,
-   closure={bb_signal=bb_signal, navgraph=navgraph},
+   return bb_signal:is_ready() and bb_signal:visibility_history() >= MIN_VIS_HIST
+      and bb_signal:red() == red
+      and bb_signal:yellow() == yellow
+      and bb_signal:green() == green
+end
+
+
+-- States
+fsm:define_states{
+   export_to=_M,
+   closure={ALIGN_POS=ALIGN_POS, bb_signal=bb_signal, navgraph=navgraph, done=done,
+      TIMEOUT=TIMEOUT, os=os, MIN_VIS_HIST=MIN_VIS_HIST},
    {"INIT", JumpState},
+   {"SKILL_ALIGN", SkillJumpState, skills={{mps_align}}, final_to="LOOK", fail_to="FAILED"},
+   {"LOOK", JumpState},
 }
 
+-- Transitions
 fsm:add_transitions{
    {"INIT", "FAILED", precond="not navgraph", desc="navgraph not available"},
    {"INIT", "FAILED", precond=invalid_node, desc="invalid node"},
    {"INIT", "FAILED", precond="not bb_signal:has_writer()", desc="bb_signal missing"},
-   {"INIT", "FINAL", cond=done},
-   {"INIT", "FAILED", timeout=10}
+   
+   {"INIT", "SKILL_ALIGN", cond=true},
+
+   {"LOOK", "FINAL", cond="(not vars.wait_for) and done()"},
+   {"LOOK", "FINAL", cond="wait_for and desired_signal()"},
+   {"LOOK", "FAILED", cond="os.time() > vars.giveup_time"},
+   {"LOOK", "SKILL_ALIGN", cond="(os.time() > vars.look_until) and (vars.best_vis_hist < MIN_VIS_HIST)", desc="move"}
 }
 
-
 function INIT:init()
+   self.fsm.vars.tries = 0
+   self.fsm.vars.giveup_time = os.time() + ((self.fsm.vars.wait_for and TIMEOUT) or 20)
    bb_sw_machine_signal:msgq_enqueue_copy(bb_sw_machine_signal.EnableSwitchMessage:new())
 
    local node = navgraph:node(self.fsm.vars.place)
@@ -75,6 +113,28 @@ function INIT:init()
    bb_signal_hint:msgq_enqueue_copy(msg)
 
    bb_sw_laser:msgq_enqueue_copy(bb_sw_laser.EnableSwitchMessage:new())
+end
+
+function LOOK:init()
+   self.fsm.vars.best_vis_hist = -1
+   self.fsm.vars.look_until = os.time() + 5
+end
+
+function LOOK:loop()
+   local vis_hist = bb_signal:visibility_history()
+   if vis_hist > self.fsm.vars.best_vis_hist then
+      self.fsm.vars.best_vis_hist = vis_hist
+   end
+end
+
+function SKILL_ALIGN:init()
+   self.fsm.vars.tries = self.fsm.vars.tries + 1
+   if self.fsm.vars.tries > #ALIGN_POS then
+      self.fsm.vars.tries = 1
+   end
+   self.skills[1].x = ALIGN_POS[self.fsm.vars.tries].x
+   self.skills[1].y = ALIGN_POS[self.fsm.vars.tries].y
+   self.skills[1].ori = ALIGN_POS[self.fsm.vars.tries].ori
 end
 
 function cleanup()
