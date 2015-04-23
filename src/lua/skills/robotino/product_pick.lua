071c8846a9734166674cca5ce3fa9a0abfe04737
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2015-04-23 10:40:41 +0200

    product_pick: use approach_mps subskill

--- src/lua/skills/robotino/product_pick.lua
+++ src/lua/skills/robotino/product_pick.lua
@@ -25,8 +25,10 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "product_pick"
 fsm                = SkillHSM:new{name=name, start="OPEN_GRIPPER", debug=true}
-depends_skills     = {"motor_move", "ax12gripper"}
-depends_interfaces = { }
+depends_skills     = {"motor_move", "ax12gripper", "approach_mps"}
+depends_interfaces = { 
+   {v = "sensor", type="RobotinoSensorInterface"}
+}
 
 documentation      = [==[The robot needs to be aligned with the machine, then just drives forward
 and opens the gripper
@@ -38,10 +40,10 @@ and opens the gripper
 skillenv.skill_module(_M)
 local tfm = require("tf_module")
 
-fsm:define_states{ export_to=_M,
+fsm:define_states{ export_to=_M, closure={sensor=sensor},
    {"OPEN_GRIPPER", SkillJumpState, skills={{ax12gripper}},
-      final_to="DRIVE_FORWARD", fail_to="FAILED"},
-   {"DRIVE_FORWARD", SkillJumpState, skills={{motor_move}},
+      final_to="APPROACH_MPS", fail_to="FAILED"},
+   {"APPROACH_MPS", SkillJumpState, skills={{approach_mps}},
       final_to="CLOSE_GRIPPER", fail_to="FAILED"},
    {"CLOSE_GRIPPER", SkillJumpState, skills={{ax12gripper}},
       final_to="WAIT", fail_to="FAILED"},
@@ -53,17 +55,8 @@ fsm:define_states{ export_to=_M,
 }
 
 fsm:add_transitions{
-   {"WAIT", "MOVE_BACK", timeout=0.5}
+   {"WAIT", "MOVE_BACK", timeout=0.5},
 }
-
-function DRIVE_FORWARD:init()
-   --TODO handle invalid point
-   local ALIGN_DISTANCE = navgraph:node(self.fsm.vars.place):property_as_float("align_distance")
-   local gripper_transformed = tfm.transform({x=ALIGN_DISTANCE, y=0, ori=0}, "/base_link", "/gripper")
-   self.skills[1].x = gripper_transformed.x
-   self.skills[1].y = gripper_transformed.y --if the gripper has an y offset
-   self.skills[1].ori = 0
-end
 
 function OPEN_GRIPPER:init()
    self.skills[1].open = true
