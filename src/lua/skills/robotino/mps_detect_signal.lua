279a843cc3f36f58b226821fbfd5455a2d842480
Victor Mataré <matare@lih.rwth-aachen.de>
2015-04-25 15:17:11 +0200

    skills:mps_detect_signal: ignore invalid place=
    use "default_place" as navgraph node if an invalid place= was passed.

--- src/lua/skills/robotino/mps_detect_signal.lua
+++ src/lua/skills/robotino/mps_detect_signal.lua
@@ -89,7 +89,7 @@ fsm:define_states{
 -- Transitions
 fsm:add_transitions{
    {"INIT", "FAILED", precond="not navgraph", desc="navgraph not available"},
-   {"INIT", "FAILED", precond=invalid_node, desc="invalid node"},
+   --{"INIT", "FAILED", precond=invalid_node, desc="invalid node"}, -- Agent may send invalid places. Undo when agent is fixed.
    {"INIT", "FAILED", precond="not bb_signal:has_writer()", desc="bb_signal missing"},
    
    {"INIT", "SKILL_ALIGN", cond=true},
@@ -104,7 +104,10 @@ function INIT:init()
    self.fsm.vars.tries = 0
    self.fsm.vars.giveup_time = os.time() + ((self.fsm.vars.wait_for and TIMEOUT) or 20)
    bb_sw_machine_signal:msgq_enqueue_copy(bb_sw_machine_signal.EnableSwitchMessage:new())
-
+ 
+   if not(self.fsm.vars.place and navgraph:node(self.fsm.vars.place):is_valid()) then
+      self.fsm.vars.place = "default_place"
+   end
    local node = navgraph:node(self.fsm.vars.place)
    local msg = bb_signal_hint.SignalPositionMessage:new()
    msg:set_translation(0, node:property_as_float("signal_hint_x"))
