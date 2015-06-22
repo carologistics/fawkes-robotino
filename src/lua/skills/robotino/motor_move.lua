bb85e5db5f7fe0c1e1cc058ed236c131cacf72c1
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2015-06-22 12:30:06 +0200

    motor_move: decrease tolerances
    We never tried to decrease the tolerances of
    motor_move on the new robotino. The motors
    seem to be more accurate, so we can align
    with less tolerances.

--- src/lua/skills/robotino/motor_move.lua
+++ src/lua/skills/robotino/motor_move.lua
@@ -39,11 +39,11 @@ documentation      = [==[Move on a (kind of) straight line relative to /base_lin
 ]==]
 
 -- Tunables
-local V_MAX =     { x=0.4,  y=0.4,  ori=1.8 }    -- ultimate limit
-local V_MIN =     { x=0.035, y=0.035, ori=0.15 }   -- below the motor won't even start
-local TOLERANCE = { x=0.04, y=0.04, ori=0.05 } -- accuracy
+local V_MAX =     { x=0.3,  y=0.3,  ori=1.0 }    -- ultimate limit
+local V_MIN =     { x=0.005, y=0.005, ori=0.04 }   -- below the motor won't even start
+local TOLERANCE = { x=0.002, y=0.002, ori=0.01 } -- accuracy
 local D_DECEL =   { x=0.07, y=0.07, ori=0.2 }    -- deceleration distance
-local ACCEL =     { x=0.05, y=0.05, ori=0.10 }   -- accelerate by this factor every loop
+local ACCEL =     { x=0.03, y=0.03, ori=0.07 }   -- accelerate by this factor every loop
 
 -- Initialize as skill module
 skillenv.skill_module(_M )
