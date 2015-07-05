8a10c9341292eb9dd548d66c330185612bfc74ea
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2015-07-05 22:50:16 +0200

    conveyor_vision: update comments

--- src/plugins/conveyor_vision/conveyor_vision_thread.cpp
+++ src/plugins/conveyor_vision/conveyor_vision_thread.cpp
@@ -538,7 +538,7 @@ void ConveyorVisionThread::detect()
       firevision::IplImageAdapter::convert_image_yuv422_planar(ipl,image_buffer);      
     }
   } else {
-    // puck not visible
+    // conveyor not visible
     if (visibility_history <= 0) {
             mps_conveyor_if_->set_visibility_history(visibility_history - 1);
     } else {
@@ -548,27 +548,4 @@ void ConveyorVisionThread::detect()
 
   }
   
-//  for( size_t i = 0; i < faces.size(); i++ )
-//  {
-////    Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-////    cout << "num faces: " << faces.size() << endl;
-////    rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar(0,0,255));
-////    ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-//
-////    Mat faceROI = frame_gray( faces[i] );
-////    std::vector<Rect> eyes;
-//
-//    //-- In each face, detect eyes
-////    eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-//
-////    for( size_t j = 0; j < eyes.size(); j++ )
-////     {
-//////       Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
-//////       int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-////       rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar(0,0,255));
-//////       circle( frame, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-////     }
-//  }
-//  //-- Show what you got
-//  imshow( window_name, frame );
  }
