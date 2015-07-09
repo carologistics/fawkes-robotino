d2c28ebf02d830fc49497c316d604abe321432f0
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2015-07-09 14:42:08 +0200

    slide_put: don't use laserlines
    Instead drive a fixed distance from the
    conveyor belt, because the conveyor vision
    alignment is quite good

--- src/lua/skills/robotino/slide_put.lua
+++ src/lua/skills/robotino/slide_put.lua
@@ -26,53 +26,39 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "slide_put"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
-depends_skills     = {"motor_move", "ax12gripper"}
-depends_interfaces = {
-   {v = "line1", type = "LaserLineInterface"},
-}
+depends_skills     = {"motor_move", "ax12gripper","approach_mps"}
+depends_interfaces = {}
 
 documentation      = [==[ slide_put
 
-                          This skill does:
-                          - drives to the given Navgraphpunkt (ppgoto)
-                          - aligns to the machine             (align_mps)
-                          - Picks of Shelf                    (SKILL-TODO)
-
-                          @param nav_point  string  the name of the navgraph point to drive to
-                          @param slot       string  the slot to pick the puck of; options ( LEFT | MIDDLE | RIGHT )
-                          @param tag_id     int     the tag_id to variafy the alignmend to the correct machine
-
-                          TODO this skill
+                     This skill does:
+                      - Put a base on the slide
+                     This skill needs:
+                      - to be aligned in front of the conveyor by the vision
 ]==]
 
 -- Initialize as skill module
 skillenv.skill_module(_M)
 
 fsm:define_states{ export_to=_M,
-   {"INIT",       JumpState,  },
+   {"INIT", JumpState},
    {"GOTO_SLIDE", SkillJumpState, skills={{motor_move}}, final_to="APPROACH_SLIDE", fail_to="FAILED"},
-   {"APPROACH_SLIDE", SkillJumpState, skills={{motor_move}}, final_to="STORE_PRODUCT", fail_to="FAILED"},
+   {"APPROACH_SLIDE", SkillJumpState, skills={{approach_mps}}, final_to="STORE_PRODUCT", fail_to="FAILED"},
    {"STORE_PRODUCT", SkillJumpState, skills={{ax12gripper}}, final_to="LEAVE_SLIDE", fail_to="FAILED"},
    {"LEAVE_SLIDE", SkillJumpState, skills={{motor_move}}, final_to="FINAL", fail_to="FAILED"},
 }
 
 fsm:add_transitions{
-   {"INIT",       "GOTO_SLIDE", cond=true},
+   {"INIT", "GOTO_SLIDE", cond=true},
 }
 
 
 function GOTO_SLIDE:init()
-   self.skills[1].x = 0.2
-   if line1:end_point_1(1) < 0 then
-     self.skills[1].y = line1:end_point_1(1) + 0.1
-   else
-     self.skills[1].y = line1:end_point_2(1) + 0.1
-   end
---   self.skills[1].y = -0.02 -(self.fsm.vars.slot * 0.094)
+   self.skills[1].y = 0.28 --TODO measure exact value
 end
 
 function APPROACH_SLIDE:init()
-   self.skills[1].x = 0.12
+   self.skills[1].x = 0.07 --TODO measure exact value
 end
 
 function STORE_PRODUCT:init()
