a775e0c925d61600fb98034beaecd96fd615fd71
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2015-07-10 10:19:33 +0200

    conveyor_vision: set conversion from cm to m in y for lines
    The unit delivered in the interface for the y-dimension was still
    propagating cm leading to an error factor of 100. This is now fixed by
    dividing the resulting length by 100 to get a correct amount of meters.

--- src/plugins/conveyor_vision/conveyor_vision_thread.cpp
+++ src/plugins/conveyor_vision/conveyor_vision_thread.cpp
@@ -513,6 +513,7 @@ void ConveyorVisionThread::detect()
       world_pos_x = sin(center_x_angle) * d_dash;
       
       world_pos_z_average = sqrt(d_dash*d_dash + world_pos_x*world_pos_x) / 100.;
+      world_pos_x /= 100.;
       }
     mps_conveyor_if_->set_translation(0, world_pos_z_average);
     mps_conveyor_if_->set_translation(1, world_pos_x);
