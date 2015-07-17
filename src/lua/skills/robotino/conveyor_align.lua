c55f13dcf0e12e4bd03797159a389f79db89979e
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2015-07-17 09:58:38 +0200

    skill/conveyor_align: add handling of z-alignment
    The skill conveyor_align now handles z-errors and makes use of the
    z-alignment of the gripper.
    It first tries to correct the z-position on driving in the y-direction
    and then corrects the z-position until the desired position (Z_DEST_POS)
    is reached at a given tolerance (TOLERANCE_Z).

--- src/lua/skills/robotino/conveyor_align.lua
+++ src/lua/skills/robotino/conveyor_align.lua
@@ -36,50 +36,72 @@ conveyor vision
 skillenv.skill_module(_M)
 
 local TOLERANCE_Y = 0.005
-local TOLERANCE_Z = 0.001
+local TOLERANCE_Z = 0.004
 local MAX_TRIES = 4
+local Z_DEST_POS=0.005
 
 function no_writer()
    return not conveyor_0:has_writer()
 end
 
-function tolerance_not_ok()
-   return not (math.abs(conveyor_0:translation(1)) <= TOLERANCE_Y and math.abs(conveyor_0:translation(2)) <= TOLERANCE_Z)
+function tolerance_y_not_ok()
+   return not (math.abs(conveyor_0:translation(1)) <= TOLERANCE_Y)
+end
+
+function tolerance_z_not_ok()
+   return not (math.abs(conveyor_0:translation(2) - Z_DEST_POS) <= TOLERANCE_Z)
 end
 
 fsm:define_states{ export_to=_M,
    closure={MAX_TRIES=MAX_TRIES},
    {"INIT", JumpState},
-   {"APPROACH_MPS", SkillJumpState, skills={{approach_mps}}, final_to="DRIVE_Y", fail_to="FAILED"},
-   {"DRIVE_Y", SkillJumpState, skills={{motor_move}, {ax12gripper}}, final_to="SETTLE", fail_to="FAILED"},
+   {"APPROACH_MPS", SkillJumpState, skills={{approach_mps}}, final_to="DRIVE_YZ", fail_to="FAILED"},
+   {"DRIVE_YZ", SkillJumpState, skills={{motor_move}, {ax12gripper}}, final_to="SETTLE", fail_to="FAILED"},
+   {"DRIVE_Z", SkillJumpState, skills={{ax12gripper}}, final_to="SETTLE_Z", fail_to="FAILED"},
    {"SETTLE", JumpState},
+   {"SETTLE_Z", JumpState},
    {"CHECK", JumpState},
+   {"CHECK_Z", JumpState},
 }
 
 fsm:add_transitions{
    {"INIT", "FAILED", cond=no_writer, desc="no conveyor vision"},
    {"INIT", "APPROACH_MPS", cond=true},
    {"SETTLE", "CHECK", timeout=0.5},
+   {"SETTLE_Z", "CHECK_Z", timeout=0.5},
    {"CHECK", "FAILED", cond="vars.counter > MAX_TRIES", desc="max tries reached"},
-   {"CHECK", "DRIVE_Y", cond=tolerance_not_ok, desc="tolerance not ok, align y distance"},
-   {"CHECK", "FINAL", cond=true},
+   {"CHECK", "DRIVE_YZ", cond=tolerance_y_not_ok, desc="tolerance not ok, align y distance"},
+   {"CHECK", "SETTLE_Z", cond=true},
+   {"CHECK_Z", "FAILED", cond="vars.counter_z > MAX_TRIES", desc="max tries reached"},
+   {"CHECK_Z", "DRIVE_Z", cond=tolerance_z_not_ok, desc="tolerance not ok, align z distance"},
+   {"CHECK_Z", "FINAL", cond=true},
 }
 
 function INIT:init()
    self.fsm.vars.counter = 1
+   self.fsm.vars.counter_z = 1
 end
 
 function APPROACH_MPS:init()
    self.skills[1].x = 0.2
 end
 
-function DRIVE_Y:init()
-   self.skills[1].y = -conveyor_0:translation(1)
+function DRIVE_YZ:init()
+   self.skills[1].y = conveyor_0:translation(1)
    self.skills[1].tolerance = { x=0.002, y=0.002, ori=0.01 }
    self.skills[2].command = "RELGOTOZ"
-   self.skills[2].z_position = -conveyor_0:translation(2)
+   self.skills[2].z_position = (conveyor_0:translation(2) - Z_DEST_POS) * 1000
+end
+
+function DRIVE_Z:init()
+   self.skills[1].command = "RELGOTOZ"
+   self.skills[1].z_position = (conveyor_0:translation(2) - Z_DEST_POS) * 1000
 end
 
 function CHECK:init()
    self.fsm.vars.counter = self.fsm.vars.counter + 1
+end
+
+function CHECK_Z:init()
+   self.fsm.vars.counter_z = self.fsm.vars.counter_z + 1
 end
