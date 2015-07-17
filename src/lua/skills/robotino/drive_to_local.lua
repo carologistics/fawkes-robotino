b4a30462e21d7581c41fdab70481928aa37be00e
Tobias Neumann <tobias.neumann@alumni.fh-aachen.de>
2015-07-17 11:39:24 +0900

    skills/drive_to_local: fixed just ori values

--- src/lua/skills/robotino/drive_to_local.lua
+++ src/lua/skills/robotino/drive_to_local.lua
@@ -100,8 +100,8 @@ function SKILL_GLOBAL_MOTOR_MOVE:init()
     self.skills[1].x   = self.fsm.vars.x
     self.skills[1].y   = self.fsm.vars.y
   end
-  self.skills[1].x   = 0
-  self.skills[1].y   = 0
+  self.skills[1].x   = nil
+  self.skills[1].y   = nil
   self.skills[1].ori = self.fsm.vars.ori
   
   printf("Drive to: call global_motor_move with: x(" .. tostring(self.skills[1].x) .. ") y(" .. tostring(self.skills[1].y)  ..") ori(" .. tostring(self.skills[1].ori) .. ")")
