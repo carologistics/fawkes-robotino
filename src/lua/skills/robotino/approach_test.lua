9dd6fb49287fbf3373e39290ad0f51fad87fa11c
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2015-07-19 21:14:58 +0800

    approach_test: add support for shelf and product put

--- src/lua/skills/robotino/approach_test.lua
+++ src/lua/skills/robotino/approach_test.lua
@@ -24,7 +24,7 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "approach_test"
 fsm                = SkillHSM:new{name=name, start="MPS_ALIGN", debug=true}
-depends_skills     = {"mps_align","product_put","product_pick","shelf_put","slide_put","conveyor_align"}
+depends_skills     = {"mps_align","product_put","product_pick","shelf_put","shelf_pick","slide_put","conveyor_align"}
 depends_interfaces = {
 }
 
@@ -37,6 +37,7 @@ Parameters:
       @param option  whether to pick or put, default is input ("put" or "pick")
       @param shelf   optional position on shelf: ( LEFT | MIDDLE | RIGHT )
       @param slide   optional true if you want to put it on the slide
+      @param tag_id
 ]==]
 -- Initialize as skill module
 skillenv.skill_module(_M)
@@ -47,13 +48,15 @@ fsm:define_states{ export_to=_M, closure={navgraph=navgraph},
    {"CONVEYOR_ALIGN", SkillJumpState, skills={{conveyor_align}}, final_to="DECIDE_ENDSKILL", fail_to="DECIDE_ENDSKILL"},
    {"DECIDE_ENDSKILL", JumpState},
    {"SKILL_SHELF_PUT", SkillJumpState, skills={{shelf_put}}, final_to="FINAL", fail_to="FAILED"},
+   {"SKILL_SHELF_PICK", SkillJumpState, skills={{shelf_pick}}, final_to="FINAL", fail_to="FAILED"},
    {"SKILL_SLIDE_PUT", SkillJumpState, skills={{slide_put}}, final_to="FINAL", fail_to="FAILED"},
    {"SKILL_PRODUCT_PUT", SkillJumpState, skills={{product_put}}, final_to="FINAL", fail_to="FAILED"},
    {"SKILL_PRODUCT_PICK", SkillJumpState, skills={{product_pick}}, final_to="FINAL", fail_to="FAILED"}
 }
 
 fsm:add_transitions{
-   {"DECIDE_ENDSKILL", "SKILL_SHELF_PUT", cond="vars.shelf", desc="Put on shelf"},
+   {"DECIDE_ENDSKILL", "SKILL_SHELF_PUT", cond="vars.shelf and vars.option=='put'", desc="Put on shelf"},
+   {"DECIDE_ENDSKILL", "SKILL_SHELF_PICK", cond="vars.shelf and vars.option=='pick'", desc="Pick from shelf"},
    {"DECIDE_ENDSKILL", "SKILL_SLIDE_PUT", cond="vars.slide", desc="Put on slide"},
    {"DECIDE_ENDSKILL", "SKILL_PRODUCT_PUT", cond="vars.option=='put'", desc="Put on conveyor"},
    {"DECIDE_ENDSKILL", "SKILL_PRODUCT_PICK", cond="vars.option=='pick'", desc="Pick from conveyor"}
@@ -62,9 +65,15 @@ fsm:add_transitions{
 function MPS_ALIGN:init()
    -- align in front of the conveyor belt
    self.skills[1].x = 0.6
+   self.skills[1].tag_id = self.fsm.vars.tag_id
 end
 
 function SKILL_SHELF_PUT:init()
+   -- Just hand through the Shelf position
+   self.skills[1].slot = self.fsm.vars.shelf
+end
+
+function SKILL_SHELF_PICK:init()
    -- Just hand through the Shelf position
    self.skills[1].slot = self.fsm.vars.shelf
 end
