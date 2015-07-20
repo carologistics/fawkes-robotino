fee84cd73638e7f8f61b92e3a507350a761af80d
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2015-07-20 18:52:28 +0800

    approach_mps: handle negative offsets

--- src/lua/skills/robotino/approach_mps.lua
+++ src/lua/skills/robotino/approach_mps.lua
@@ -41,7 +41,7 @@ documentation      = [==[
 skillenv.skill_module(_M)
 local tfm = require("tf_module")
 local sensor_index = 0
-local sensor_threshold = 0.06
+local sensor_threshold = 0.055
 
 if config:exists("/hardware/robotino/distance_front/index") then
    sensor_index = config:get_uint("/hardware/robotino/distance_front/index")
@@ -50,21 +50,38 @@ end
 fsm:define_states{ export_to=_M, closure={sensor=sensor, sensor_index=sensor_index},
    {"INIT", JumpState},
    {"APPROACH_WITH_INFRARED", SkillJumpState, skills={{motor_move}}, final_to="FINAL", fail_to="FAILED"},
+   {"CHECK", JumpState}, -- if the offset is negative drive the last bit with motor move because the sensor stop at 5mm
+   {"MOTOR_MOVE", SkillJumpState, skills={{motor_move}}, final_to="FINAL", fail_to="FAILED"},
 }
 
 fsm:add_transitions{
    {"INIT", "APPROACH_WITH_INFRARED", cond=true},
-   {"APPROACH_WITH_INFRARED", "FINAL", cond="sensor:distance(sensor_index) <= vars.sensor_threshold and sensor:distance(sensor_index) > 0"}
+   {"APPROACH_WITH_INFRARED", "CHECK", cond="sensor:distance(sensor_index) <= vars.sensor_threshold and sensor:distance(sensor_index) > 0"},
+   {"CHECK", "MOTOR_MOVE", cond="vars.offset_x < 0"},
+   {"CHECK", "FINAL", cond=true},
 }
 
-function APPROACH_WITH_INFRARED:init()
-   if self.fsm.vars.offset_x == nil then
+function INIT:init()
+   if self.fsm.vars.offset_x == nil then --for backwards compatibility
       self.fsm.vars.offset_x = 0
    end
+end
+
+function APPROACH_WITH_INFRARED:init()
    printf("x_offset is set to: %f", self.fsm.vars.offset_x)
-   self.fsm.vars.sensor_threshold = self.fsm.vars.x or sensor_threshold + self.fsm.vars.offset_x 
+   if self.fsm.vars.offset_x >= 0 then
+      self.fsm.vars.sensor_threshold = self.fsm.vars.x or sensor_threshold + self.fsm.vars.offset_x 
+   else
+      self.fsm.vars.sensor_threshold = sensor_threshold -- the threshold is negative so we adjust the rest in the upcoming motor_move state
+   end
    --TODO !!!!!!!handle negative with motor_move and further driving when putting a product!!!!!!!!!!!!!!!!
    printf("sensor threshold is: %f", self.fsm.vars.sensor_threshold)
    self.skills[1].x = 1
    self.skills[1].vel_trans = 0.05
+end
+
+function MOTOR_MOVE:init()
+   self.skills[1].x = -self.fsm.vars.offset_x --with the above condition this is negative so invert it
+   self.skills[1].vel_trans = 0.03
+   self.skills[1].tolerance = { x=0.001, y=0.002, ori=0.01 }
 end
