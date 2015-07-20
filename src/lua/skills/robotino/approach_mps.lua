0c8cc9dd53bb86120ee5deaaa24f7899412af9e8
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2015-07-20 16:29:35 +0800

    approach_mps: remove conveyor cam

--- src/lua/skills/robotino/approach_mps.lua
+++ src/lua/skills/robotino/approach_mps.lua
@@ -28,15 +28,11 @@ fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
 depends_skills     = {"motor_move"}
 depends_interfaces = { 
    {v = "sensor", type="RobotinoSensorInterface"},
-   {v = "conveyor_0", type="Position3DInterface"},
-   {v = "gripper_if", type = "AX12GripperInterface", id="Gripper AX12"}
 }
 
 documentation      = [==[
                         The robot just drives forward until a sensor threshold is reached
                         @param "x" int The x distance to the MPS when finished
-                        @param "vision" boolean if true use the conveyor vision
-                                                instead of the infrared sensor
                      ]==]
 
 
@@ -45,27 +41,18 @@ skillenv.skill_module(_M)
 local tfm = require("tf_module")
 local sensor_index = 0
 local sensor_threshold = 0.07
-local MIN_VIS_HIST = 10
 
 if config:exists("/hardware/robotino/distance_front/index") then
    sensor_index = config:get_uint("/hardware/robotino/distance_front/index")
 end
 
-fsm:define_states{ export_to=_M, closure={sensor=sensor, sensor_index=sensor_index, conveyor_0=conveyor_0, MIN_VIS_HIST=MIN_VIS_HIST},
+fsm:define_states{ export_to=_M, closure={sensor=sensor, sensor_index=sensor_index},
    {"INIT", JumpState},
    {"APPROACH_WITH_INFRARED", SkillJumpState, skills={{motor_move}}, final_to="FINAL", fail_to="FAILED"},
-   {"SETTLE", JumpState},
-   {"CHECK_VIS_HIST", JumpState},
-   {"APPROACH_WITH_CAM", SkillJumpState, skills={{motor_move}}, final_to="FINAL", fail_to="FAILED"},
 }
 
 fsm:add_transitions{
-   {"INIT", "SETTLE", cond="vars.vision == true"},
-   {"INIT", "FAILED", cond="not conveyor_0:has_writer()", desc="Conveyor vision disabled"},
    {"INIT", "APPROACH_WITH_INFRARED", cond=true},
-   {"SETTLE", "CHECK_VIS_HIST", timeout=1},
-   {"CHECK_VIS_HIST", "APPROACH_WITH_CAM", cond="conveyor_0:visibility_history() >= MIN_VIS_HIST"},
-   {"CHECK_VIS_HIST", "FAILED", timeout=10},
    {"APPROACH_WITH_INFRARED", "FINAL", cond="sensor:distance(sensor_index) <= vars.sensor_threshold and sensor:distance(sensor_index) > 0"}
 }
 
@@ -73,13 +60,4 @@ function APPROACH_WITH_INFRARED:init()
    self.fsm.vars.sensor_threshold = self.fsm.vars.x or 0.07
    self.skills[1].x = 1
    self.skills[1].vel_trans = 0.05
-end
-
-function APPROACH_WITH_CAM:init()
-   self.skills[1].x = tfm.transform({x=conveyor_0:translation(0),y=0,ori=0}, "/cam_conveyor", "/gripper").x
-   if gripper_if:is_holds_puck() then
-      self.skills[1].x = self.skills[1].x + 0.02
-   end
-   self.skills[1].vel_trans = 0.05
-   self.skills[1].TOLERANCE = { x=0.002, y=0.002, ori=0.01 }
 end
