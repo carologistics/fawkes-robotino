dfb6cf89e7277c900a9f2bfe3af94fcdddbff32d
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2015-09-25 12:24:45 +0200

    webtools-bridge: Streams Manipulation
    After removing lots of cofusions about the concepts of stream..The
    ostringstream that was used to process the streambuf (which has the
    received msgs) was replaced with an std::istream.
    
    The rapidjson can only read UTF-8 byte streams directly..Since i am not
    sure which encoding is arriving from the sockect i want to make it
    flixable. Luckly Rapidjson has AutoUTFInputStream that will detect
    encoding. Problem is you have to pass a CustomStream implementing their
    peek4() function that helps with the detection..
    
    For that i created a istream wrapper to pass it to the
    AutoUTFInputStream

---
+++ src/plugins/bridge/IStreamWrapper.cpp
@@ -0,0 +1,28 @@
+class IStreamWrapper {
+public:
+    typedef char Ch;
+    IStreamWrapper(std::istream& is) : is_(is) {
+    }
+    Ch Peek() const { // 1
+        int c = is_.peek();
+        return c == std::char_traits<char>::eof() ? '\0' : (Ch)c;
+    }
+    Ch Take() { // 2
+        int c = is_.get();
+        return c == std::char_traits<char>::eof() ? '\0' : (Ch)c;
+    }
+    size_t Tell() const { return (size_t)is_.tellg(); } // 3
+    Ch* PutBegin() { assert(false); return 0; }
+    void Put(Ch) { assert(false); }''
+    void Flush() { assert(false); }
+    size_t PutEnd(Ch*) { assert(false); return 0; }
+    // For encoding detection only.
+    const Ch* Peek4() const {
+    return Tell() + 4 <= size_ ? src_ : 0;
+    }
+
+private:
+    IStreamWrapper(const IStreamWrapper&);
+    IStreamWrapper& operator=(const IStreamWrapper&);
+    std::istream& is_;
+};
