cb190b9074fd7af98ba6416af11819ea799e220c
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2015-10-05 00:27:48 +0200

    webtools-bridge: Dispatcher intializing the ros_client from dispatcher
    The thread intiated in the endpoint to run the transport in the
    backgound,  was always terminate if i copied the endpoint_shared_ptr to
    a global shared_ptr of the same type. even though the thread keeps
    running if i only exited the init() without trying to copy the
    shared_ptr.
    
    it wokred When passed the global shared_ptr to the function as a paramter and let it do the binding

--- src/plugins/bridge/dispatcher.cpp
+++ src/plugins/bridge/dispatcher.cpp
@@ -1,12 +1,5 @@
 
 #include "dispatcher.h"
-#include "ros_proxy.h"
-#include <boost/bind.hpp>
-#include <exception> 
-#include <boost/lexical_cast.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/make_shared.hpp>
-
 #include <cassert>
 #include <exception>
 #include <iostream>
@@ -19,21 +12,28 @@
 #include "rapidjson/encodedstream.h"// AutoUTFInputStream
 #include "rapidjson/memorystream.h"    
 #include "rapidjson/error/en.h"
-#include "rapidjson/filereadstream.h"
-using namespace boost::asio;
+
+
+
 using namespace rapidjson;
 
-
-
-
-Dispatcher::Dispatcher(unsigned short client_port, unsigned short server_port):
-		acceptor_(io_service_, ip::tcp::endpoint( ip::tcp::v6(), client_port))
-		,client_port_(client_port)
-		,server_port_(server_port)
+Dispatcher::Dispatcher():
+		rosbridge_up_(false)
 		{
-		acceptor_.set_option(socket_base::reuse_address(true));
-		start_accept();
-		this->io_service_.run();
+		init(rosbridge_ptr_);
+}
+
+void
+Dispatcher::init(ros_endpoint::ptr rosbridge_ptr){
+    
+  rosbridge_ptr = websocketpp::lib::make_shared<ros_endpoint>();
+  //m_ros_list[0] = websocketpp::lib::make_shared<ros_endpoint>();
+   int id =  rosbridge_ptr->connect("ws://localhost:9090");
+    if (id != -1) {
+        std::cout << "> Created connection with id " << id << std::endl;
+   }
+
+ // m_ros_list.push_back(rosbridge_ptr);
 
 }
 
@@ -41,160 +41,116 @@ Dispatcher::Dispatcher(unsigned short client_port, unsigned short server_port):
 
 Dispatcher::~Dispatcher()
 {
-	boost::system::error_code err;
-  	io_service_.stop();
-  	delete rosProxy_;
+  	//delete rosbridge_;
+ }
   	
-}
+
+
 
 void
-Dispatcher::start_accept(){
-	//this is wrong ...this makes a new instance in the same var every time someone tries to connect
-	//we want to have one of this mapping instance every
-	rosProxy_=new RosProxy(io_service_, server_port_); 
-	rosProxy_->start_server();
-	acceptor_.async_accept(rosProxy_->client_socket_,boost::bind(&Dispatcher::handle_accept, this,boost::asio::placeholders::error));
-}
-
-
-void 
-Dispatcher::handle_accept(const boost::system::error_code &ec){
-	if(!ec){		
-		boost::system::error_code ec;
-
-		start_dispatching();
-
-		//Before you start accept again you have to make sure that it would replace the rosProxy instance
-		//start_accept();
-	}
-}
-
-void
-Dispatcher::start_dispatching(){
-
-	boost::asio::async_read(rosProxy_->client_socket_, boost::asio::buffer(readBuffer,sizeof(readBuffer)),boost::asio::transfer_at_least(1),
-			boost::bind(&Dispatcher::handle_client_reads, this,
-							boost::asio::placeholders::error,
-			   				boost::asio::placeholders::bytes_transferred)
-			);
-}
-
-void
-Dispatcher::handle_client_reads(const boost::system::error_code &ec , size_t bytes_transferred){
-
-	if(!ec){
-
-
-	std::cout << "CLINET HANDLER \n";
-	
-	std::string s(readBuffer);  
-	//std::istream is(&buff_c);
-	//s = ss.str();
-	//todo:choose the right rosProxy instance
-
-	MemoryStream ms(readBuffer,sizeof(readBuffer));
-	//FileReadStream ms(is, buff_c, sizeof(buff_c));
-
-
-
-	// std::cout << "is by \n";
-	// std::cout << s; 
-	// std::cout << "is by \n";
-
-	// std::cout << is.rdbuf();
-
-	// std::cout << "is by \n";
-	// std::cout << bytes_transferred;
-	// std::cout << "is sdfsdfy \n";
-	// 	is>>s;
-
-
-	// std::cout << s;
-	// std::cout << "is sdfsdfy \n";
-	
-	// 	is>>s;
-
-
-	// std::cout << s;
-	// std::cout << "is sdfsdfy \n";
-
-	// 	is>>s;
-
-
-	// std::cout << s;
-	// std::cout << "is sdfsdfy \n";
-
-	// 	is>>s;
-
-
-
-	//std::ostringstream ss;
-	//eis << &buff_c;
-	//s = eis.str();
-	//todo:choose the right rosProxy instance
-
-
-	//this waiting for server has to be replaced with a better solution
-	do{
-	std::cout << "waiting for rosbridge proxy to come alive \n";
-	}
-	while(!rosProxy_->check_rosBridge_alive());
-	rosProxy_->process_req(s);
-
-
-		
-	AutoUTFInputStream<unsigned, MemoryStream> eis(ms);
-
-	 Document d;
-	 if(!d.ParseStream<0, AutoUTF<unsigned> >(eis).HasParseError()){
-		std::cout << "JSON: parsed! \n";
-		if(d.IsObject()){
-			std::cout << "JSON: Object! \n";
-			if(d.HasMember("op")){
-			std::cout << "JSON: op is there! \n";
-			}
-			else{std::cout << "NOT Member! \n";}
-
-		
-		}
-		else
-			std::cout << "NOT object \n!";
+Dispatcher::dispatch_msg(){
 
 }
-else{
- fprintf(stderr, "\nError(offset %u): %s\n", 
-        (unsigned)d.GetErrorOffset(),
-        GetParseError_En(d.GetParseError()));
-    // ...
-	}
 
-	start_dispatching();
-	}else {
-    //disconnect("handle_client_reads", ec.message().c_str());
-  }
-
-}
-  
 // void
-// Dispatcher::write_to_client(std::string s)
-// {
-// 	boost::system::error_code ec;
- 	
-// 	size_t t =boost::asio::buffer_size(boost::asio::buffer(s));
+// Dispatcher::handle_client_reads(const boost::system::error_code &ec , size_t bytes_transferred){
+
+// 	if(!ec){
+
+
+// 	std::cout << "CLINET HANDLER \n";
 	
+// 	std::string s="";  
+// 	std::ostringstream ss;
+// 	ss << &buff_c;
+// 	s = ss.str();
+// 	//todo:choose the right rosProxy instance
 
-// 	std::cout << "Writing to client!";
-// 	std::cout << t;
-// 	std::cout << "bytes \n!";
 
-//  	boost::asio::write(*client_socket_ptr,boost::asio::buffer(s) , ec);
-// 	if(ec){
-// 	  		std::cout << "Failed to write! \n";
-// 	  }
-  
+
+// 	MemoryStream ms(s.c_str(),sizeof(s.c_str()));
+
+
+// 	// std::cout << "is by \n";
+// 	// std::cout << s; 
+// 	// std::cout << "is by \n";
+
+// 	// std::cout << is.rdbuf();
+
+// 	// std::cout << "is by \n";
+// 	// std::cout << bytes_transferred;
+// 	// std::cout << "is sdfsdfy \n";
+// 	// 	is>>s;
+
+
+// 	// std::cout << s;
+// 	// std::cout << "is sdfsdfy \n";
+	
+// 	// 	is>>s;
+
+
+// 	// std::cout << s;
+// 	// std::cout << "is sdfsdfy \n";
+
+// 	// 	is>>s;
+
+
+// 	// std::cout << s;
+// 	// std::cout << "is sdfsdfy \n";
+
+// 	// 	is>>s;
+
+
+
+// 	//std::ostringstream ss;
+// 	//eis << &buff_c;
+// 	//s = eis.str();
+// 	//todo:choose the right rosProxy instance
+
+
+// 	//this waiting for server has to be replaced with a better solution
+// 	do{
+// 	std::cout << "waiting for rosbridge proxy to come alive \n";
+// 	}
+// 	while(!rosProxy_->check_rosBridge_alive());
+// 	rosProxy_->process_req(s);
+
+
+		
+// 	AutoUTFInputStream<unsigned, MemoryStream> eis(ms);
+
+// 	 Document d;
+// 	 if(!d.ParseStream<0, AutoUTF<unsigned> >(eis).HasParseError()){
+// 		std::cout << "JSON: parsed! \n";
+// 		if(d.IsObject()){
+// 			std::cout << "JSON: Object! \n";
+// 			if(d.HasMember("op")){
+// 			std::cout << "JSON: op is there! \n";
+// 			}
+// 			else{std::cout << "NOT Member! \n";}
+
+		
+// 		}
+// 		else
+// 			std::cout << "NOT object \n!";
+
 // }
+// else{
+//  fprintf(stderr, "\nError(offset %u): %s\n", 
+//         (unsigned)d.GetErrorOffset(),
+//         GetParseError_En(d.GetParseError()));
+//     // ...
+// 	}
+
+// 	start_dispatching();
+// 	}else {
+//     //disconnect("handle_client_reads", ec.message().c_str());
+//   }
+
+ //}
   
  
 int main(){
-	Dispatcher *dispatcher=new Dispatcher(8080,9090);
+	Dispatcher *dispatcher=new Dispatcher();
+	//dispatcher->init();
 }
