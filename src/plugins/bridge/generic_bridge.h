cbe66635d0e02d803cf22f96cad9b02005491e9f
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2015-11-20 12:31:56 +0100

    webtools-bridge: abstract class generic-bridge as base class

---
+++ src/plugins/bridge/generic_bridge.h
@@ -0,0 +1,29 @@
+#include <ibridge.h>
+#include <idispatcher.h>
+
+
+class GenericBridge: public ibridge
+{
+
+public:
+
+	GenericBridge(std::shared_ptr<Idispatcher> dispatcher)
+	:dispatcher_(dispatcher)
+	{
+
+	}
+
+	~GenericBridge();
+
+	virtual void incoming(std::msg jsonStr)=0;
+
+	void outgoing(std::msg jsonStr);
+
+	void serialize(std::msg jsonStr);
+
+	void deserialize(std::msg jsonStr);
+
+private:
+	std::shared_ptr<Idispatcher>  dispatcher_;
+
+}
