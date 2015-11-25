6cecfccbed09cdb234e6a12897df7c53e9d6d7b9
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2015-11-25 12:54:12 +0100

    webtools-bridge: subscription forms

--- src/plugins/bridge/subscribe.h
+++ src/plugins/bridge/subscribe.h
@@ -1,3 +1,17 @@
-calss Subscribe{
-	
+#include <map>
+
+
+class Subscribe{
+	public:
+		Subscribe();
+		~Subscribe();
+
+		void subscirbe(std::string topic,std::string client_id);
+		void unsubscirbe(std::string topic, std::string client_id);
+
+		void publish(std::string topic, std::string client_id );
+
+	private:
+		std::map <std::string,std::string> subscirbtions;
+
 }
---
+++ src/plugins/bridge/subscribtion.h
@@ -0,0 +1,18 @@
+struct  client{
+
+   float throttle_rate;
+   int queue_length;
+   int fragment_size;
+   // compression": compression
+};
+
+class subscribtion{
+	
+	public:
+
+	private:
+		std::string 			client_id;
+		std::string 			topic;
+		//handler 		 publish			find a way to register it
+
+}
