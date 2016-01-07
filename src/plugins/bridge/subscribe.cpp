dcafaf2391bcae531b8ecfd7d52a6e9dac0df702
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2016-01-07 19:53:09 +0100

    webtools-bridge: throttle_rate threading bug fixed

--- src/plugins/bridge/subscribe.cpp
+++ src/plugins/bridge/subscribe.cpp
@@ -1,9 +1,11 @@
 #include "subscribe.h"
 
-#include <utils/time/time.h>
 #include "rapidjson/document.h"     // rapidjson's DOM-style API
 #include "rapidjson/prettywriter.h" // for stringify JSON
 #include <cstdio>
+
+#include <utils/time/time.h>
+#include <core/threading/mutex.h>
 
 
 
@@ -17,6 +19,7 @@ using namespace fawkes;
 		{
 			clock_=clock;
 			client_id_="";
+
 		}
 
 		Subscribe::~Subscribe(){}
@@ -41,36 +44,37 @@ using namespace fawkes;
 			std::cout<< " TO" << topic_name << std::endl;
 
 			if(topic_subscirbtions_.find(topic_name)==topic_subscirbtions_.end()){
-					if (manager_->subscribe(topic_name))
+					//if (manager_->subscribe(topic_name))
 						topic_subscirbtions_[topic_name]= std::make_shared<Subscribtion>(clock_,client_id_,topic_name);
-					else
-						return;
+					//else
+					//	return;
 			}
 
 
-			details subscribe_args;
+			details *subscribe_args=new details();
 			if(d.HasMember("id"))
-			subscribe_args.subscribtion_id=std::string(d["id"].GetString());
+				subscribe_args->subscribtion_id=std::string(d["id"].GetString());
 			if(d.HasMember("type"))
-			subscribe_args.msg_type=std::string(d["type"].GetString());
+				subscribe_args->msg_type=std::string(d["type"].GetString());
 			if(d.HasMember("throttle_rate"))
-			subscribe_args.throttle_rate=d["throttle_rate"].GetInt();
+				subscribe_args->throttle_rate=d["throttle_rate"].GetInt();
 			if(d.HasMember("queue_length"))
-			subscribe_args.queue_length=d["queue_length"].GetInt();
+				subscribe_args->queue_length=d["queue_length"].GetInt();
 			if(d.HasMember("fragment_size"))
-			subscribe_args.fragment_size=d["fragment_size"].GetInt();
+				subscribe_args->fragment_size=d["fragment_size"].GetInt();
 			if(d.HasMember("compression"))
-			subscribe_args.compression=std::string(d["compression"].GetString());	
+				subscribe_args->compression=std::string(d["compression"].GetString());	
 
-			topic_subscirbtions_[topic_name]->subscribe(&subscribe_args);
+			topic_subscirbtions_[topic_name]->subscribe(subscribe_args);
 
 		
 		}
 
 		bool Subscribe::publish()
 		{
-			  			std::cout <<"Subscribe::publish()....map sizea";
-			  			std::cout <<topic_subscirbtions_.size()<<std::endl;
+			// std::cout <<"Subscribe::publish()....map sizea";
+			// std::cout <<topic_subscirbtions_.size()<<std::endl;
+
 			for (std::map<std::string,std::shared_ptr<Subscribtion>>::iterator it= topic_subscirbtions_.begin()
 					;it!=topic_subscirbtions_.end(); ++it ){
 				if (it->second->publish())//is it time to publish
@@ -90,6 +94,8 @@ using namespace fawkes;
 		,client_id_(client_id)
 		{
 			clock_=clock;
+			time_mutex_=new Mutex();
+			sub_list_mutex_= new Mutex();
 		}
 
 		Subscribtion::~Subscribtion(){}
@@ -98,32 +104,45 @@ using namespace fawkes;
 		Subscribtion::subscribe(details *subscirbe_args){
   			
   			std::cout <<"REACHED THE SUBSCRIBTION STAGE WITH "<<topic_<<std::endl;
+
+  			sub_list_mutex_->lock();
 			details_list_.push_back(subscirbe_args);
 
 			details_list_.sort(compare_throttle_rate);
+			sub_list_mutex_->unlock();
 			
+			time_mutex_->lock();
 			last_published_time_=new Time(clock_);
 			last_published_time_->stamp();
 			std::cout << last_published_time_->in_msec()<<std::endl;	
-
+			time_mutex_->unlock();
 		}
 		
 		//Will be called by the publich method in Subscribe  
-		//for each iteration to know whither i should publish this topic or not yet
+		//for each iteration 	to know whither i should publish this topic or not yet
 
 		bool //Return wither its time to publish this topic or not
 		Subscribtion::publish(){
 
+			sub_list_mutex_->lock();
 			int throttle_rate=details_list_.front()->throttle_rate; //should be garantead to be the smallest
+			sub_list_mutex_->unlock();
+
 			fawkes::Time now(clock_);
 			std::cout << last_published_time_->in_msec()<<std::endl;
+			std::cout << throttle_rate <<std::endl;
+			std::cout<< (now.in_msec() - (*last_published_time_).in_msec()) <<std::endl;
 
-			if ((now.in_msec() - (*last_published_time_).in_msec()) >= throttle_rate);
+			if ( (now.in_msec() - (*last_published_time_).in_msec()) >= throttle_rate)
 			{
+				time_mutex_->lock();
 				last_published_time_->stamp();
+				time_mutex_->unlock();
+
 				std::cout << "STAMPED"<<std::endl;	
 				return true;
 			}
+
 			return false;
 		}
 
