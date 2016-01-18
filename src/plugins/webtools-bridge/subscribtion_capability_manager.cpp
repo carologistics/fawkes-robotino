b39e37a87e6b442f692fe32fd6f1e91cc3a76ab9
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2016-01-18 16:36:44 +0100

    webtools-bridge: SubscriptionCapability became pure virtual

--- src/plugins/webtools-bridge/subscribtion_capability_manager.cpp
+++ src/plugins/webtools-bridge/subscribtion_capability_manager.cpp
@@ -10,12 +10,7 @@ using namespace rapidjson;
 //and the processor...but Subscribtion then will contain all the list of any ever subscribed processor 
 // at the very same place..
 
-class SubscribtionCapabilityManager
-: public CapabilityManger
-{
-private:
 
-};
 
 SubscribtionCapabilityManager::SubscribtionCapabilityManager()
 :	CapabilityManager("subscribtion")
@@ -25,8 +20,60 @@ SubscribtionCapabilityManager::SubscribtionCapabilityManager()
 
 SubscribtionCapabilityManager::~SubscribetionCapabilityManager()
 {
-
+	//TODO: check if something need to be changed
 }
+
+void 
+SubscriptionCapabilityManager::subscribe(	std::string bridge_prefix
+										,	Document &d
+										,	std::shared_ptr<WebSession> session)
+{
+	//TODO::MOVE ALL THE TYPE RELATED STUFF TO a proper protocol Class
+	if(!d.HasMember("topic") || !d.HasMember("id")){
+		//throw missing msg feild exception
+	}
+	
+	//TODO:: Make a Protocol Structure in the until
+	if(d.HasMember("topic"))		std::string topic_name 		= 	std::string( d["topic"].GetString() );
+	if(d.HasMember("id")) 			std::string id 				= 	std::string(d["id"].GetString());
+	if(d.HasMember("compression")) 	std::string compression		=	std::string(d["compression"].GetString());	
+	if(d.HasMember("throttle_rate"))unsigned int throttle_rate	=	d["throttle_rate"].GetUint();
+	if(d.HasMember("queue_length")) unsigned int queue_length 	=	d["queue_length"].GetUint();
+	if(d.HasMember("fragment_size"))unsigned int fragment_size 	=	d["fragment_size"].GetUint();
+
+	
+	try{
+		//always creates a new subscriber for that topic with the given Session and parameters
+		//TODO:: pass the pure string arguments or a "protocol" type
+		std::shared_ptr <Subscribtion> subscriber = processores_[match_prefix]-> subscribe(Document &d, session);
+	}catch(Exception){
+		//TODD:: Thorw the appropriate expcetion
+	}
+
+	/*push it to the topic_subscribertion_map maintaing only ONE instance per topic
+	*containing all the clients maped to thier individuale requests*/
+
+	//Is it a new topic? Just push it to the map and activate the subscriber
+
+	//Mutex.lock()
+	if( topic_subscribtion_.find(topic_name) == topic_subscribtion_.end() )
+	{
+		topic_subscribtion_[topic_name] = subscriber;
+		//Activate the listeners or whatever the publishs
+		subscriber.activate();
+	}else{
+		topic_subscribtion_[topic_name].append(subscriber);
+	}
+	//Mutex.unlock();
+}
+
+void
+SubscriptionCapabilityManager::unsubscribe(Document &d
+										,	std::shared_ptr<WebSession> session)
+{
+	
+}
+
 
 bool
 SubscribtionCapabilityManager::register_processor(std::shared_ptr <BridgeProcessor> processor )
@@ -55,6 +102,8 @@ void
 SubscribtionCapabilityManager::handle_message(Document &d
 	,	std::shared_ptr<WebSession> session)
 {
+
+	//TODO::Pass the Dom the a Protocol Class that will have the deserialized types
 	try
 	{
 		std::string msg_op = std::string(d["op"].GetString());
@@ -70,12 +119,12 @@ SubscribtionCapabilityManager::handle_message(Document &d
 		it != processores_.end(); it++)
 	{
 		std::string processor_prefix = it->first;
-		std::size_t found = msg_topic.find(processor_prefix);
+		std::size_t found_at = msg_topic.find(processor_prefix);
 		if(found != std::string::npos)
 		{
 			//allow freedom of 2 characters before the match to account 
 			//for leading "/" ot "//" or and other startting charachters
-			if (found < 1)
+			if (found_at < 1)
 			{
 				if(processor_prefix.length() = match_prefix.length())
 				{
@@ -103,14 +152,12 @@ SubscribtionCapabilityManager::handle_message(Document &d
 	//Go with the bridge name to the operation
 	if(msg_op=="subscribe")
 	{	
-		processores_[match_prefix]->
-			subscribe(Document &d, session);
+		subscribe(match_prefix, Document &d, session);
 	}else 
 
 	if (msg_op=="unsubscribe")
 	}
-		processores_[match_prefix]->
-			unsubscribe(Document &d);
+		unsubscribe(match_prefix, Document &d, session);
 	}
 
 }
