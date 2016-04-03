6dde1578a78bd876db112494ce00150086aaae79
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2016-04-03 21:54:41 +0200

    webtools-bridge: Refactoring of RosBridge Proxy to better keep track of unique session's status by moving the mapping from the processor to a ProxySession object. and gives each proxy_session handlers for its own events.
    Class ProxySession:each instace wrappes around a unique WebSession acting as a proxy to RosBridge Server.

---
+++ src/plugins/webtools-bridge/proxy_session.cpp
@@ -0,0 +1,167 @@
+#include "proxy_session.h"
+
+#include "web_session.h"
+#include <core/threading/mutex.h>
+#include <core/threading/mutex_locker.h>
+#include "callable.h"
+
+
+using namespace fawkes;
+using namespace websocketpp;
+
+ProxySession::ProxySession(std::shared_ptr <WebSession> web_session )
+: EventEmitter()
+, web_session_(web_session)
+, status_("connecting")
+{
+	mutex_=new fawkes::Mutex();
+}
+
+ProxySession::~ProxySession()
+{
+    delete mutex_;
+}
+
+void
+ProxySession::terminate()
+{
+    std::shared_ptr <ProxySession> me= shared_from_this();
+    web_session_->unregister_callback(EventType::TERMINATE , shared_from_this() );
+
+    //emit termination event for all listeners of this session (should delete all ptrs to this instance)
+    me->emitt_event( EventType::TERMINATE );    
+}
+
+
+void
+ProxySession::on_open( websocketpp::connection_hdl hdl , std::shared_ptr<Client> endpoint_ptr )
+{
+    MutexLocker ml(mutex_);
+    web_session_->register_callback(EventType::TERMINATE , shared_from_this() );
+	hdl_ = hdl;
+    endpoint_ptr_=endpoint_ptr;
+	status_="open";
+}
+
+void
+ProxySession::on_fail(websocketpp::connection_hdl hdl)
+{
+	MutexLocker ml(mutex_);
+	status_="failed";
+	terminate();
+	//TODO:do something about the fail
+}
+
+/* It Notifies anyone that registered for the session termination by directly calling their registed callback with the session ptr.
+ */
+void ProxySession::on_close(websocketpp::connection_hdl hdl)
+{   
+ 	MutexLocker ml(mutex_);
+ 	status_= "closed";
+ 	terminate();
+}
+
+void
+ProxySession::on_message(websocketpp::connection_hdl hdl, websocketpp::client<websocketpp::config::asio_client>::message_ptr msg)
+{
+    MutexLocker ml(mutex_);
+    if(status_!="open")
+    {
+        return;
+        //throw
+    }
+    std::string jsonString = msg -> get_payload();  
+    web_session_-> send(jsonString);        
+}
+
+//TODO::catch exceptions and print in the log
+bool 
+ProxySession::send(std::string msg){
+
+	MutexLocker ml(mutex_);
+    if(status_!="open")
+    {
+        return false;
+        //throw
+    }
+
+	websocketpp::lib::error_code ec;
+
+    std::cout << ">TO Proxy::sending message: " << std::endl;
+
+	endpoint_ptr_->send(hdl_, msg, websocketpp::frame::opcode::text, ec);
+
+    if (ec) {
+        std::cout << "> Error sending message: " << ec.message() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+void
+ProxySession::set_id(int id)
+{
+	session_id_=id;
+}
+
+int 
+ProxySession::get_id()
+{
+	return session_id_;
+}
+
+std::string 
+ProxySession::get_status()
+{
+    MutexLocker ml(mutex_);
+	return status_;
+}
+
+websocketpp::connection_hdl
+ProxySession::get_connection_hdl()
+{
+	return hdl_;
+}
+
+std::shared_ptr <WebSession>
+ProxySession::get_web_session()
+{
+	return web_session_;
+}
+
+void  
+ProxySession::callback( EventType event_type , std::shared_ptr <EventEmitter> event_emitter)
+{
+
+    try{
+        //check if the event emitter was a web_session
+        std::shared_ptr <WebSession> session = std::dynamic_pointer_cast<WebSession> (event_emitter);
+        if(session != NULL)
+        {
+            if(event_type == EventType::TERMINATE )
+            {
+                websocketpp::lib::error_code ec;
+            	endpoint_ptr_->close( hdl_ , websocketpp::close::status::going_away, "", ec);
+		        if (ec)
+		        {
+		           // logger_->log_info("ProxySession","cloud not close connection to rosbridge: %s" , ec.message().c_str());
+		            return;
+		        }
+		     }
+        }
+    }
+    catch(Exception &e){
+        //if exception was fired it only means that the casting failed becasue the emitter is not a session
+    }
+} 
+
+void
+ProxySession::emitt_event(EventType event_type)
+{
+	for(it_callables_  = callbacks_ [event_type].begin();
+		it_callables_ != callbacks_ [event_type].end() ; 
+		it_callables_++)
+	{
+		(*it_callables_)->callback(event_type , shared_from_this());
+	}
+}
