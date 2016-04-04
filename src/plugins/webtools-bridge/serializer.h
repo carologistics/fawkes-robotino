b67a7cc4746759df985a083091fc3fcfda0ad4ef
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2016-04-04 18:51:31 +0200

    webtools-bridge:Serialization of  AdvertismentCapabiliy operations

--- src/plugins/webtools-bridge/serializer.h
+++ src/plugins/webtools-bridge/serializer.h
@@ -7,16 +7,19 @@
 class Serializer
 {
 public:
-	static std::string  op_subscribe(std::string prefixed_topic_name 
-		                              , std::string id    
-		                              , std::string compression
-		                              , unsigned int throttle_rate  
-		                              , unsigned int queue_length   
-		                              , unsigned int fragment_size);
+	static std::string  op_subscribe( std::string topic_name 
+		                            , std::string id    
+		                            , std::string compression
+		                            , unsigned int throttle_rate  
+		                            , unsigned int queue_length   
+		                            , unsigned int fragment_size);
 
-	static std::string  op_unsubscribe(std::string prefixed_topic_name 
-		                              , std::string id) ;
+	static std::string  op_unsubscribe(std::string topic_name , std::string id);
 
+	static std::string	op_advertise 	( std::string topic_name , std::string id , std::string type );
+	static std::string	op_unadvertise	( std::string topic_name , std::string id );
+	static std::string	op_publish		( std::string topic_name , std::string id , std::string msg_in_json);
+				
 };
 
 #endif
