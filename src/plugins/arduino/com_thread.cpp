0c48678f3f51771222a66edd3a68ac117105840c
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2016-04-14 18:44:28 +0200

    arduino: add com_thread
    The com_thread is based on the robotino direct_com_thread in order to
    communicate with a serial device via boost::asio.

---
+++ src/plugins/arduino/com_thread.cpp
@@ -0,0 +1,286 @@
+
+/***************************************************************************
+ *  com_thread.cpp - Arduino com thread
+ *
+ *  Created: Thu Sep 11 13:18:00 2014
+ *  Copyright  2011-2014  Tim Niemueller [www.niemueller.de]
+ *                  2016  Nicolas Limpert
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include "com_thread.h"
+#include <baseapp/run.h>
+#include <core/threading/mutex.h>
+#include <core/threading/mutex_locker.h>
+#include <utils/math/angle.h>
+#include <utils/time/wait.h>
+//#include <tf/types.h>
+
+#include <interfaces/BatteryInterface.h>
+#include <interfaces/ArduinoInterface.h>
+//#include <interfaces/IMUInterface.h>
+
+#include <unistd.h>
+
+#include <libudev.h>
+#include <boost/lambda/bind.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/thread/thread.hpp>
+
+
+using namespace fawkes;
+
+/** @class ArduinoComThread "openarduino_com_thread.h"
+ * Thread to communicate with Arduino via OpenArduino API (v1 or v2).
+ * @author Tim Niemueller
+ */
+
+/** Constructor. */
+ArduinoComThread::ArduinoComThread(std::string &cfg_name,
+        std::string &cfg_prefix)
+: Thread("ArduinoComThread", Thread::OPMODE_WAITFORWAKEUP),
+BlackBoardInterfaceListener("ArduinoThread(%s)", cfg_name.c_str()),
+BlockedTimingAspect(BlockedTimingAspect::WAKEUP_HOOK_WORLDSTATE),
+serial_(io_service_), deadline_(io_service_)
+{
+    data_mutex_ = new Mutex();
+    new_data_ = false;
+    cfg_prefix_ = cfg_prefix;
+    cfg_name_ = cfg_name;
+}
+
+/** Destructor. */
+ArduinoComThread::~ArduinoComThread()
+{
+}
+
+void
+ArduinoComThread::init()
+{
+    last_seqnum_ = 0;
+    time_wait_ = new TimeWait(clock, cfg_sensor_update_cycle_time_ * 1000);
+
+    // -------------------------------------------------------------------------- //
+
+    try {
+        cfg_device_ = config->get_string("/arduino/device");
+    } catch (Exception &e) {
+    }
+
+    arduino_if =
+            blackboard->open_for_writing<ArduinoInterface>("Arduino", cfg_name_.c_str());
+
+    deadline_.expires_at(boost::posix_time::pos_infin);
+
+    open_device();
+
+    opened_ = true;
+    open_tries_ = 0;
+
+    blackboard->register_listener(this);
+}
+
+void
+ArduinoComThread::finalize()
+{
+    delete time_wait_;
+}
+
+void
+ArduinoComThread::loop()
+{
+    time_wait_->mark_start();
+
+    if (opened_) {
+        while (!arduino_if->msgq_empty()) {
+            if (arduino_if->msgq_first_is<ArduinoInterface::MoveUpwardsMessage>()) {
+                ArduinoInterface::MoveUpwardsMessage *msg = arduino_if->msgq_first(msg);
+
+                ArduinoComMessage req;
+                req.add_command(ArduinoComMessage::CMD_STEP_UP);
+                req.set_num_steps(msg->num_mm());
+                send_and_recv(req);
+            } else if (arduino_if->msgq_first_is<ArduinoInterface::MoveDownwardsMessage>()) {
+                ArduinoInterface::MoveDownwardsMessage *msg = arduino_if->msgq_first(msg);
+
+                ArduinoComMessage req;
+                req.add_command(ArduinoComMessage::CMD_STEP_DOWN);
+                req.set_num_steps(msg->num_mm());
+                send_and_recv(req);
+            }
+            arduino_if->msgq_pop();
+        }
+
+    } else {
+        try {
+            open_device();
+            opened_ = true;
+            logger->log_info(name(), "Connection re-established after %u tries", open_tries_ + 1);
+        } catch (Exception &e) {
+            open_tries_ += 1;
+            if (open_tries_ >= (1000 / cfg_sensor_update_cycle_time_)) {
+                logger->log_error(name(), "Connection problem to base persists");
+                open_tries_ = 0;
+            }
+        }
+    }
+
+    time_wait_->wait();
+}
+
+bool
+ArduinoComThread::is_connected()
+{
+    return serial_.is_open();
+}
+
+void
+ArduinoComThread::open_device()
+{
+    logger->log_debug(name(), "Open device");
+    try {
+        input_buffer_.consume(input_buffer_.size());
+
+        boost::mutex::scoped_lock lock(io_mutex_);
+        boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+
+        serial_.open(cfg_device_);
+
+        boost::asio::serial_port::baud_rate BAUD(115200);
+        boost::asio::serial_port::flow_control FLOW(boost::asio::serial_port::flow_control::none);
+        boost::asio::serial_port::parity PARITY(boost::asio::serial_port::parity::none);
+        boost::asio::serial_port::stop_bits STOP(boost::asio::serial_port::stop_bits::one);
+        boost::asio::serial_port::character_size thecsize(boost::asio::serial_port::character_size(8U));
+
+        serial_.set_option(BAUD);
+        serial_.set_option(FLOW);
+        serial_.set_option(PARITY);
+        serial_.set_option(STOP);
+        serial_.set_option(thecsize);
+
+    } catch (boost::system::system_error &e) {
+        throw Exception("Arduino failed I/O: %s", e.what());
+    }
+}
+
+void
+ArduinoComThread::close_device()
+{
+    boost::mutex::scoped_lock lock(io_mutex_);
+    serial_.cancel();
+    serial_.close();
+}
+
+void
+ArduinoComThread::flush_device()
+{
+    if (serial_.is_open()) {
+        try {
+            boost::system::error_code ec = boost::asio::error::would_block;
+            size_t bytes_read = 0;
+            do {
+                ec = boost::asio::error::would_block;
+                bytes_read = 0;
+
+                deadline_.expires_from_now(boost::posix_time::milliseconds(200));
+                boost::asio::async_read(serial_, input_buffer_,
+                        boost::asio::transfer_at_least(1),
+                        (boost::lambda::var(ec) = boost::lambda::_1,
+                        boost::lambda::var(bytes_read) = boost::lambda::_2));
+
+                do io_service_.run_one(); while (ec == boost::asio::error::would_block);
+
+                if (bytes_read > 0) {
+                    logger->log_warn(name(), "Flushing %zu bytes\n", bytes_read);
+                }
+
+            } while (bytes_read > 0);
+            deadline_.expires_from_now(boost::posix_time::pos_infin);
+        } catch (boost::system::system_error &e) {
+            // ignore, just assume done, if there really is an error we'll
+            // catch it later on
+        }
+    }
+}
+
+void
+ArduinoComThread::send_message(ArduinoComMessage &msg)
+{
+    boost::mutex::scoped_lock lock(io_mutex_);
+    boost::asio::write(serial_, boost::asio::const_buffers_1(msg.buffer()));
+}
+
+std::shared_ptr<ArduinoComMessage>
+ArduinoComThread::send_and_recv(ArduinoComMessage &msg)
+{
+    boost::mutex::scoped_lock lock(io_mutex_);
+    boost::asio::write(serial_, boost::asio::const_buffers_1(msg.buffer()));
+    return NULL;
+}
+
+/// @cond INTERNAL
+
+class match_char
+{
+public:
+
+    explicit match_char(char c) : c_(c)
+    {
+    }
+
+    template <typename Iterator>
+    std::pair<Iterator, bool> operator()(
+            Iterator begin, Iterator end) const
+    {
+        Iterator i = begin;
+        while (i != end) {
+            //            std::cout << "compare: " << (int) c_ << ": " << c_ << " with: " << (int) *i << std::endl;
+            if (c_ == *i++)
+                return std::make_pair(i, true);
+        }
+        return std::make_pair(i, false);
+    }
+
+private:
+    char c_;
+};
+
+/// @endcond
+
+void
+ArduinoComThread::read_packet()
+{
+    boost::system::error_code ec = boost::asio::error::would_block;
+    size_t bytes_read = 0;
+
+    deadline_.expires_from_now(boost::posix_time::milliseconds(200));
+    boost::asio::async_read_until(serial_, input_buffer_, "\r\n",
+            (boost::lambda::var(ec) = boost::lambda::_1,
+            boost::lambda::var(bytes_read) = boost::lambda::_2));
+
+    do io_service_.run_one(); while (ec == boost::asio::error::would_block);
+
+
+    if (ec) {
+        if (ec.value() == boost::system::errc::operation_canceled) {
+        }
+    }
+
+    // Read all potential junk before the start header
+    if (bytes_read > 1) {
+        logger->log_warn(name(), "Read junk off line");
+    }
+    input_buffer_.consume(bytes_read - 1);
+}
---
+++ src/plugins/arduino/com_thread.h
@@ -0,0 +1,126 @@
+/***************************************************************************
+ *  direct_com_thread.h - Arduino com thread for direct communication
+ *
+ *  Created: Mon Apr 04 11:48:36 2016
+ *  Copyright  2011-2016  Tim Niemueller [www.niemueller.de]
+ *                  2016  Nicolas Limpert
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#ifndef __PLUGINS_ARDUINO_COM_THREAD_H_
+#define __PLUGINS_ARDUINO_COM_THREAD_H_
+
+#include "com_message.h"
+#include <core/threading/thread.h>
+#include <aspect/logging.h>
+#include <aspect/clock.h>
+#include <aspect/configurable.h>
+#include <aspect/blackboard.h>
+#include <blackboard/interface_listener.h>
+#include <aspect/blocked_timing.h>
+
+#include <utils/time/time.h>
+
+#include <memory>
+#include <boost/asio.hpp>
+#include <boost/thread/mutex.hpp>
+
+class ArduinoComMessage;
+
+namespace fawkes {
+    class Mutex;
+    class Clock;
+    class TimeWait;
+
+    class BatteryInterface;
+    class ArduinoInterface;
+}
+
+class ArduinoComThread
+: public fawkes::Thread,
+public fawkes::LoggingAspect,
+public fawkes::ConfigurableAspect,
+public fawkes::ClockAspect,
+public fawkes::BlackBoardAspect,
+public fawkes::BlackBoardInterfaceListener,
+public fawkes::BlockedTimingAspect
+{
+public:
+
+    ArduinoComThread();
+    ArduinoComThread(std::string &cfg_name, std::string &cfg_prefix);
+    virtual ~ArduinoComThread();
+
+    virtual void init();
+    //	virtual void once();
+    virtual void loop();
+    virtual void finalize();
+
+    virtual bool is_connected();
+
+    /** Stub to see name in backtrace for easier debugging. @see Thread::run() */
+protected:
+
+    virtual void run() {
+        Thread::run();
+    }
+
+private:
+    void open_device();
+    void close_device();
+    void flush_device();
+
+    void read_packet();
+    void send_message(ArduinoComMessage &msg);
+    std::shared_ptr<ArduinoComMessage>
+    send_and_recv(ArduinoComMessage &msg);
+    void process_message(ArduinoComMessage::pointer m);
+
+private:
+    std::string cfg_device_;
+    std::string cfg_hostname_;
+    std::string cfg_prefix_;
+    std::string cfg_name_;
+    bool cfg_enable_gyro_;
+    unsigned int cfg_sensor_update_cycle_time_;
+    bool cfg_gripper_enabled_;
+
+    bool opened_;
+    unsigned int open_tries_;
+
+    fawkes::TimeWait *time_wait_;
+    unsigned int last_seqnum_;
+
+    boost::asio::io_service io_service_;
+    boost::asio::serial_port serial_;
+    boost::asio::deadline_timer deadline_;
+    boost::asio::streambuf input_buffer_;
+    boost::mutex io_mutex_;
+    fawkes::ArduinoInterface *arduino_if;
+
+protected:
+    /** Mutex to protect data_. Lock whenever accessing it. */
+    fawkes::Mutex *data_mutex_;
+    /** Data struct that must be updated whenever new data is available. */
+    //        SensorData        data_;
+    /** Flag to indicate new data, set to true if data_ is modified. */
+    bool new_data_;
+
+
+};
+
+
+#endif
+
