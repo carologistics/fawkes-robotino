d2109b49509a7b4042f7b13aa661c713bc20e8e6
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2016-04-14 18:13:22 +0200

    arduino: add initial plugin structure
    This plugin is supposed to handle communication with an Arduino by
    making use of the Adafruit Stepper Shield V2.
    It supports sending the number of desired mm to let the gripper move to
    a desired relative position.

---
+++ src/plugins/arduino/arduino_plugin.cpp
@@ -0,0 +1,56 @@
+
+/***************************************************************************
+ *  robotino_plugin.cpp - Plugin for Arduino platform support
+ *
+ *  Created: Sun Nov 13 15:31:57 2011
+ *  Copyright  2011  Tim Niemueller [www.niemueller.de]
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <core/plugin.h>
+
+#include "com_thread.h"
+
+using namespace fawkes;
+
+/** Plugin to provide Arduino platform support for Fawkes.
+ * @author Tim Niemueller, Nicolas Limpert
+ */
+class ArduinoPlugin : public fawkes::Plugin
+{
+public:
+
+    /** Constructor.
+     * @param config Fawkes configuration
+     */
+    ArduinoPlugin(Configuration *config)
+    : Plugin(config)
+    {
+        std::string prefix = "/arduino";
+
+        std::string cfg_name = prefix.substr(1, prefix.length());
+        std::string cfg_prefix = prefix + "/";
+
+        cfg_name = cfg_name.substr(0, cfg_name.find("/"));
+
+        ArduinoComThread *com_thread = new ArduinoComThread(cfg_name, cfg_prefix);
+
+        thread_list.push_back(com_thread);
+    }
+};
+
+PLUGIN_DESCRIPTION("Arduino platform support")
+EXPORT_PLUGIN(ArduinoPlugin)
