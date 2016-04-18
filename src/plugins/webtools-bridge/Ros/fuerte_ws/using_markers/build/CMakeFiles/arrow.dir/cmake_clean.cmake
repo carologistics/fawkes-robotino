48ba28de64f3bcc33f17c62ff265d4f1f5091a83
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2016-04-18 06:44:29 +0200

    webtools-bridge: organizing Ros nodes and Js in a work space folder
    Arrow_marker corrected: frame_id and publishing loop

---
+++ src/plugins/webtools-bridge/Ros/fuerte_ws/using_markers/build/CMakeFiles/arrow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/arrow.dir/src/arrow.o"
+  "../bin/arrow.pdb"
+  "../bin/arrow"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/arrow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
