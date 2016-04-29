8ce3f0de3f597da1f3652e2e627e4f9d9ee8ed21
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2016-04-29 13:46:06 +0200

    webtools-bridge : webdev, json corrected traversing and each fact in differnt line

--- src/plugins/webtools-bridge/webdev/WebMonitor/simple_monitor.js
+++ src/plugins/webtools-bridge/webdev/WebMonitor/simple_monitor.js
@@ -3,7 +3,7 @@ function simple_monitor(){
 
 	var div_ids=0;
 
-	$("body").append("<div id=monitor_div  class=wedgit container smplmntr>  </dev>");
+	$("body").append("<div id=monitor_div  class=wedgit container>  </dev>");
 	$("#monitor_div").append("<button class=smplmntr > subscribe </button>");
 	$("#monitor_div").append("<input  type='text' class=smplmntr > </input>");
 
@@ -22,31 +22,36 @@ function simple_monitor(){
 			    throttle_rate:1000,
 		  	});
 		  	listener.div_index=div_ids;
+
 		  	listener.subscribe(function(message) {
-		  		var parsed_obj=JSON.parse(message.data);
+		  		var parsed_obj=message;
 
 		  		var dev_id="#monitor_div_"+this.div_index;
-		  		var dev_content;
+		  		$(dev_id).empty();
 
 		  		for (var key in parsed_obj) 
 		  		{
-		  			if(parsed_obj[key] === Array)
+		  			var dev_content = key + " : ";
+		  			if(parsed_obj[key].constructor === Array)
 		  			{
-		  				dev_content+= key + ":";
+		  				
 		  				for( var fact in parsed_obj[key])
 		  				{
-		  					dev_content+= JSON.stringify(fact, null, 5) + "<br>"; 
+		  					dev_content+= JSON.stringify(parsed_obj[key][fact], null, 5) + "<br>"; 
 		  				}
 
 		  			}
 		  			else
 		  			{
-		  				dev_content+= key +":"+ parsed_obj[key] + "<br>"; 
+		  				dev_content+= parsed_obj[key] + "<br>"; 
 		  			}
-
+		  			
+		  			dev_content+=  "<br>"; 
+		  			
+		  			$(dev_id).append(dev_content);
 		  		}
 
-		  		$(dev_name).text(dev_content);
+		  		
 		  	});
 
 		  	div_ids++;
