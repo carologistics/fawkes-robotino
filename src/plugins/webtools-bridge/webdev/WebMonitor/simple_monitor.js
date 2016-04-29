8fb497eaa44d910f3ae7601561447be859e799d1
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2016-04-29 12:04:56 +0200

    webtools-bridge: WebDev, simple  monitor refactored ids and class

--- src/plugins/webtools-bridge/webdev/WebMonitor/simple_monitor.js
+++ src/plugins/webtools-bridge/webdev/WebMonitor/simple_monitor.js
@@ -2,26 +2,51 @@ function simple_monitor(){
 	init();
 
 	var div_ids=0;
-	$("body").append("<button id=monitor_btn  class=smplmntr > subscribe </button>");
-	$("body").append("<input id=monitor_inp type='text' class=smplmntr > </input>");
 
-	$(document).ready(function(){
-	    $("#monitor_btn").click(function() {
-	    	var topic_name = $("#monitor_inp").val();
+	$("body").append("<div id=monitor_div  class=wedgit container smplmntr>  </dev>");
+	$("#monitor_div").append("<button class=smplmntr > subscribe </button>");
+	$("#monitor_div").append("<input  type='text' class=smplmntr > </input>");
 
-	    	$("body").append("<div id=d_"+div_ids+" class=smplmntr > </div>");
+	$(document).ready(function()
+	{
+	    $("#monitor_div").find(":button") .click(function() 
+	    {
+	    	var topic_name = $("#monitor_div").find(":text").val(); 
 
-	    	
+	    	$("#monitor_div").append("<div id=monitor_div_"+div_ids+" class=smplmntr > </div>");
+
 	    	var listener = new ROSLIB.Topic({
 			    ros : ros,
 			    name : topic_name,
 			    messageType : 'mm',
 			    throttle_rate:1000,
 		  	});
-		  	listener.div_id=div_ids;
+		  	listener.div_index=div_ids;
 		  	listener.subscribe(function(message) {
-		  		var dev_name="#d_"+this.div_id;
-		  		$(dev_name).text(JSON.stringify(message, null, 5));
+		  		var parsed_obj=JSON.parse(message.data);
+
+		  		var dev_id="#monitor_div_"+this.div_index;
+		  		var dev_content;
+
+		  		for (var key in parsed_obj) 
+		  		{
+		  			if(parsed_obj[key] === Array)
+		  			{
+		  				dev_content+= key + ":";
+		  				for( var fact in parsed_obj[key])
+		  				{
+		  					dev_content+= JSON.stringify(fact, null, 5) + "<br>"; 
+		  				}
+
+		  			}
+		  			else
+		  			{
+		  				dev_content+= key +":"+ parsed_obj[key] + "<br>"; 
+		  			}
+
+		  		}
+
+		  		$(dev_name).text(dev_content);
 		  	});
 
 		  	div_ids++;
