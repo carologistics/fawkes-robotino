7a4cdb0e6fbf737b8e3600d9ea26a0684f27c874
Tobias Neumann <t.neumann@fh-aachen.de>
2016-05-30 11:07:43 +0200

    skills/conveyor_align: adapt to new conveyor_pose skill

--- src/lua/skills/robotino/conveyor_align.lua
+++ src/lua/skills/robotino/conveyor_align.lua
@@ -25,8 +25,8 @@ fsm                = SkillHSM:new{name=name, start="INIT", debug=false}
 depends_skills     = {"motor_move","approach_mps", "ax12gripper"}
 depends_interfaces = { 
    {v = "motor", type = "MotorInterface", id="Robotino" },
-   {v = "conveyor_0", type = "Position3DInterface"},
-   {v = "gripper_if", type = "AX12GripperInterface", id="Gripper AX12"},
+   {v = "if_conveyor", type = "Position3DInterface", id="/conveyor-pose"},
+   {v = "if_gripper", type = "AX12GripperInterface", id="Gripper AX12"},
 }
 
 documentation      = [==[aligns the robot orthogonal to the conveyor by using the
@@ -35,81 +35,127 @@ conveyor vision
 
 -- Initialize as skill module
 skillenv.skill_module(_M)
+local tfm = require("fawkes.tfutils")
 
 local TOLERANCE_Y = 0.005
 local TOLERANCE_Z = 0.002
 local MAX_TRIES = 4
-local Z_DEST_POS_WITH_PUCK = 0.002
-local Z_DEST_POS_WITHOUT_PUCK = 0.008
+local X_DEST_POS_PRE = 0.235
+local X_DEST_POS = 0.19
+local Z_DEST_POS_WITH_PUCK = 0.032
+local Z_DEST_POS_WITHOUT_PUCK = 0.03
 local Z_DEST_POS = Z_DEST_POS_WITH_PUCK
 local Z_DIVISOR = 2
+local cfg_frame_ = "mount_profile"
 
 function no_writer()
-   return not conveyor_0:has_writer()
+   return not if_conveyor:has_writer()
+end
+
+function see_conveyor()
+   return if_conveyor:visibility_history() > 0
 end
 
 function tolerance_y_not_ok()
-   return not (math.abs(conveyor_0:translation(1)) <= TOLERANCE_Y)
+   return not (math.abs(if_conveyor:translation(1)) <= TOLERANCE_Y)
 end
 
 function tolerance_z_not_ok()
-   return not (math.abs(conveyor_0:translation(2) - Z_DEST_POS) <= TOLERANCE_Z)
+   return not (math.abs(if_conveyor:translation(2) - Z_DEST_POS) <= TOLERANCE_Z)
+end
+
+function check_for_second_try(self)
+   return self.fsm.vars.counter >= 0
+end
+
+function pose_offset(self)
+   if if_gripper:is_holds_puck() then
+      Z_DEST_POS = Z_DEST_POS_WITH_PUCK
+   else
+      Z_DEST_POS = Z_DEST_POS_WITHOUT_PUCK
+   end
+
+   local from = { x = if_conveyor:translation(0),
+                  y = if_conveyor:translation(1),
+                  z = if_conveyor:translation(2),
+                  ori = { x = if_conveyor:rotation(0),
+                          y = if_conveyor:rotation(1),
+                          z = if_conveyor:rotation(2),
+                          w = if_conveyor:rotation(3),
+                        }
+                }
+   local cp = tfm.transform6D(from, if_conveyor:frame(), cfg_frame_)
+
+   -- TODO check nil
+
+   local ori = fawkes.tf.get_yaw( fawkes.tf.Quaternion:new(cp.ori.x, cp.ori.y, cp.ori.z, cp.ori.w))
+   print("ori want: ".. ori)
+   if math.abs(ori) > 0.7 then
+      ori = 0
+   end
+
+   return { x = cp.x,
+            y = cp.y -0.025,
+            z = cp.z - Z_DEST_POS,
+            ori = ori
+          }
+end
+
+function pose_des_pre(self)
+   local pose = pose_offset(self)
+   pose.x = pose.x - X_DEST_POS_PRE
+   return pose
+end
+
+function pose_des(self)
+   local pose = pose_offset(self)
+   pose.x = pose.x - X_DEST_POS
+   return pose
 end
 
 fsm:define_states{ export_to=_M,
    closure={MAX_TRIES=MAX_TRIES, Z_DEST_POS=Z_DEST_POS},
    {"INIT", JumpState},
-   {"APPROACH_MPS", SkillJumpState, skills={{approach_mps}}, final_to="DRIVE_YZ", fail_to="FAILED"},
-   {"DRIVE_YZ", SkillJumpState, skills={{motor_move}, {ax12gripper}}, final_to="SETTLE", fail_to="FAILED"},
-   {"DRIVE_Z", SkillJumpState, skills={{ax12gripper}}, final_to="SETTLE_Z", fail_to="FAILED"},
-   {"SETTLE", JumpState},
-   {"SETTLE_Z", JumpState},
-   {"CHECK", JumpState},
-   {"CHECK_Z", JumpState},
+   {"APPROACH", SkillJumpState, skills={{approach_mps}}, final_to="DRIVE_PRE", fail_to="FAILED"},
+   {"SETTLE_PRE", JumpState},
+   {"DRIVE_PRE", SkillJumpState, skills={{motor_move}, {ax12gripper}}, final_to="DECIDE_TRY", fail_to="FAILED"},
+   {"DECIDE_TRY", JumpState},
+   {"ROTATE", SkillJumpState, skills={{motor_move}}, final_to="FINAL", fail_to="FAILED"},
+   {"DRIVE_FINAL", SkillJumpState, skills={{motor_move}, {ax12gripper}}, final_to="FINAL", fail_to="FAILED"},
 }
 
 fsm:add_transitions{
    {"INIT", "FAILED", cond=no_writer, desc="no conveyor vision"},
-   {"INIT", "APPROACH_MPS", cond=true},
-   {"SETTLE", "CHECK", timeout=0.5},
-   {"SETTLE_Z", "CHECK_Z", timeout=0.5},
-   {"CHECK", "FAILED", cond="vars.counter > MAX_TRIES", desc="max tries reached"},
-   {"CHECK", "DRIVE_YZ", cond=tolerance_y_not_ok, desc="tolerance not ok, align y distance"},
-   {"CHECK", "SETTLE_Z", cond=true},
-   {"CHECK_Z", "DRIVE_Z", cond=tolerance_z_not_ok, desc="tolerance not ok, align z distance"},
-   {"CHECK_Z", "FINAL", cond=true},
+   {"INIT", "FAILED", timeout=2, desc="no VIS HIST on conveyor vision"},
+   {"INIT", "APPROACH", cond=see_conveyor},
+   {"SETTLE_PRE", "DRIVE_PRE", cond=true},
+   {"DECIDE_TRY", "SETTLE_PRE", cond=check_for_second_try, desc="do 2. alignmend"},
+   {"DECIDE_TRY", "FINAL", cond=true, desc="pre aligned"},
 }
 
 function INIT:init()
-   self.fsm.vars.counter = 1
+   self.fsm.vars.counter = 0
 end
 
-function APPROACH_MPS:init()
+function APPROACH:init()
    self.args["approach_mps"] = { x = 0.2 }
 end
 
-function DRIVE_YZ:init()
-   if gripper_if:is_holds_puck() then
-      Z_DEST_POS = Z_DEST_POS_WITH_PUCK
-   else
-      Z_DEST_POS = Z_DEST_POS_WITHOUT_PUCK
-   end
-   self.args["motor_move"] = {y = conveyor_0:translation(1), tolerance = { x=0.002, y=0.002, ori=0.01 }}
+function ROTATE:init()
+   local pose = pose_des_pre(self)
+   self.args["motor_move"] = {ori = pose.ori, tolerance = { x=0.002, y=0.002, ori=0.01 }, vel_rot = 0.2}
+end
+
+function DECIDE_TRY:init()
+   self.fsm.vars.counter = self.fsm.vars.counter - 1
+end
+
+function DRIVE_PRE:init()
+   local pose = pose_des_pre(self)
+
+   self.args["motor_move"] = {x = pose.x, y = pose.y, tolerance = { x=0.002, y=0.002, ori=0.01 }}
    self.args["ax12gripper"].command = "RELGOTOZ"
    if tolerance_z_not_ok() then
-      self.args["ax12gripper"].z_position = ((conveyor_0:translation(2) - Z_DEST_POS) * 1000) / Z_DIVISOR
+      self.args["ax12gripper"].z_position = (pose.z * 1000) / Z_DIVISOR
    end
-end
-
-function DRIVE_Z:init()
-   self.args["ax12gripper"].command = "RELGOTOZ"
-   if conveyor_0:translation(2) - Z_DEST_POS < 0 then
-      self.args["ax12gripper"].z_position = -1
-   else
-      self.args["ax12gripper"].z_position = 1
-   end
-end
-
-function CHECK:init()
-   self.fsm.vars.counter = self.fsm.vars.counter + 1
 end
