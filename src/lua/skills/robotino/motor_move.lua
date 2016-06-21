18a8c6a80d6832060f7b21f151a7170651793a58
Victor Mataré <matare@lih.rwth-aachen.de>
2016-06-21 15:57:05 +0200

    skills:motor_move: reduce V_MIN_CAM.ori & TOLERANCE_CAM.ori

--- src/lua/skills/robotino/motor_move.lua
+++ src/lua/skills/robotino/motor_move.lua
@@ -59,9 +59,9 @@ documentation      = [==[Move on a (kind of) straight line relative to the given
 -- Tunables
 local V_MAX =         { x=0.35, y=0.35, ori=1.4 }    -- ultimate limit
 local V_MAX_CAM =     { x=0.06, y=0.06, ori=0.6 }
-local V_MIN =         { x=0.01, y=0.01, ori=0.06 }   -- below the motor won't even start
+local V_MIN =         { x=0.01, y=0.01, ori=0.07 }   -- below the motor won't even start
 local TOLERANCE =     { x=0.02, y=0.02, ori=0.025 } -- accuracy
-local TOLERANCE_CAM = { x=0.005, y=0.005, ori=0.005 }
+local TOLERANCE_CAM = { x=0.005, y=0.005, ori=0.01 }
 local D_DECEL =       { x=0.035, y=0.035, ori=0.15 }    -- deceleration distance
 local ACCEL =         { x=0.06, y=0.06, ori=0.21 }   -- accelerate by this factor every loop
 
