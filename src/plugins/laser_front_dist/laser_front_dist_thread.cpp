68df982d9b795ef85bcb9476b122345c47c0fb4d
Frederik Zwilling <zwilling@kbsg.rwth-aachen.de>
2016-06-23 23:26:15 +0200

    laser-front-dist: plugin to get distance to mps with laser
    To robustly drive the right distance to the mps, we could use the laser
    sensor. Because single lines have an error about 0.5cm in gripper
    distance to the mps, we need to average over multiple beams in the
    front.

---
+++ src/plugins/laser_front_dist/laser_front_dist_thread.cpp
@@ -0,0 +1,96 @@
+
+/***************************************************************************
+ *  laser_front_dist_thread.cpp - laser_front_dist
+ *
+ *  Plugin created: Thu Jun 23 22:35:32 2016
+
+ *  Copyright  2016  Frederik Zwilling
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include "laser_front_dist_thread.h"
+#include <interfaces/Laser360Interface.h>
+#include <interfaces/Position3DInterface.h>
+#include <math.h>
+
+using namespace fawkes;
+
+/** @class LaserFrontDistThread 'laser_front_dist_thread.h' 
+ * Calculates distance to object in front with average of laser beams in front
+ * @author Frederik Zwilling
+ */
+
+LaserFrontDistThread::LaserFrontDistThread()
+ : Thread("LaserFrontDistThread", Thread::OPMODE_WAITFORWAKEUP),
+             BlockedTimingAspect(BlockedTimingAspect::WAKEUP_HOOK_SKILL) 
+{
+}
+
+void
+LaserFrontDistThread::init()
+{
+	//read config values
+	beams_used_ = config->get_int("plugins/laser-front-dist/number_beams_used");
+	//open interfaces
+	if_laser_ = blackboard->open_for_reading<Laser360Interface>(config->get_string("plugins/laser-front-dist/input_laser_interface").c_str());
+	if_result_ = blackboard->open_for_writing<Position3DInterface>(config->get_string("plugins/laser-front-dist/output_result_interface").c_str());
+}
+
+void
+LaserFrontDistThread::loop()
+{
+	//calculate average:
+	if_laser_->read();
+	float sum = 0.0;
+	for(int i = 0; i < beams_used_/2 + beams_used_%2; i++)
+	{
+		if(!isnormal(if_laser_->distances(i)))
+		{
+			//this is invalid
+			if_result_->set_visibility_history(-1);
+			if_result_->write();
+			return;
+		}
+		sum += if_laser_->distances(i);
+	}
+	for(int i = 0; i < beams_used_/2; i++)
+	{
+		if(!isnormal(if_laser_->distances(359-i)))
+		{
+			//this is invalid
+			if_result_->set_visibility_history(-1);
+			if_result_->write();
+			return;
+		}
+		sum += if_laser_->distances(360-i);
+	}
+	float average = sum / (float) beams_used_;
+	frame_ = if_laser_->frame();
+
+	//write result
+	if_result_->set_visibility_history(1);
+	if_result_->set_translation(1, average);
+	if_result_->set_frame(frame_.c_str());
+	if_result_->write();
+}
+
+void
+LaserFrontDistThread::finalize()
+{
+	blackboard->close( if_laser_ );
+	blackboard->close( if_result_ );
+}
+
