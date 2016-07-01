567ccb85f80d72bea430e7a5cc4911be6e562521
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2016-07-01 10:03:42 +0200

    mps-laser-gen: generate fake laser data for MPS
    Use the localization position and calculate laser data for static MPS
    obstacles.

---
+++ src/plugins/mps-laser-gen/mps-laser-gen_plugin.cpp
@@ -0,0 +1,48 @@
+
+/***************************************************************************
+ *  mps-laser-gen_plugin.cpp - mps-laser-gen
+ *
+ *  Plugin created: Thu Jun 30 21:54:46 2016
+
+ *  Copyright  2016  Tim Niemueller
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <core/plugin.h>
+
+#include "mps-laser-gen_thread.h"
+
+using namespace fawkes;
+
+/* @class Mps-laser-genPlugin "mps-laser-gen_plugin.cpp"
+ * Foo
+ * @author Tim Niemueller
+ */
+class MPSLaserGenPlugin : public fawkes::Plugin
+{
+ public:
+  /* Constructor
+   * @param config Fakwes configuration
+   */
+  MPSLaserGenPlugin(Configuration *config)
+     : Plugin(config)
+  {
+     thread_list.push_back(new MPSLaserGenThread());
+  }
+};
+
+PLUGIN_DESCRIPTION("Foo")
+EXPORT_PLUGIN(MPSLaserGenPlugin)
