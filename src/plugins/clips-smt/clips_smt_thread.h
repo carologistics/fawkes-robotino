e0a1b416e0bdd38b00543ce6b5b65758b5b03982
Igor Bongartz <nicolai.bongartz@rwth-aachen.de>
2017-03-17 17:03:40 +0100

    Clips-smt: Add correct computation of distance from C-ins-in

--- src/plugins/clips-smt/clips_smt_thread.h
+++ src/plugins/clips-smt/clips_smt_thread.h
@@ -100,7 +100,6 @@ class ClipsSmtThread
   z3::expr_vector clips_smt_create_formula();
   z3::expr_vector clips_smt_encoder(std::map<std::string, z3::expr>& variables_pos, std::map<std::string, z3::expr>& variables_d, std::map<std::string, z3::expr>& variables_m);
   z3::check_result clips_smt_solve_formula(std::map<std::string, z3::expr>& variables_pos, std::map<std::string, z3::expr>& variables_d, std::map<std::string, z3::expr>& variables_m,z3::expr_vector formula);
-  void clips_smt_react_on_result(z3::check_result result);
 
   std::map<int ,std::string> actions_robot_1;
   std::map<int ,std::string> actions_robot_2;
