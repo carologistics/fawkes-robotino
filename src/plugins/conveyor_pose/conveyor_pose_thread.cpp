a47b792889117150291a3866d0fcebee52ac0006
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2017-05-06 16:55:27 +0200

    conveyor pose: only send message to realsense switch if writer exists
    If no writer exists, the realsense is not running. In that case, print a
    warning and abort the loop.

--- src/plugins/conveyor_pose/conveyor_pose_thread.cpp
+++ src/plugins/conveyor_pose/conveyor_pose_thread.cpp
@@ -218,6 +218,7 @@ void
 ConveyorPoseThread::loop()
 {
   if_read();
+  realsense_switch_->read();
   //logger->log_debug(name(),"CONVEYOR-POSE 1: Interface read");
   if ( ! pc_in_check() || ! bb_enable_switch_->is_enabled() ) {
     if ( enable_pose_ ) {
@@ -229,10 +230,23 @@ ConveyorPoseThread::loop()
     if ( cfg_pose_close_if_no_new_pointclouds_ ) {
       bb_pose_conditional_close();
     }
-     realsense_switch_->msgq_enqueue(new SwitchInterface::DisableSwitchMessage());
+    if (!bb_enable_switch_->is_enabled()) {
+      if (realsense_switch_->has_writer()) {
+        realsense_switch_->msgq_enqueue(
+          new SwitchInterface::DisableSwitchMessage());
+      } else {
+        logger->log_warn(name(),
+          "Tried to disable realsense, but no writer for interface %s",
+          cfg_bb_realsense_switch_name_.c_str());
+      }
+    }
     return;
   }
-  realsense_switch_->read();
+  if (!realsense_switch_->has_writer()) {
+    logger->log_error(name(), "No writer for realsense switch %s",
+      cfg_bb_realsense_switch_name_.c_str());
+    return;
+  }
   if (!realsense_switch_->is_enabled()) {
     realsense_switch_->msgq_enqueue(new SwitchInterface::EnableSwitchMessage());
     start_waiting();
