f116fb01ebe900a36079fb64649d03e590bd8a7b
Carsten Stoffels <carsten.stoffels@rwth-aachen.de>
2017-05-07 13:17:25 +0200

    plugins/markerless_recognition/markerless_rec_thread:  added readImage()

--- src/plugins/markerless_recognition/markerless_rec_thread.cpp
+++ src/plugins/markerless_recognition/markerless_rec_thread.cpp
@@ -2,7 +2,9 @@
 
 
 #include "markerless_rec_thread.h"
-
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 #include <tf/types.h>
 #include <interfaces/MPSRecognitionInterface.h>
 
@@ -47,4 +49,35 @@ MarkerlessRecognitionThread::loop()
     logger->log_warn(name(), "No writer for pose interface");
   }
 */
+}
+
+void MarkerlessRecognitionThrad::readImage(){ 
+	 /* 
+	Mat image;
+  	image = imread("/home/casto/Carologistic/OpenTC/Training/ExperimentalTestData/BS_Depth_2682.jpg");   // Read the filemi, dev/video_tag should be the path for the RealSense
+
+	// Show it for testing reasons
+	namedWindow("image", CV_WINDOW_AUTOSIZE);
+    	imshow("image", grey);
+	*/
+	    // init firevision camera
+   	 // CAM swapping not working (??)
+        /*if(fv_cam != NULL){
+        // free the camera
+        fv_cam->stop();
+        fv_cam->flush();
+        fv_cam->dispose_buffer();
+        fv_cam->close();
+        delete fv_cam;
+        fv_cam = NULL;
+    }
+    if(fv_cam == NULL){
+      std::string connection = this->config->get_string((prefix + "camera").c_str());
+        fv_cam = vision_master->register_for_camera(connection.c_str(), this);
+        fv_cam->start();
+        fv_cam->open();
+        this->img_width = fv_cam->pixel_width();
+        this->img_height = fv_cam->pixel_height();
+    }*/
+
 }
