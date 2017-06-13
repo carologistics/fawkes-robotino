cc64d995ca98cd381211d146190978c198d76b7c
Carsten Stoffels <carsten.stoffels@rwth-aachen.de>
2017-06-13 18:29:05 +0200

    plugin/markerless_recognition: Added fv_cam ready() check in loop
    Changed Message handling and deleted not needed messages
    
    Deleted unneces

--- src/plugins/markerless_recognition/markerless_rec_thread.cpp
+++ src/plugins/markerless_recognition/markerless_rec_thread.cpp
@@ -36,60 +36,21 @@ MarkerlessRecognitionThread::MarkerlessRecognitionThread()
     ipl = NULL;
 
 }
-/*
-void MarkerlessRecognitionThread::clear_data()
-{
-}
-*/
-void
-MarkerlessRecognitionThread::finalize()
-{
- blackboard->close(mps_rec_if_);
-/*  delete fv_cam;
-  fv_cam = NULL;
-  delete shm_buffer;
-  shm_buffer= NULL;
-  image_buffer = NULL;
-  ipl = NULL;*/
+
+void MarkerlessRecognitionThread::finalize() {
+ 	blackboard->close(mps_rec_if_);
+        delete fv_cam;
+  	fv_cam = NULL;
+  	delete shm_buffer;
+  	shm_buffer= NULL;
+  	image_buffer = NULL;
+ 	ipl = NULL;
 }
 
-int MarkerlessRecognitionThread::checkProbability(Probability prob){
-	for(int i = 0; i<5; ++i){
-		if(prob.p[i]<0) return -1;
-
-	}
-	return 0;
-}
-
-void MarkerlessRecognitionThread::estimate_mps_type(const Probability &prob) {
-	float pmax = 0.;
-	float psec = 0.;
-	MPSType bestfit;
-	for (int i = 0; i < 5; ++i) {
-		if (pmax < prob.p[i]) {
-			psec = pmax;
-			bestfit = (MPSType) i;
-			pmax = prob.p[i];
-		}
-	}
-
-	if(pmax < th_first || psec > th_sec){
-		bestfit = NoStationDetected;
-	}
-	std::cout << "MPS: " << bestfit << std::endl;
-
-	//segfault when trying to write to the blackboard
 	
-//	mps_rec_if_->set_final(true);
-//	mps_rec_if_->set_mpstype(
-//	        (fawkes::MPSRecognitionInterface::MPSType) bestfit);
-//	mps_rec_if_->set_p_correct(pmax);
-	//mps_rec_if_->set_ptot_mpstype(prob.p);
-	//mps_rec_if_->write();
-	
-}
 
 Probability MarkerlessRecognitionThread::recognize_current_pic(const std::string image) {
+	
 	Probability result;
 	my_function evaluate;
 	void *handle;
@@ -154,36 +115,7 @@ void  MarkerlessRecognitionThread::recognize_mps() {
 	//then decides which mps was seen in the set of images
 }
 
-	
-void MarkerlessRecognitionThread::readImage(){ 
- 
-       	
- 	Mat image = imread("/home/casto/Carologistics/OpenTC/Training/ExperimentalTrainingData/BS/BS_Depth_2682.jpg") ; //Read the file , dev/video_tag should be the path for the RealSense
-
-	if(! image.data )                              // Check for invalid input
-   	{
-        	cout <<  "Could not open or find the image" << std::endl ;
-   	}
-	try{ // to show , doesn't work (black screen) 
-    		
-	 	double min;
-        	double max;
-       		cv::minMaxIdx(image, &min, &max);
-        	cv::Mat adjMap;
-        	cv::convertScaleAbs(image, adjMap, 255 / max);
-        	//cv::imshow("Out", adjMap);
-	}
-	catch( cv::Exception& e )
-	{
-    		const char* err_msg = e.what();
-  		std::cout << "exception caught: " << err_msg << std::endl;
-	}
-}
-
-void
-MarkerlessRecognitionThread::init()
-{
-
+void MarkerlessRecognitionThread::init(){
       	home.assign(getenv("HOME"),strlen(getenv("HOME")));
        	recognize_current_pic("/TestData/BS/BS_9.jpg");
 
@@ -254,40 +186,65 @@ void MarkerlessRecognitionThread::setupCamera(){
 }
 
 
-void
-MarkerlessRecognitionThread::loop(){
-/*
-   if(fv_cam == NULL || !fv_cam->ready()){
-        logger->log_info(name(),"Camera not ready");
-	setupCamera();
-      return;
-   }
-*/
+void MarkerlessRecognitionThread::loop(){
+
+   	if(fv_cam == NULL || !fv_cam->ready()){
+        	logger->log_info(name(),"Camera not ready");
+		setupCamera();
+      	return;
+   	}
+
     
-   while ( ! mps_rec_if_->msgq_empty() ) {
-    if ( mps_rec_if_->msgq_first_is<MPSRecognitionInterface::ClearMessage>() ) {
-     	std::cout << "Recieved Clear Message" << std::endl;
-	clear_data();
-    } else if ( mps_rec_if_->msgq_first_is<MPSRecognitionInterface::ComputeMessage>() ) {
-      	std::cout << "Recieved Compute Message" << std::endl;
- 	logger->log_info(name(), "Recognition started");
-	MPSRecognitionInterface::TakeDataMessage *m = mps_rec_if_
-		->msgq_first<MPSRecognitionInterface::TakeDataMessage>();
-	mps_rec_if_->set_msgid(m->id());
-	mps_rec_if_->set_final(false);
-	mps_rec_if_->write();
-	cout << " Start recognize " << std::endl; 
-	recognize_mps();
-    } else if ( mps_rec_if_->msgq_first_is<MPSRecognitionInterface::TakeDataMessage>() ) {
-    	std::cout << "Recieved Take Data Message" << std::endl;
-   	readImage();
-    }	
-    else {
-    	logger->log_warn(name(), "Unknown message received");
-    }
-    mps_rec_if_->msgq_pop();
+   	while ( ! mps_rec_if_->msgq_empty() ) {
+     			
+    		
+		if ( mps_rec_if_->msgq_first_is<MPSRecognitionInterface::ComputeMessage>() ) {
+      			
+			std::cout << "Recieved Compute Message" << std::endl;
+ 			//logger->log_info(name(), "Recognition started");
+			//MPSRecognitionInterface::TakeDataMessage *m = mps_rec_if_->msgq_first<MPSRecognitionInterface::TakeDataMessage>();
+			//mps_rec_if_->set_msgid(m->id());
+			//mps_rec_if_->set_final(false);
+			//mps_rec_if_->write();
+			cout << " Start recognize " << std::endl; 
+			recognize_mps();
+    		
+		} else if ( mps_rec_if_->msgq_first_is<MPSRecognitionInterface::TakeDataMessage>() ) {
+    	
+	 		std::cout << "Recieved Take Data Message" << std::endl;
+   			readImage();
+    		}	
+   	 	else {
+    			logger->log_warn(name(), "Unknown message received");
+    		}
+    			mps_rec_if_->msgq_pop();
     
-  }
+  		}
 }
 
+/*
+void MarkerlessRecognitionThread::readImage(){
+
+
+        Mat image = imread("/home/casto/Carologistics/OpenTC/Training/ExperimentalTrainingData/BS/BS_Depth_2682.jpg") ; //Read the file , dev/video_tag should be the path for the RealSense
+
+        if(! image.data )                              // Check for invalid input
+        {
+                cout <<  "Could not open or find the image" << std::endl ;
+        }
+        try{ // to show , doesn't work (black screen) 
+
+                double min;
+                double max;
+                cv::minMaxIdx(image, &min, &max);
+                cv::Mat adjMap;
+                cv::convertScaleAbs(image, adjMap, 255 / max);
+                //cv::imshow("Out", adjMap);
+        }
+        catch( cv::Exception& e )
+        {
+                const char* err_msg = e.what();
+                std::cout << "exception caught: " << err_msg << std::endl;
+        }
+}*/
 
