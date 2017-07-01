96b20ffdffea89c35ddf9df17c6179bf02e3a4f5
Carsten Stoffels <carsten.stoffels@rwth-aachen.de>
2017-07-01 22:34:50 +0200

    skills/zone_recog: Added final result calculation

--- src/lua/skills/robotino/zone_recog.lua
+++ src/lua/skills/robotino/zone_recog.lua
@@ -27,7 +27,9 @@ name               = "zone_recog"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=false}
 depends_skills     = {"mps_recog", "goto"}
 depends_interfaces = {
-   {v = "speechsynth", type = "SpeechSynthInterface", id = "Flite"}
+   {v = "speechsynth", type = "SpeechSynthInterface", id = "Flite"},
+   {v = "mps_recognition_if", type = "MPSRecognitionInterface" ,id="/MarkerlessRecognition"},
+
 }
 
 documentation      = [==[ drive_to_zone
@@ -49,11 +51,52 @@ START_DIST_TO_MPS = 0.15+BOT_RADIUS
 
 START_POS={-MPS_WIDTH/2-START_DIST_TO_MPS,0.,0.}
 
+-- Constants
+MPS_TYPES = {
+'Base Station',
+'Cap Station',
+'Delivery Station',
+'Ring Station',
+'Storage Station',
+}
+
+
+function calc_final_result(self) 
+
+    result = {}
+    for i=0,5 do
+      result[i] = 0
+    end
+
+    result[self.fsm.vars.resultStart] = result[self.fsm.vars.resultStart] + 1
+    result[self.fsm.vars.resultSecond] = result[self.fsm.vars.resultSecond] + 1
+    result[self.fsm.vars.resultThird] = result[self.fsm.vars.resultThird] + 1
+    result[self.fsm.vars.resultFinal] = result[self.fsm.vars.resultFinal] + 1
+   
+    max = 1 
+
+    for j=0,4 do 
+        if result[j+1] > result[j] then
+	   max = result[j+1]
+ 	end 
+    end
+    
+    recognition_result = MPS_TYPES[max]
+    printf("The result of resultStart is %s",self.fsm.vars.resultStart)
+    printf("The result of resultSecond is %s",self.fsm.vars.resultSecond)
+    printf("The result of resultThird is %s",self.fsm.vars.resultThird)
+    printf("The result of resultFinal is %s",self.fsm.vars.resultFinal)
+    printf("The result of max is %s",max) 
+    printf("The result of zone_recog is %s",recognition_result)
+    printf("The result of zone_recog is %s",recognition_result)
+
+    return true
+
+end
 
 
 function calc_xy_coordinates(self)
- 
- 
+
   -- zone argument is of the form  M-Z21
   self.fsm.vars.xZone = tonumber(string.sub(self.fsm.vars.zone, 4, 4)) - 0.5
   self.fsm.vars.yZone = tonumber(string.sub(self.fsm.vars.zone, 5, 5)) - 0.5
@@ -81,12 +124,14 @@ fsm:define_states{ export_to=_M,
    {"RECOGNIZE_MPS_START", SkillJumpState, skills={{mps_recog}}, final_to="DRIVE_TO_SECOND", fail_to="FAILED"},
    {"RECOGNIZE_MPS_SECOND", SkillJumpState, skills={{mps_recog}}, final_to="DRIVE_TO_THIRD", fail_to="FAILED"},
    {"RECOGNIZE_MPS_THIRD", SkillJumpState, skills={{mps_recog}}, final_to="DRIVE_TO_FINAL", fail_to="FAILED"},
-   {"RECOGNIZE_MPS_FINAL", SkillJumpState, skills={{mps_recog}}, final_to="FINAL", fail_to="FAILED"},
+   {"RECOGNIZE_MPS_FINAL", SkillJumpState, skills={{mps_recog}}, final_to="CALCULATE_RESULT", fail_to="FAILED"},
+   {"CALCULATE_RESULT",JumpState}, 
 }
 
 fsm:add_transitions{
    {"INIT", "DRIVE_TO_START", cond=calc_xy_coordinates},
-   {"INIT", "FAILED", cond=true, desc="zoneID is not between 1 and 106"},
+   {"INIT", "FAILED", cond=true},
+   {"CALCULATE_RESULT","FINAL", cond=calc_final_result},
 }
 
 
@@ -96,19 +141,23 @@ function DRIVE_TO_START:init()
    self.args["goto"].y = self.fsm.vars.yZone
    --self.args["goto"].region_trans = 2
    self.args["goto"].ori = 0
+   self.fsm.vars.resultStart = mps_recognition_if:mpstype(); 
 end
 
 function DRIVE_TO_SECOND:init()
    self.args["goto"].x = self.fsm.vars.xZone
    self.args["goto"].y = self.fsm.vars.yDown
    --self.args["goto"].region_trans = 2
-   self.args["goto"].ori = 1.57 
+   self.args["goto"].ori = 1.57
+   self.fsm.vars.resultSecond = mps_recognition_if:mpstype(); 
 end
+
 function DRIVE_TO_THIRD:init()
    self.args["goto"].x = self.fsm.vars.xRight
    self.args["goto"].y = self.fsm.vars.yZone
    --self.args["goto"].region_trans = 2
-   self.args["goto"].ori = 3.1415
+   self.args["goto"].ori = 3.1415	
+   self.fsm.vars.resultThird = mps_recognition_if:mpstype(); 
 end
 
 function DRIVE_TO_FINAL:init()
@@ -116,6 +165,8 @@ function DRIVE_TO_FINAL:init()
    self.args["goto"].y = self.fsm.vars.yUp
    --self.args["goto"].region_trans = 2
    self.args["goto"].ori = 4.71
+   self.fsm.vars.resultFinal = mps_recognition_if:mpstype();
+
 end
 
 
