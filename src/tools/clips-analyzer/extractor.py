ed84164f3a2a8d23fa42bafb36411aab4df29ae3
Sebastian Schoenitz <sebastian.schoenitz@ima-zlw-ifu.rwth-aachen.de>
2017-07-25 14:12:00 +0200

    extractor.py: Calculate game times

--- src/tools/clips-analyzer/extractor.py
+++ src/tools/clips-analyzer/extractor.py
@@ -6,13 +6,24 @@ import re
 
 import argparse
 
+def to_seconds(hour, minute, second):
+    return hour*3600+minute*60+second
+
+def converttime(starttime, time):
+    if starttime <=time:
+        return time - starttime
+    else:
+        return 86400 + time - starttime
+
+
 class Line:
     """A simple class to save attributes"""
-    def __init__(self,content,rel_line_counter,abs_line_counter):
+    def __init__(self,content,rel_line_counter,abs_line_counter,time=None):
         self.cont = content
         self.relc = rel_line_counter
         self.absc = abs_line_counter
         self.attr = set([])
+        self.time = time
         self.check_for_blackboard()
         self.check_for_fire()
         self.check_for_retract()
@@ -23,6 +34,9 @@ class Line:
         self.check_for_ff_feature_request()
         self.check_for_sync_template()
         self.check_for_skill_statechange()
+        
+        self.return_time()
+
 
         self.stripped_content = self.strip_content()
 
@@ -56,6 +70,12 @@ class Line:
     def check_for_skill_statechange(self):
         if(re.search(r"Skill.*is.*was",self.cont)!= None):
             self.attr.add('skill_statechange')
+    
+    def return_time(self):
+        time = re.search(r"[a-zA-Z]\s([0-9]*):([0-9]*):([0-9]*)",self.cont)
+        self.time = to_seconds(time.group(1),time.group(2),time.group(3))
+        print(str(self.time)+"\n")
+    
     def strip_content(self):
         return re.sub(r"(.*agent\)*:)",'',self.cont)
 
@@ -67,19 +87,19 @@ def extract_from_file(filename):
         debug_list = []
         for i,line in enumerate(debug_file):
            abs_line_counter = i #abs_line_counter +1
-           rel_line_counter = rel_line_counter +1
            line = line.lstrip().rstrip()
            line = re.search('.*CLIPS.*',line)
            if( line != None ):
                if(re.search(r'Trying CLIPS file .*/fawkes-robotino/fawkes/src/plugins/clips/clips/ff-config.clp',line.group())!=None):
                    if(debug_list!=[]):
-                       rel_line_counter = 1
                        game_list.append(debug_list)
                        debug_list=[]
                        debug_list.append(Line(line.group(),rel_line_counter,abs_line_counter))
+                       rel_line_counter = 0
                        
                else:
                     debug_list.append(Line(line.group(),rel_line_counter,abs_line_counter))
+                    rel_line_counter = rel_line_counter +1
 
 
         game_list.append(debug_list)
@@ -92,7 +112,7 @@ def interactive(game_list):
         print("There are "+ str(len(game_list)) + " games in this logfile: "+ filename)
     
     print("\n")
-    print("You can operate on these games as following:\n")
+    print("Please use the flag -h or --help for how this programm can be used\n")
     print("")
 
 filename = "../../../bin/debug1.log"
@@ -101,31 +121,34 @@ filename = "../../../bin/debug1.log"
 #else:
 #    filename=sys.argv[1]
 game_list = extract_from_file(filename)
-interactive(game_list)
-def setup(game_number=0):
-    start = False
-    retract_dict = {}
-    assert_dict = {}
-    switch = None
-    for i,line in enumerate(game_list[game_number]):
-    #    print("Relative line: "+ str(line.relc) + ", absolute line: "+ str(line.absc)+ ", attribute: "+ str(line.attr) +", content: "+ line.cont)
-    #    print("Relative line: "+ str(line.relc) + ", absolute line: "+ str(line.absc)+ ", attribute: "+ str(line.attr) +", content: "+ line.stripped_content)
-        if 'f-0' in line.stripped_content:
-            start=True
-        if start:
-            if 'assert' in line.attr or 'retract' in line.attr:
-                switch = 'assert'
-                temp = re.search('(<\=\=\s)(f\-[0-9]*)\s*(.*)',line.stripped_content)
-                if temp == None:
-                    switch = 'retract'
-                    temp = re.search('(\=\=>\s)(f\-[0-9]*)\s*(.*)',line.stripped_content)
+#interactive(game_list)
+start = False
+retract_dict = {}
+assert_dict = {}
+switch = None
+game_number=0
+starttime = 0
+starttime = to_seconds(13,14,45)#game_list[game_number][0].time
+print("Startzeit: "+ str(starttime))
+for i,line in enumerate(game_list[game_number]):
+    #print("Relative line: "+ str(line.relc) + ", absolute line: "+ str(line.absc)+ ", attribute: "+ str(line.attr) +", content: "+ line.cont)
+    #print("Relative line: "+ str(line.relc) + ", absolute line: "+ str(line.absc)+ ", attribute: "+ str(line.attr) +", content: "+ line.stripped_content)
+    if 'f-0' in line.stripped_content:
+        start=True
+    if start:
+        if 'assert' in line.attr or 'retract' in line.attr:
+            switch = 'assert'
+            temp = re.search('(<\=\=\s)(f\-[0-9]*)\s*(.*)',line.stripped_content)
+            if temp == None:
+                switch = 'retract'
+                temp = re.search('(\=\=>\s)(f\-[0-9]*)\s*(.*)',line.stripped_content)
 
-                a,b = temp.group(2), temp.group(3)
-                fused = {i:[a,b]}
-                if switch == 'assert':
-                    assert_dict.update(fused)
-                else:
-                    retract_dict.update(fused)
+            a,b = temp.group(2), temp.group(3)
+            fused = {i:[a,b]}
+            if switch == 'assert':
+                assert_dict.update(fused)
+            else:
+                retract_dict.update(fused)
 
 def current_factbase(assert_dict, retract_dict, point_in_time):
     factbase = []
@@ -135,9 +158,10 @@ def current_factbase(assert_dict, retract_dict, point_in_time):
         elif timestep in retract_dict:
             if retract_dict[timestep] in factbase:
                 factbase.remove(retract_dict[timestep])
-    print ('Facts')
-    for f_ in factbase:
-        print(f_[1])
+    return factbase
+    #print ('Facts')
+    #for f_ in factbase:
+    #    print(f_[1])
 #current_factbase(assert_dict,retract_dict,1500)
 
 # Was stand wann und wann in der factbase
@@ -154,13 +178,24 @@ parser.add_argument("-s", "--slotname", type=str, help="Filters for a specific s
 parser.add_argument("-g", "--game", type=int, help="Shows a specific game")
 parser.add_argument("-l", "--list", action="store_true", help="Lists all games found")
 parser.add_argument("-t", "--time", type=int, help="Shows factbase at a specific point in time, counted from the start")
-parser.add_argument("-T", "--Time", type=int, help="Shows factbase at a specific point in time (absolute time)")
+parser.add_argument("-T", "--Time", type=int, help="Shows factbase at a specific point in time")
 args = parser.parse_args()
 
+
+
+def list_games():
+    print("There are " + str(len(game_list))+ " games that can be found in " + filename)
+    print("\nFor more information use -h or --help")
+def contains(cfilter):
+    fbase = current_factbase(asser_dict, retract_dict, 1500) 
+    print(fbase)
+
+def slotname(sfilter):
+    pass
+
 if args.list:
-    for i, _ in enumerate(game_list):
-        print(i)
-if args.game>=0:
-    setup()
-    current_factbase(assert_dict,retract_dict,1500)
-    
+    list_games()
+if args.game!=None:
+    if type(args.game) is int and args.game >= 0:
+        current_factbase(assert_dict,retract_dict,1500)
+
