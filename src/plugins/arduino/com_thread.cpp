3833ef4410655d1070212dd2bd28ddb72930eace
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2017-07-27 04:25:41 +0200

    arduino: setup correct RestoreMessage handling
    The restore feature was not correctly handled leading to errornous z
    movement.

--- src/plugins/arduino/com_thread.cpp
+++ src/plugins/arduino/com_thread.cpp
@@ -148,12 +148,18 @@ ArduinoComThread::loop()
                 move_to_z_0_pending_ = true;
 
             } else if (arduino_if_->msgq_first_is<ArduinoInterface::RestoreMessage>()) {
-                ArduinoComMessage req;
-                req.set_command(ArduinoComMessage::CMD_STEP_UP);
-                req.set_number((cfg_init_mm_ - current_z_position_) * ArduinoComMessage::NUM_STEPS_PER_MM);
-                req.set_msecs(((double) ((cfg_init_mm_ - current_z_position_) * ArduinoComMessage::NUM_STEPS_PER_MM) / (double)cfg_speed_) * 1000. * 10.);
-                logger->log_debug(name(), "Moving to init_mm position, %u steps", (cfg_init_mm_ - current_z_position_) * ArduinoComMessage::NUM_STEPS_PER_MM);
-                messages_.push(req);
+                if((cfg_init_mm_ - current_z_position_) != 0) {
+                    ArduinoComMessage req;
+                    if ((cfg_init_mm_ - current_z_position_) < 0) {
+                        req.set_command(ArduinoComMessage::CMD_STEP_UP);
+                    } else {
+                        req.set_command(ArduinoComMessage::CMD_STEP_DOWN);
+                    }
+                    req.set_number(abs(cfg_init_mm_ - current_z_position_) * ArduinoComMessage::NUM_STEPS_PER_MM);
+                    req.set_msecs(((double) abs((cfg_init_mm_ - current_z_position_) * ArduinoComMessage::NUM_STEPS_PER_MM) / (double)cfg_speed_) * 1000. * 10.);
+                    logger->log_debug(name(), "Moving to init_mm position, %u steps", (cfg_init_mm_ - current_z_position_) * ArduinoComMessage::NUM_STEPS_PER_MM);
+                    messages_.push(req);
+                }
             }
             arduino_if_->msgq_pop();
         }
