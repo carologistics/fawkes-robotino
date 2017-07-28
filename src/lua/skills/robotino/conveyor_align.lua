5b5624e964aae5ff8b6ee1a17e2f78a347e6dd79
Daniel Kuenster <daniel.kuenster@rwth-aachen.de>
2017-07-28 06:09:05 +0200

    conveyor_align: align at same height with puck
    We don't align higher when carrying a puck anymore, so that we can use
    the funnel when putting a product onto a machine

--- src/lua/skills/robotino/conveyor_align.lua
+++ src/lua/skills/robotino/conveyor_align.lua
@@ -27,7 +27,6 @@ depends_interfaces = {
    {v = "motor", type = "MotorInterface", id="Robotino" },
    {v = "if_conveyor", type = "Position3DInterface", id="conveyor_pose/pose"},
    {v = "conveyor_switch", type = "SwitchInterface", id="conveyor_pose/switch"},
-   {v = "if_gripper", type = "AX12GripperInterface", id="Gripper AX12"},
 }
 
 documentation      = [==[aligns the robot orthogonal to the conveyor by using the
@@ -55,7 +54,6 @@ local MAX_TRIES = 20
 local X_DEST_POS = 0.16
 local Y_DEST_POS = 0.0
 local Z_DEST_POS = z_pos
-local Z_DEST_POS_WITH_PUCK = z_pos + 0.004
 local cfg_frame_ = "gripper"
 
 function no_writer()
@@ -79,9 +77,6 @@ function max_tries_not_reached(self)
 end
 
 function pose_offset(self)
-   if if_gripper:is_holds_puck() then
-      Z_DEST_POS = Z_DEST_POS_WITH_PUCK
-   end
 
    local from = { x = if_conveyor:translation(0),
                   y = if_conveyor:translation(1),
