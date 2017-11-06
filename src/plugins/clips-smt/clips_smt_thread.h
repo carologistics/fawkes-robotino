c7ae14931a48b2eb6fff426df0c43781209b088d
Igor Bongartz <nicolai.bongartz@rwth-aachen.de>
2017-11-06 16:18:19 +0100

    Clips-smt: Refactor code

--- src/plugins/clips-smt/clips_smt_thread.h
+++ src/plugins/clips-smt/clips_smt_thread.h
@@ -38,12 +38,6 @@
 #  undef FALSE
 #endif
 
-#include <z3++.h>
-// #include <carl/numbers/numbers.h>
-// #include <carl/core/VariablePool.h>
-// #include <carl/formula/Formula.h>
-// #include <carl/io/SMTLIBStream.h>
-
 #include <vector>
 #include <string>
 #include <map>
@@ -52,17 +46,21 @@
 #include <math.h>
 #include <memory>
 
+// Leonard Korp approach
+// #include <carl/numbers/numbers.h>
+// #include <carl/core/VariablePool.h>
+// #include <carl/formula/Formula.h>
+// #include <carl/io/SMTLIBStream.h>
 // #include "FormulaGenerator/FormulaGenerator.h"
 // #include "FormulaGenerator/GameData.h"
 // #include "FormulaGenerator/formulaGeneratorTest.cpp"
 
 #include <llsf_msgs/ClipsSmtData.pb.h>
-
+#include <z3++.h>
 #include <boost/cerrno.hpp>
 
 
 namespace fawkes {
-	// class SubProcess;
 	class NavGraphStaticListEdgeCostConstraint;
 }
 
@@ -72,7 +70,6 @@ class ClipsSmtThread
 	public fawkes::ConfigurableAspect,
 	public fawkes::NavGraphAspect,
 	public fawkes::NavGraph::ChangeListener,
-	//public fawkes::TransformAspect,
 	public fawkes::CLIPSFeature,
 	public fawkes::CLIPSFeatureAspect
 {
@@ -106,7 +103,12 @@ private:
 	z3::context _z3_context;
 	bool _solver_done;
 
-	// Francescos formula encoding
+	// Solve formula
+	z3::expr clips_smt_extract_formula_from_smt_file(std::string path);
+	void clips_smt_solve_formula(z3::expr_vector formula, std::string var);
+	// void clips_smt_solve_formula_from_fg_smt_file(std::string path, FormulaGenerator fg);
+
+	// Francescos Leofantes formula encoding
 	z3::expr_vector clips_smt_encoder(std::map<std::string, z3::expr>& varStartTime,
 										std::map<std::string, z3::expr>& varRobotDuration,
 										std::map<std::string, z3::expr>& varRobotPosition,
@@ -118,27 +120,34 @@ private:
 										std::map<std::string, z3::expr>& varS,
 										std::map<std::string, z3::expr>& varRew,
 										std::map<std::string, z3::expr>& varInit);
-	void clips_smt_solve_formula(z3::expr_vector formula);
-	void clips_smt_solve_formula_from_smt_file(std::string path);
-	void clips_smt_optimize_formula_from_smt_file(std::string path, std::string var);
-	// void clips_smt_solve_formula_from_fg_smt_file(std::string path, FormulaGenerator fg);
 
-	std::map<int ,std::string> actions_robot_1;
-	std::map<int ,std::string> actions_robot_2;
-	std::map<int ,std::string> actions_robot_3;
+	/*
+	 * The constants for orders are defined as follows:
+	 *	- index_delivery_action_ci is the index of the action which have to be performed to deliver the product
+	 *	- number_required_action_ci is the amount of steps necessary to create the order
+	 *	- index_upper_bound_actions_ci is the upper bound of action_ids which have to be considered for the order (note that not all might be needed)
+	 *
+	 * Then we define variables based on the constants and the number of orders in the regarding complexity
+	 *	- index_upper_bound_actions is set to the index_upper_bound_actions_ci of the highest available order
+	 *	- number_total_actions is the amount of action_ids covering all orders
+	 */
 
-	// Constants for C0-C1
-	const int number_final_actions_c0 = 6;
+	// c0
+	const int index_delivery_action_c0 = 6;
 	const int number_required_actions_c0 = 6;
-	const int number_total_actions_c0 = 6;
+	const int index_upper_bound_actions_c0 = 6;
 
-	const int number_final_actions_c1 = 11;
+	// c1
+	const int index_delivery_action_c1 = 11;
 	const int number_required_actions_c1 = 8;
-	const int number_total_actions_c1 = 11;
+	const int index_upper_bound_actions_c1 = 11;
 
-	int number_required_actions;
+	// All ci
+	int index_upper_bound_actions;
 	int number_total_actions;
+	int plan_horizon;
 	
+	// Time constants // TODO evalutate realistic constants
 	const int deadline = 900;
 	const int time_to_prep = 5;
 	const int time_to_fetch = 5;
@@ -146,43 +155,51 @@ private:
 	const int time_to_disc = 5;
 	const int time_to_del = 5;
 
-	int plan_horizon;
-
+	// Consider delivery window for orders
 	const bool add_temporal_constraint = false;
 
-	// States of machines for C0-C1
+	// States of machines
+	// State1 refers to the prepared state of a machine (DS, CS, RS, BS)
 	std::map<std::string, int> state1_machines;
 	const int min_state1_machines = 0, max_state1_machines = 13;
+
+	// State2 refers to the possibility of mounting (CS, RS)
 	std::map<std::string, int> state2_machines;
 	const int min_state2_machines = 0, max_state2_machines = 6;
+
+	// State3 refers to the possibilty of retrieving (CS, RS)
 	std::map<std::string, int> state3_machines;
 	const int min_state3_machines = -1, max_state3_machines = 45;
+
+	// Products refers to all possible products
 	std::map<std::string, int> products;
 	std::map<int, std::string> products_inverted;
 	const int min_products = 0, max_products = 45;
+
+	// Machine_groups refers to all possible machines (DS, CS, RS, BS)
 	std::map<std::string, int> machine_groups;
 	const int min_machine_groups = 0, max_machine_groups = 3;
 
-	std::map<int, std::string> actions;
-
 	// Visualization of computed plan
-	std::map<int , float> model_times;
-	std::map<int , int> model_positions;
-	std::map<int , int> model_positions_R1;
-	std::map<int , int> model_positions_R2;
-	std::map<int , int> model_positions_R3;
-	std::map<int , int> model_robots;
-	std::map<int , int> model_actions;
-	std::map<int , int> model_holdA;
-	std::map<int , int> model_state1A;
-	std::map<int , int> model_state2A;
-	std::map<int , int> model_state3A;
-	std::map<int , int> model_holdB;
-	std::map<int , int> model_state1B;
-	std::map<int , int> model_state2B;
-	std::map<int , int> model_state3B;
+	std::map<int, std::string> actions;
+	
+	std::map<int, float> model_times;
+	std::map<int, int> model_positions;
+	std::map<int, int> model_positions_R1;
+	std::map<int, int> model_positions_R2;
+	std::map<int, int> model_positions_R3;
+	std::map<int, int> model_robots;
+	std::map<int, int> model_actions;
+	std::map<int, int> model_holdA;
+	std::map<int, int> model_state1A;
+	std::map<int, int> model_state2A;
+	std::map<int, int> model_state3A;
+	std::map<int, int> model_holdB;
+	std::map<int, int> model_state1B;
+	std::map<int, int> model_state2B;
+	std::map<int, int> model_state3B;
 
-	// Leonards formula encoding
+	// Leonard Korps formula encoding
 	// GameData clips_smt_convert_protobuf_to_gamedata();
 	// std::vector<int> actions_robot_fg_1;
 	// std::vector<int> actions_robot_fg_2;
@@ -202,13 +219,13 @@ private:
 
 	std::string data_env;
 	std::string data_handle;
+	std::map<std::string, fawkes::LockPtr<CLIPS::Environment> >  envs_;
 
 	// Global variables
 	int number_machines;
 	int number_robots;
-	// int number_bits;
-	int number_orders;
 	int number_orders_protobuf;
+	int number_orders;
 	const int number_orders_c0 = 1;
 	const int number_orders_c1 = 0;
 
@@ -217,11 +234,12 @@ private:
 	void clips_smt_fill_robot_names();
 	void clips_smt_compute_distances_robots();
 	void clips_smt_compute_distances_machines();
+
 	std::map<int, std::string> robot_names_;
-	std::map<int, std::string> node_names_;
-	std::map<std::pair<std::string, std::string>, float> distances_;
-	std::map<std::string, int> node_names_inverted;
 	std::map<int, int> robot_permutation_;
+	std::map<int, std::string> node_names_;
+	std::map<std::string, int> node_names_inverted;
+	std::map<std::pair<std::string, std::string>, float> distances_;
 
 	std::map<std::string, std::string> colors_input;
 	std::map<std::string, std::string> colors_output;
@@ -233,10 +251,7 @@ private:
 
 	// Test
 	// void clips_smt_test_z3();
-	// void clips_smt_test_carl();
 	// void clips_smt_test_formulaGenerator();
-
-	std::map<std::string, fawkes::LockPtr<CLIPS::Environment> >  envs_;
 
 	// Help
 	z3::expr getVar(std::map<std::string, z3::expr>& vars, std::string var_id);
