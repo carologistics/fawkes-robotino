3c107cf1fc662a5773446c52271cb5a372384503
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2018-01-22 16:14:57 +0100

    velocity_share: share positions and velocities via robot-memory
    This plugin shares position and velocity information among robots via
    robot-memory.
    Each robot is identified by its "robot-name", (taken from the configuration)
    and creates an object for itself if not available in the "memory.robots" collection.
    
    The position is gathered from the Position3DInterface "pose".
    The velocity is gathered from the MotorInterface "motor_if".

---
+++ src/plugins/velocity_share/velocity_share_plugin.cpp
@@ -0,0 +1,50 @@
+/***************************************************************************
+ *  velocity_share_plugin.cpp - A plugin to share positions and velocities
+ *  via robot_memory
+ *
+ *  Created: Sat Jan 20 16:47:28 2018
+ *  Copyright  2018  Nicolas Limpert
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <core/plugin.h>
+
+#include "velocity_share_thread.h"
+
+using namespace fawkes;
+
+/** Plugin to share positions and velocities between multiple robots.
+ *  The respective positions and velocities are written to an interface
+ *  but also published as a ROS message.
+ *  Publication as a ROS message can help in providing a more advanced
+ *  collision avoidance when ROS move_base is used and the respective info
+ *  about other moving robots is processed accordingly.
+ * @author Nicolas Limpert
+ */
+class VelocitySharePlugin : public fawkes::Plugin
+{
+ public:
+  /** Constructor.
+   * @param config Fawkes configuration
+   */
+  VelocitySharePlugin(Configuration *config)
+    : Plugin(config)
+  {
+    thread_list.push_back(new VelocityShareThread());
+  }
+};
+
+PLUGIN_DESCRIPTION("Fawkes Velocity Share Plugin to share position and velocities")
+EXPORT_PLUGIN(VelocitySharePlugin)
---
+++ src/plugins/velocity_share/velocity_share_thread.h
@@ -0,0 +1,87 @@
+/***************************************************************************
+ *  velocity_share_thread.h - Thread to gather position and velocity
+ *  information and share it with other robots via robot_memory
+ *
+ *  Created: Sat Jan 20 16:55:11 2018
+ *  Copyright  2018  Nicolas Limpert
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#ifndef __PLUGINS_VELOCITY_SHARE_THREAD_H_
+#define __PLUGINS_VELOCITY_SHARE_THREAD_H_
+
+#include <aspect/logging.h>
+#include <aspect/tf.h>
+#include <core/threading/thread.h>
+#include <tf/types.h>
+#include <plugins/ros/aspect/ros.h>
+#include <utils/time/wait.h>
+#include <plugins/robot-memory/aspect/robot_memory_aspect.h>
+#include <interfaces/Position3DInterface.h>
+#include <interfaces/MotorInterface.h>
+#include <geometry_msgs/Twist.h>
+#include <velocity_share_msgs/RobotVelInfoStamped.h>
+#include <ros/subscriber.h>
+#include <ros/node_handle.h>
+#include <config/change_handler.h>
+
+
+class VelocityShareThread
+    : public fawkes::Thread,
+    public fawkes::ClockAspect,
+    public fawkes::LoggingAspect,
+    public fawkes::ConfigurableAspect,
+    public fawkes::ConfigurationChangeHandler,
+    public fawkes::BlackBoardAspect,
+    public fawkes::ROSAspect,
+    public fawkes::TransformAspect,
+    public fawkes::RobotMemoryAspect
+{
+public:
+  VelocityShareThread();
+
+  virtual void init();
+  virtual void loop();
+  virtual void finalize();
+
+private:
+  // Implemented abstracts inherited from ConfigurationChangeHandler
+  virtual void config_tag_changed(const char *new_tag);
+  virtual void config_value_changed(const fawkes::Configuration::ValueIterator *v);
+  virtual void config_comment_changed(const fawkes::Configuration::ValueIterator *v);
+  virtual void config_value_erased(const char *path);
+
+  fawkes::Position3DInterface *pose_if_;
+  fawkes::MotorInterface *motor_if_;
+  std::string robotname_;
+  fawkes::TimeWait *time_wait_;
+  float update_rate_;
+
+  // velocity scaling to increase the actual velocity reported to other robots.
+  float velocity_scale_;
+  float cur_vx_, cur_vy_, cur_vori_;
+  float last_vx_, last_vy_, last_vori_;
+  bool update_needed_;
+
+  bool almost_equal(float a, float b);
+  void load_config();
+
+  ros::Time now_;
+
+  std::string vel_share_pub_topic_;
+  ros::Publisher vel_share_pub_;
+};
+
+#endif
