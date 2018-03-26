242c31553bb1da3cbcd6b89ce219e91c6b034baf
Igor Bongartz <nicolai.bongartz@rwth-aachen.de>
2018-03-26 13:35:38 +0200

    clips-executive/rcll-smt: Add wp to wp-get-shelf in plugin clips-smt

--- src/plugins/clips-smt/clips_smt_thread.cpp
+++ src/plugins/clips-smt/clips_smt_thread.cpp
@@ -393,7 +393,10 @@ ClipsSmtThread::clips_smt_get_plan(std::string env_name, std::string handle)
 					param->set_value(node_names_[model_positions[i]]);
 					param = action->add_params();
 					param->set_key("shelf");
-					param->set_value(getNextShelf()); // TODO Replace TRUE by getNextShelf() once implemented
+					param->set_value(getNextShelf());
+					param = action->add_params();
+					param->set_key("wp");
+					param->set_value(getCapCarrier(getCapColor(data.orders(order_id).cap_color()))); // TODO Take value of NextShelf into account
 
 					++action_id;
 					action = plan->add_actions();
