abd7561a1024dfd8ffb158fdfb5bcf5ee1ecce79
Victor Mataré <matare@fh-aachen.de>
2018-04-18 17:30:04 +0200

    conveyor_pose: implement ICP
    correspondence grouping turned out to be too imprecise, so generate an
    initial estimate from the laser line and then perform Iterative Closest
    Point matching.

--- src/plugins/conveyor_pose/correspondence_grouping_thread.h
+++ src/plugins/conveyor_pose/correspondence_grouping_thread.h
@@ -8,19 +8,35 @@
 
 #include "conveyor_pose_thread.h"
 
-class CorrespondenceGroupingThread
+class RecognitionThread
     : public fawkes::Thread
     , public fawkes::LoggingAspect
 {
 public:
-  CorrespondenceGroupingThread(ConveyorPoseThread *cp_thread);
+  RecognitionThread(ConveyorPoseThread *cp_thread);
 
   using pose = ConveyorPoseThread::pose;
 
   virtual void loop() override;
+  virtual void init() override;
 
 private:
   ConveyorPoseThread *main_thread_;
 };
+
+
+// Define a new point representation for < x, y, z, curvature >
+class MyPointRepresentation : public pcl::PointRepresentation<pcl::PointNormal>
+{
+public:
+  MyPointRepresentation()
+  {
+    nr_dimensions_ = 4;
+  }
+
+  // Override the copyToFloatArray method to define our feature vector
+  virtual void copyToFloatArray (const pcl::PointNormal &p, float * out) const override;
+};
+
 
 #endif // CORRESPONDENCE_GROUPING_THREAD_H
