f40a1e99ed3e722bf7996b0f1e03c45e1662ff3e
Victor Mataré <matare@fh-aachen.de>
2018-04-21 17:53:50 +0200

    conveyor_pose: cancel ICP on SetStationMessage
    also reset euclidean_fitness to minimum

--- src/plugins/conveyor_pose/correspondence_grouping_thread.cpp
+++ src/plugins/conveyor_pose/correspondence_grouping_thread.cpp
@@ -33,7 +33,6 @@ void RecognitionThread::loop()
 {
   pcl::PointCloud<pcl::PointNormal>::Ptr model_with_normals(new pcl::PointCloud<pcl::PointNormal>());
   pcl::PointCloud<pcl::PointNormal>::Ptr scene_with_normals(new pcl::PointCloud<pcl::PointNormal>());
-  CloudPtr aligned_model(new Cloud());
   Eigen::Matrix4f initial_tf;
 
   { fawkes::MutexLocker locked { &main_thread_->cloud_mutex_ };
@@ -46,74 +45,79 @@ void RecognitionThread::loop()
     scene_with_normals->header = main_thread_->scene_with_normals_->header;
 
     initial_tf = main_thread_->initial_tf_;
+  }
 
-    /*pcl::transformPointCloud(*main_thread_->model_, *aligned_model, initial_tf);
-    main_thread_->cloud_publish(aligned_model, main_thread_->cloud_out_model_);*/
+  MyPointRepresentation point_representation;
+  // weigh the 'curvature' dimension so that it is balanced against x, y, and z
+  float alpha[4] = {1.0, 1.0, 1.0, 1.0};
+  point_representation.setRescaleValues (alpha);
 
-    MyPointRepresentation point_representation;
-    // weigh the 'curvature' dimension so that it is balanced against x, y, and z
-    float alpha[4] = {1.0, 1.0, 1.0, 1.0};
-    point_representation.setRescaleValues (alpha);
+  // Align
+  pcl::IterativeClosestPointNonLinear<pcl::PointNormal, pcl::PointNormal> reg;
+  reg.setTransformationEpsilon (1e-6);
+  reg.setMaxCorrespondenceDistance (double(main_thread_->cfg_icp_max_corr_dist_));
+  reg.setPointRepresentation (boost::make_shared<const MyPointRepresentation> (point_representation));
 
-    // Align
-    pcl::IterativeClosestPointNonLinear<pcl::PointNormal, pcl::PointNormal> reg;
-    reg.setTransformationEpsilon (1e-6);
-    reg.setMaxCorrespondenceDistance (double(main_thread_->cfg_icp_max_corr_dist_));
-    reg.setPointRepresentation (boost::make_shared<const MyPointRepresentation> (point_representation));
+  reg.setInputTarget (scene_with_normals);
 
-    reg.setInputTarget (scene_with_normals);
+  // Run the same optimization in a loop and visualize the results
+  Eigen::Matrix4f Ti(Eigen::Matrix4f::Identity());
+  Eigen::Matrix4f prev;
+  reg.setMaximumIterations (2);
 
-    // Run the same optimization in a loop and visualize the results
-    Eigen::Matrix4f Ti(Eigen::Matrix4f::Identity());
-    Eigen::Matrix4f prev;
-    reg.setMaximumIterations (2);
+  pcl::PointCloud<pcl::PointNormal>::Ptr reg_result(new pcl::PointCloud<pcl::PointNormal>());
 
-    pcl::PointCloud<pcl::PointNormal>::Ptr reg_result(new pcl::PointCloud<pcl::PointNormal>());
+  reg.setInputSource (model_with_normals);
+  reg.align (*reg_result, initial_tf);
 
+  for (int i = 0; i < 30; ++i) {
+    model_with_normals = reg_result;
+
+    // Estimate
     reg.setInputSource (model_with_normals);
-    reg.align (*reg_result, initial_tf);
+    reg.align (*reg_result);
 
-    for (int i = 0; i < 30; ++i) {
-      model_with_normals = reg_result;
+    //accumulate transformation between each Iteration
+    Ti = reg.getFinalTransformation () * Ti;
 
-      // Estimate
-      reg.setInputSource (model_with_normals);
-      reg.align (*reg_result);
+    //if the difference between this transformation and the previous one
+    //is smaller than the threshold, refine the process by reducing
+    //the maximal correspondence distance
+    if (std::abs((reg.getLastIncrementalTransformation() - prev).sum()) < reg.getTransformationEpsilon())
+      reg.setMaxCorrespondenceDistance (reg.getMaxCorrespondenceDistance () * 0.8);
 
-      //accumulate transformation between each Iteration
-      Ti = reg.getFinalTransformation () * Ti;
+    prev = reg.getLastIncrementalTransformation ();
 
-      //if the difference between this transformation and the previous one
-      //is smaller than the threshold, refine the process by reducing
-      //the maximal correspondence distance
-      if (std::abs((reg.getLastIncrementalTransformation() - prev).sum()) < reg.getTransformationEpsilon())
-        reg.setMaxCorrespondenceDistance (reg.getMaxCorrespondenceDistance () * 0.8);
+    if (main_thread_->icp_cancelled_)
+      return;
+  }
 
-      prev = reg.getLastIncrementalTransformation ();
+
+  CloudPtr aligned_model(new Cloud());
+  pcl::copyPointCloud(*model_with_normals, *aligned_model);
+  main_thread_->cloud_publish(aligned_model, main_thread_->cloud_out_model_);
+
+  Eigen::Matrix4f m = Ti * initial_tf;
+
+  double new_fitness = (1 / reg.getFitnessScore())
+      / double(std::min(model_with_normals->size(), scene_with_normals->size()));
+
+  { MutexLocker locked2(&main_thread_->bb_mutex_);
+
+    if (!main_thread_->icp_cancelled_) {
+      main_thread_->result_fitness_ = new_fitness;
+      main_thread_->result_pose_.setOrigin( { double(m(0,3)), double(m(1,3)), double(m(2,3)) } );
+      main_thread_->result_pose_.setBasis( {
+                                             double(m(0,0)), double(m(0,1)), double(m(0,2)),
+                                             double(m(1,0)), double(m(1,1)), double(m(1,2)),
+                                             double(m(2,0)), double(m(2,1)), double(m(2,2))
+                                           } );
+      main_thread_->result_pose_.frame_id = scene_with_normals->header.frame_id;
+      main_thread_->result_pose_.stamp = Time { static_cast<long>(scene_with_normals->header.stamp) };
+
+      main_thread_->pose_write();
+      main_thread_->pose_publish_tf(main_thread_->result_pose_);
     }
-
-
-    pcl::copyPointCloud(*model_with_normals, *aligned_model);
-
-    Eigen::Matrix4f m = Ti * initial_tf;
-
-    double new_fitness = (1 / reg.getFitnessScore())
-        / double(std::min(model_with_normals->size(), scene_with_normals->size()));
-
-    main_thread_->cloud_publish(aligned_model, main_thread_->cloud_out_model_);
-    MutexLocker locked2(&main_thread_->pose_mutex_);
-    main_thread_->result_fitness_ = new_fitness;
-    main_thread_->result_pose_.setOrigin( { double(m(0,3)), double(m(1,3)), double(m(2,3)) } );
-    main_thread_->result_pose_.setBasis( {
-                                           double(m(0,0)), double(m(0,1)), double(m(0,2)),
-                                           double(m(1,0)), double(m(1,1)), double(m(1,2)),
-                                           double(m(2,0)), double(m(2,1)), double(m(2,2))
-                                         } );
-    main_thread_->result_pose_.frame_id = scene_with_normals->header.frame_id;
-    main_thread_->result_pose_.stamp = Time { static_cast<long>(scene_with_normals->header.stamp) };
-
-    main_thread_->pose_write();
-    main_thread_->pose_publish_tf(main_thread_->result_pose_);
   }
 }
 
