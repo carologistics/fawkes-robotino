89fdb11f0c94af0937a8e01e797ee3c0118f353f
David Schmidt <david.schmidt@rwth-aachen.de>
2018-04-23 19:27:52 +0200

    WIP

--- src/lua/skills/robotino/get_product_from.lua
+++ src/lua/skills/robotino/get_product_from.lua
@@ -43,6 +43,16 @@ Parameters:
 -- Initialize as skill module
 skillenv.skill_module(_M)
 -- Constants
+if config:exists("/navgraph-generator-mps/left-shelf-distance") then
+   left_shelf_distance = config:get_float("/navgraph-generator-mps/left-shelf-distance")
+end
+if config:exists("/navgraph-generator-mps/middle-shelf-distance") then
+   middle_shelf_distance = config:get_float("/navgraph-generator-mps/middle-shelf-distance")
+end
+if config:exists("/navgraph-generator-mps/right-shelf-distance") then
+   right_shelf_distance = config:get_float("/navgraph-generator-mps/right-shelf-distance")
+end
+
 
 function already_at_conveyor(self)
    return (self.fsm.vars.atmps == "CONVEYOR")
@@ -52,9 +62,9 @@ fsm:define_states{ export_to=_M, closure={navgraph=navgraph},
    {"INIT", JumpState},
    {"DRIVE_TO", SkillJumpState, skills={{drive_to_local}}, final_to="MPS_ALIGN", fail_to="FAILED"},
    {"MPS_ALIGN", SkillJumpState, skills={{mps_align}}, final_to="CONVEYOR_ALIGN", fail_to="FAILED"},
-   {"CONVEYOR_ALIGN", SkillJumpState, skills={{conveyor_align}}, final_to="DECIDE_ENDSKILL", fail_to="FAILED"},
-   {"DECIDE_ENDSKILL", JumpState},
-   {"SKILL_SHELF_PICK", SkillJumpState, skills={{shelf_pick}}, final_to="FINAL", fail_to="FAILED"},
+   {"CONVEYOR_ALIGN", SkillJumpState, skills={{conveyor_align}}, final_to="SKILL_PRODUCT_PICK", fail_to="FAILED"},
+   -- {"DECIDE_ENDSKILL", JumpState},
+   -- {"SKILL_SHELF_PICK", SkillJumpState, skills={{shelf_pick}}, final_to="FINAL", fail_to="FAILED"},
    {"SKILL_PRODUCT_PICK", SkillJumpState, skills={{product_pick}}, final_to="FINAL", fail_to="FAILED"}
 }
 
@@ -63,8 +73,8 @@ fsm:add_transitions{
    {"INIT", "FAILED", cond="not vars.node:is_valid()", desc="point invalid"},
    {"INIT", "MPS_ALIGN", cond=already_at_conveyor, desc="Already in front of the mps, align"},
    {"INIT", "DRIVE_TO", cond=true, desc="Everything OK"},
-   {"DECIDE_ENDSKILL", "SKILL_SHELF_PICK", cond="vars.shelf", desc="Pick from shelf"},
-   {"DECIDE_ENDSKILL", "SKILL_PRODUCT_PICK", cond="true", desc="Pick from conveyor"},
+   -- {"DECIDE_ENDSKILL", "SKILL_SHELF_PICK", cond="vars.shelf", desc="Pick from shelf"},
+   -- {"DECIDE_ENDSKILL", "SKILL_PRODUCT_PICK", cond="true", desc="Pick from conveyor"},
 }
 
 function INIT:init()
@@ -72,7 +82,10 @@ function INIT:init()
 end
 
 function DRIVE_TO:init()
-   if self.fsm.vars.side == "input" or self.fsm.vars.shelf then
+   if self.fsm.vars.shelf ~= nil then
+      -- navgraph point name for shelf is e.g. C-CS1-R last letter (R, M, L) for right, middle, left
+      self.args["drive_to_local"] = {place = self.fsm.vars.place .. "-" .. string.sub(self.fsm.vars.shelf, 1, 1)}
+   elseif self.fsm.vars.side == "input" then
       self.args["drive_to_local"] = {place = self.fsm.vars.place .. "-I"}
    else --if no side is given drive to output
       self.args["drive_to_local"] = {place = self.fsm.vars.place .. "-O"}
@@ -86,10 +99,17 @@ function MPS_ALIGN:init()
       self.args["mps_align"].tag_id = navgraph:node(self.fsm.vars.place):property_as_float("tag_output")
    end
 
-   self.args["mps_align"].x = 0.4
+   self.args["mps_align"].x = 0.2
 
    if self.fsm.vars.side == "input" or self.fsm.vars.shelf then
       self.args["mps_align"].y = 0.03
+      if self.fsm.vars.shelf == "LEFT" then
+          self.args["mps_align"].y += left_shelf_distance
+      elseif self.fsm.vars.shelf == "MIDDLE" then
+          self.args["mps_align"].y += middle_shelf_distance
+      elseif self.fsm.vars.shelf == "RIGHT" then
+          self.args["mps_align"].y += right_shelf_distance
+      end
    else
       self.args["mps_align"].y = -0.03
    end
@@ -111,6 +131,6 @@ function SKILL_PRODUCT_PICK:init()
    self.args["product_pick"].offset_x = 0 
 end
 
-function SKILL_SHELF_PICK:init()
-   self.args["shelf_pick"] = {slot = self.fsm.vars.shelf}
-end
+-- function SKILL_SHELF_PICK:init()
+--    self.args["shelf_pick"] = {slot = self.fsm.vars.shelf}
+-- end
