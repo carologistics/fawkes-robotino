7795070f0dda52c80ad624a6b73e40d319636040
Carsten Stoffels <carsten.stoffels@rwth-aachen.de>
2018-04-25 20:03:27 +0200

    skills/shelf_pick_new: now using gripper_commands_new

--- src/lua/skills/robotino/shelf_pick_new.lua
+++ src/lua/skills/robotino/shelf_pick_new.lua
@@ -22,8 +22,9 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "shelf_pick_new"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=false}
-depends_skills     = {"motor_move"}
+depends_skills     = {"motor_move","gripper_commands_new"}
 depends_interfaces = {
+
 }
 
 documentation  = [==[The robot needs to be aligned with the conveyor and then grips product from shelf.
@@ -37,14 +38,18 @@ skillenv.skill_module(_M)
 local shelf_to_conveyor = 0.1
 local shelf_distance = 0.1
 local drive_back_distance = -0.2
+local gripper_x = 0
+local gripper_y = 0
+local gripper_z = 0
 
 fsm:define_states{ export_to=_M, closure={gripper_if=gripper_if},
    {"INIT", JumpState},
    {"GOTO_SHELF", SkillJumpState, skills={{motor_move}}, final_to="OPEN_GRIPPER", fail_to="FAILED"},
-   {"OPEN_GRIPPER", SkillJumpState, skills={{}}, final_to="MOVE_GRIPPER"},
-   {"MOVE_GRIPPER", SkillJumpState, skills={{}}, final_to="CLOSE_GRIPPER", fail_to="FAILED"},
-   {"CLOSE_GRIPPER", SkillJumpState, skills={{}}, final_to="MOVE_GRIPPER_BACK", fail_to="FAILED"},
-   {"MOVE_GRIPPER_BACK", SkillJumpState, skills={{}}, final_to="DRIVE_BACK", fail_to="FAILED"},
+   {"OPEN_GRIPPER", SkillJumpState, skills={{gripper_commands_new}}, final_to="MOVE_GRIPPER"},
+   {"MOVE_GRIPPER_XY", SkillJumpState, skills={{gripper_commands_new}}, final_to="MOVE_GRIPPER_XY", fail_to="FAILED"},
+   {"MOVE_GRIPPER_Z", SkillJumpState, skills={{gripper_commands_new}}, final_to="CLOSE_GRIPPER", fail_to="FAILED"},
+   {"CLOSE_GRIPPER", SkillJumpState, skills={{gripper_commands_new}}, final_to="MOVE_GRIPPER_BACK", fail_to="FAILED"},
+   {"MOVE_GRIPPER_BACK", SkillJumpState, skills={{gripper_commands_new}}, final_to="DRIVE_BACK", fail_to="FAILED"},
    {"DRIVE_BACK", SkillJumpState, skills={{motor_move}}, final_to="FINAL", fail_to="FAILED"},
 }
 
@@ -71,6 +76,32 @@ function GOTO_SHELF:init()
 			{ y = -dest_y, --shelf is on the right side of the conveyor
 				tolerance = { x=0.002, y=0.002, ori=0.01 }
 			}
+end
+
+function OPEN_GRIPPER:init()
+  self.args["gripper_commands_new"].command = "OPEN"
+end
+
+function MOVE_GRIPPER_XY:init()
+  self.args["gripper_commands_new"].x = gripper_x
+  self.args["gripper_commands_new"].y = gripper_y
+  self.args["gripper_commands_new"].command = "MOVEABS"
+end
+
+function MOVE_GRIPPER_Z:init()
+  self.args["gripper_commands_new"].z = gripper_z
+  self.args["gripper_commands_new"].command = "MOVEABS"
+end
+
+
+
+function CLOSE_GRIPPER:init()
+  self.args["gripper_commands_new"].command = "CLOSE"
+end
+
+function MOVE_GRIPPER_BACK:init()
+  self.args["gripper_commands_new"].z = gripper_back_z
+  self.args["gripper_commands_new"].command = "MOVEABS"
 end
 
 function DRIVE_BACK:init()
