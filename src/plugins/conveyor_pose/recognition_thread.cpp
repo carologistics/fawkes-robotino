e863c1d84d60608f7d5702270693418a98c85b58
David Schmidt <david.schmidt@rwth-aachen.de>
2018-04-26 22:17:17 +0200

    conveyor_pose: remove logging output of tf

--- src/plugins/conveyor_pose/recognition_thread.cpp
+++ src/plugins/conveyor_pose/recognition_thread.cpp
@@ -181,30 +181,30 @@ void RecognitionThread::loop()
 }
 
 void RecognitionThread::constrainTransformToGround(fawkes::tf::Stamped<fawkes::tf::Pose>& fittedPose_conv){
-  fawkes::tf::Stamped<fawkes::tf::Pose> fittedPose_base;
-  tf_listener->transform_pose("base_link", fittedPose_conv, fittedPose_base);
-  fawkes::tf::Matrix3x3 basis = fittedPose_base.getBasis();
-  // fawkes::tf::Vector3 yaxis_possibility_row = basis.getRow(1);
-  //printf("\nrow basis: %f/%f/%f\n", yaxis_possibility_row[0], yaxis_possibility_row[1], yaxis_possibility_row[2]);
-  fawkes::tf::Vector3 yaxis_tf = basis.getColumn(1);
-  //printf("col basis: %f/%f/%f\n", yaxis_tf[0], yaxis_tf[1], yaxis_tf[2]);
-  fawkes::tf::Vector3 aligned_y(0, 0, -1);
-  fawkes::tf::Scalar angle = yaxis_tf.angle(aligned_y);
-  fawkes::tf::Vector3 rotational_axis = yaxis_tf.cross(aligned_y);
-  rotational_axis.normalize();
-  fawkes::tf::Quaternion resultRotation(rotational_axis, angle);
-  fawkes::tf::Quaternion poseRotation = fittedPose_base.getRotation();
-  resultRotation *= poseRotation;
-  fittedPose_base.setRotation(resultRotation);
-
-  //sanity check
-  basis = fittedPose_base.getBasis();
-  // yaxis_possibility_row = basis.getRow(1);
-  // printf("row basis: %f/%f/%f", yaxis_possibility_row[0], yaxis_possibility_row[1], yaxis_possibility_row[2]);
-  yaxis_tf = basis.getColumn(1);
-  printf("col basis result: %f/%f/%f\n", yaxis_tf[0], yaxis_tf[1], yaxis_tf[2]);
-
-  // tf_listener->transform_pose(fittedPose_conv.frame_id, fittedPose_base, fittedPose_conv);
+  // fawkes::tf::Stamped<fawkes::tf::Pose> fittedPose_base;
+  // tf_listener->transform_pose("base_link", fittedPose_conv, fittedPose_base);
+  // fawkes::tf::Matrix3x3 basis = fittedPose_base.getBasis();
+  // // fawkes::tf::Vector3 yaxis_possibility_row = basis.getRow(1);
+  // //printf("\nrow basis: %f/%f/%f\n", yaxis_possibility_row[0], yaxis_possibility_row[1], yaxis_possibility_row[2]);
+  // fawkes::tf::Vector3 yaxis_tf = basis.getColumn(1);
+  // //printf("col basis: %f/%f/%f\n", yaxis_tf[0], yaxis_tf[1], yaxis_tf[2]);
+  // fawkes::tf::Vector3 aligned_y(0, 0, -1);
+  // fawkes::tf::Scalar angle = yaxis_tf.angle(aligned_y);
+  // fawkes::tf::Vector3 rotational_axis = yaxis_tf.cross(aligned_y);
+  // rotational_axis.normalize();
+  // fawkes::tf::Quaternion resultRotation(rotational_axis, angle);
+  // fawkes::tf::Quaternion poseRotation = fittedPose_base.getRotation();
+  // resultRotation *= poseRotation;
+  // fittedPose_base.setRotation(resultRotation);
+  //
+  // //sanity check
+  // basis = fittedPose_base.getBasis();
+  // // yaxis_possibility_row = basis.getRow(1);
+  // // printf("row basis: %f/%f/%f", yaxis_possibility_row[0], yaxis_possibility_row[1], yaxis_possibility_row[2]);
+  // yaxis_tf = basis.getColumn(1);
+  // printf("col basis result: %f/%f/%f\n", yaxis_tf[0], yaxis_tf[1], yaxis_tf[2]);
+  //
+  // // tf_listener->transform_pose(fittedPose_conv.frame_id, fittedPose_base, fittedPose_conv);
 }
 
 void MyPointRepresentation::copyToFloatArray (const pcl::PointNormal &p, float * out) const
