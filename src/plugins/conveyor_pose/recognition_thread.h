bb30f5b29781211b570610d43c838d4585212407
Victor Mataré <matare@fh-aachen.de>
2018-05-15 18:16:12 +0200

    conveyor_pose: implement hypothesis verification
    This is a major behaviour change that also introduces lots of new
    tunables. The config should now reflect the recognition logic much more
    clearly. All magic constants are now configurable, and the hypothesis
    verification should allow for precise calibration that eliminates those
    pesky false positives.
    
    Everything is yet untested and all newly introduced config parameters
    are wild guesses.

--- src/plugins/conveyor_pose/recognition_thread.h
+++ src/plugins/conveyor_pose/recognition_thread.h
@@ -8,6 +8,7 @@
 #include <atomic>
 
 #include <pcl/registration/icp_nl.h>
+#include <pcl/recognition/hv/hv_papazov.h>
 
 #include "conveyor_pose_thread.h"
 
@@ -17,6 +18,20 @@ public:
   using pcl::IterativeClosestPointNonLinear<pcl::PointNormal, pcl::PointNormal>::IterativeClosestPointNonLinear;
 
   double getScaledFitness();
+};
+
+
+// Define a new point representation for < x, y, z, curvature >
+class MyPointRepresentation : public pcl::PointRepresentation<pcl::PointNormal>
+{
+public:
+  MyPointRepresentation()
+  {
+    nr_dimensions_ = 4;
+  }
+
+  // Override the copyToFloatArray method to define our feature vector
+  virtual void copyToFloatArray (const pcl::PointNormal &p, float * out) const override;
 };
 
 
@@ -33,27 +48,32 @@ public:
 
   virtual void loop() override;
   virtual void init() override;
+
+  void restart_icp();
+
+  void publish_result();
+
   void constrainTransformToGround(fawkes::tf::Stamped<fawkes::tf::Pose>& fittedPose_conv);
 
 private:
   ConveyorPoseThread *main_thread_;
 
+  fawkes::WaitCondition wait_enabled_;
+  std::atomic_bool enabled_;
+
   fawkes::tf::Stamped<fawkes::tf::Pose> initial_guess_icp_odom_;
-  double initial_guess_tracked_fitness_;
-};
+  Eigen::Matrix4f initial_tf_;
+  pcl::PointCloud<pcl::PointNormal>::Ptr model_with_normals_;
+  pcl::PointCloud<pcl::PointNormal>::Ptr scene_with_normals_;
 
+  CustomICP icp_;
+  pcl::PapazovHV<pcl::PointNormal, pcl::PointNormal> hypot_verif_;
+  Eigen::Matrix4f prev_last_tf_;
+  pcl::PointCloud<pcl::PointNormal>::Ptr icp_result_;
+  Eigen::Matrix4f final_tf_;
 
-// Define a new point representation for < x, y, z, curvature >
-class MyPointRepresentation : public pcl::PointRepresentation<pcl::PointNormal>
-{
-public:
-  MyPointRepresentation()
-  {
-    nr_dimensions_ = 4;
-  }
-
-  // Override the copyToFloatArray method to define our feature vector
-  virtual void copyToFloatArray (const pcl::PointNormal &p, float * out) const override;
+  unsigned int iterations_;
+  double last_raw_fitness_;
 };
 
 
