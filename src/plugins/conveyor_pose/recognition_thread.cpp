67887ff89563a1b236b648823cb10c65e702d3e1
Victor Mataré <matare@lih.rwth-aachen.de>
2018-05-27 14:59:37 +0200

    conveyor_pose: restructure to clarify thread data
    In particular, remove friend declaration for the two worker threads and
    move members so that shared members are more clearly identifiable

--- src/plugins/conveyor_pose/recognition_thread.cpp
+++ src/plugins/conveyor_pose/recognition_thread.cpp
@@ -25,7 +25,7 @@ double CustomICP::getScaledFitness()
 
 
 RecognitionThread::RecognitionThread(ConveyorPoseThread *cp_thread)
-  : Thread("CorrespondenceGrouping", Thread::OPMODE_CONTINUOUS)
+  : Thread("ICPThread", Thread::OPMODE_CONTINUOUS)
   , TransformAspect(fawkes::TransformAspect::BOTH, "conveyor_pose_initial_guess")
   , main_thread_(cp_thread)
   , enabled_(false)
@@ -53,21 +53,21 @@ void RecognitionThread::restart_icp()
 
   { fawkes::MutexLocker locked { &main_thread_->cloud_mutex_ };
     std::vector<int> tmp;
-    model_with_normals_ = main_thread_->model_;
-    pcl::removeNaNFromPointCloud(*model_with_normals_, *model_with_normals_, tmp);
-    scene_with_normals_ = main_thread_->scene_;
-    pcl::removeNaNFromPointCloud(*scene_with_normals_, *scene_with_normals_, tmp);
+    model_ = main_thread_->model_;
+    pcl::removeNaNFromPointCloud(*model_, *model_, tmp);
+    scene_ = main_thread_->scene_;
+    pcl::removeNaNFromPointCloud(*scene_, *scene_, tmp);
 
     try {
       if (!main_thread_->have_laser_line_) {
         tf_listener->transform_pose(
-              scene_with_normals_->header.frame_id,
+              scene_->header.frame_id,
               tf::Stamped<tf::Pose>(initial_guess_icp_odom_, Time(0,0), initial_guess_icp_odom_.frame_id),
               initial_pose_cam);
       }
       else {
         tf_listener->transform_pose(
-              scene_with_normals_->header.frame_id,
+              scene_->header.frame_id,
               tf::Stamped<tf::Pose>(main_thread_->initial_guess_laser_odom_, Time(0,0),
                                     main_thread_->initial_guess_laser_odom_.frame_id),
               initial_pose_cam);
@@ -79,11 +79,11 @@ void RecognitionThread::restart_icp()
     }
   }
 
-  hypot_verif_.setSceneCloud(scene_with_normals_);
-  hypot_verif_.setResolution(main_thread_->cfg_voxel_grid_leaf_size_);
-  hypot_verif_.setInlierThreshold(main_thread_->cfg_icp_hv_inlier_thresh_);
-  hypot_verif_.setPenaltyThreshold(main_thread_->cfg_icp_hv_penalty_thresh_);
-  hypot_verif_.setSupportThreshold(main_thread_->cfg_icp_hv_support_thresh_);
+  hypot_verif_.setSceneCloud(scene_);
+  hypot_verif_.setResolution(main_thread_->cloud_resolution());
+  hypot_verif_.setInlierThreshold(cfg_icp_hv_inlier_thresh_);
+  hypot_verif_.setPenaltyThreshold(cfg_icp_hv_penalty_thresh_);
+  hypot_verif_.setSupportThreshold(cfg_icp_hv_support_thresh_);
 
   initial_tf_ = pose_to_eigen(initial_pose_cam);
 
@@ -97,13 +97,13 @@ void RecognitionThread::restart_icp()
   icp_ = CustomICP();
 
   // Set tunables
-  icp_.setTransformationEpsilon(std::pow(main_thread_->cfg_icp_tf_epsilon_, 2));
-  icp_.setMaxCorrespondenceDistance(double(main_thread_->cfg_icp_max_corr_dist_));
-  icp_.setInputTarget(scene_with_normals_);
-  icp_.setMaximumIterations(main_thread_->cfg_icp_max_iterations_);
+  icp_.setTransformationEpsilon(std::pow(cfg_icp_tf_epsilon_, 2));
+  icp_.setMaxCorrespondenceDistance(double(cfg_icp_max_corr_dist_));
+  icp_.setInputTarget(scene_);
+  icp_.setMaximumIterations(cfg_icp_max_iterations_);
 
   // Run first alignment with initial estimate
-  icp_.setInputSource(model_with_normals_);
+  icp_.setInputSource(model_);
   icp_.align(*icp_result_, initial_tf_);
   final_tf_ = icp_.getFinalTransformation();
 
@@ -114,7 +114,7 @@ void RecognitionThread::restart_icp()
 
 void RecognitionThread::loop()
 {
-  if (!enabled_) {
+  if (!enabled_|| do_restart_) {
     if (!do_restart_) {
       logger->log_info(name(), "ICP stopped");
 
@@ -125,7 +125,7 @@ void RecognitionThread::loop()
     restart_icp();
   }
 
-  if (!enabled_) // cancel if disabled from ConveyorPoseThread
+  if (!enabled_ || do_restart_) // cancel if disabled from ConveyorPoseThread
     return;
 
   icp_.setInputSource(icp_result_);
@@ -134,7 +134,7 @@ void RecognitionThread::loop()
   //accumulate transformation between each Iteration
   final_tf_ = icp_.getFinalTransformation() * final_tf_;
 
-  if (!enabled_) // cancel if disabled from ConveyorPoseThread
+  if (!enabled_ || do_restart_) // cancel if disabled from ConveyorPoseThread
     return;
 
   //if the difference between this transformation and the previous one
@@ -142,15 +142,17 @@ void RecognitionThread::loop()
   //the maximal correspondence distance
   bool epsilon_reached = false;
   if (double(std::abs((icp_.getLastIncrementalTransformation() - prev_last_tf_).sum())) < icp_.getTransformationEpsilon()) {
-    icp_.setMaxCorrespondenceDistance(icp_.getMaxCorrespondenceDistance () * main_thread_->cfg_icp_refinement_factor_);
+    icp_.setMaxCorrespondenceDistance(icp_.getMaxCorrespondenceDistance () * cfg_icp_refinement_factor_);
     epsilon_reached = true;
   }
   prev_last_tf_ = icp_.getLastIncrementalTransformation();
 
-  if (iterations_++ >= main_thread_->cfg_icp_min_loops_ || epsilon_reached) {
+  if (iterations_++ >= cfg_icp_min_loops_ || epsilon_reached) {
     // Perform hypothesis verification
     std::vector<Cloud::ConstPtr> icp_result_vector { icp_result_ };
-    hypot_verif_.setSceneCloud(scene_with_normals_);
+
+    // This resets the result vectors (see hypot_mask below) so we only ever verify the last ICP result
+    hypot_verif_.setSceneCloud(scene_);
     hypot_verif_.addModels(icp_result_vector, true);
 
     hypot_verif_.verify();
@@ -164,13 +166,13 @@ void RecognitionThread::loop()
       logger->log_info(name(), "FIT!");
     }
 
-    if (iterations_ >= main_thread_->cfg_icp_max_loops_) {
+    if (iterations_ >= cfg_icp_max_loops_) {
       if (last_raw_fitness_ >= std::numeric_limits<double>::max() - 1) {
         logger->log_warn(name(), "No acceptable fit after %u iterations", iterations_);
         restart_icp();
       }
       else {
-        if (main_thread_->cfg_icp_auto_restart_)
+        if (cfg_icp_auto_restart_)
           restart_icp();
         else
           enabled_ = false;
@@ -189,14 +191,12 @@ void RecognitionThread::publish_result()
 
   tf::Stamped<tf::Pose> result_pose {
     eigen_to_pose(final_tf_),
-    Time { long(scene_with_normals_->header.stamp) / 1000 },
-    scene_with_normals_->header.frame_id
+    Time { long(scene_->header.stamp) / 1000 },
+    scene_->header.frame_id
   };
 
   // constrainTransformToGround(result_pose);
 
-  /*double new_fitness = (1 / reg.getFitnessScore())
-      / double(std::min(model_with_normals->size(), scene_with_normals->size()));*/
   double new_fitness = (1 / icp_.getFitnessScore()) / 10000;
 
   { MutexLocker locked2(&main_thread_->bb_mutex_);
@@ -239,6 +239,7 @@ void RecognitionThread::constrainTransformToGround(fawkes::tf::Stamped<fawkes::t
   tf_listener->transform_pose(fittedPose_conv.frame_id, fittedPose_base, fittedPose_conv);
 }
 
+
 void RecognitionThread::enable()
 {
   enabled_ = true;
@@ -252,8 +253,8 @@ void RecognitionThread::disable()
 
 void RecognitionThread::restart()
 {
-  disable();
   do_restart_ = true;
+  enable();
 }
 
 
