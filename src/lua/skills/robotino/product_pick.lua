fa913d65dcea9778b0a32c28e345ffd35fb620b4
Carsten Stoffels <carsten.stoffels@rwth-aachen.de>
2018-05-29 18:14:50 +0200

    skills/product_pick: Added init gripper pos and second move down state

--- src/lua/skills/robotino/product_pick.lua
+++ src/lua/skills/robotino/product_pick.lua
@@ -48,8 +48,15 @@ local gripper_tolerance_z = 0.5 -- gripper z tolerance according to conveyor pos
 
 local gripper_forward_x = 0 -- distance to move gripper forward after align
 local gripper_down_z = 0    -- distance to move gripper down after driving over product
+local gripper_down_z = 0    -- distance to mover gripper down second time
 local gripper_back_x = 0    -- distance to move gripper back after closing gripper
 local drive_back_x = -0.2   -- distance to drive back after closing the gripper
+
+local gripper_init_x = 0 -- initial x position of the gripper
+local gripper_init_y = 0 -- initial y position of the gripper
+local gripper_init_z = 0 -- initial z position of the gripper
+
+local align_target_frame = "gripper_home" -- the gripper align is made relative to this frame (according to gripper_commands_new)
 
 local cfg_frame_ = "gripper"
 
@@ -96,21 +103,24 @@ end
 
 fsm:define_states{ export_to=_M, closure={gripper_if=gripper_if},
    {"INIT", JumpState},
+   {"INIT_GRIPPER", SkillJumpState, skills={{gripper_commands_new}}, final_to="OPEN_GRIPPER", fail_to="CLEANUP_FAILED"},
    {"OPEN_GRIPPER", SkillJumpState, skills={{gripper_commands_new}},final_to="CHECK_VISION", fail_to="CLEANUP_FAILED"},
    {"CHECK_VISION", JumpState},
    {"GRIPPER_ALIGN", SkillJumpState, skills={{gripper_commands_new}}, final_to="CHECK_TOLERANCE",fail_to="CLEANUP_FAILED"},
    {"CHECK_TOLERANCE",JumpState},
    {"MOVE_GRIPPER_FORWARD", SkillJumpState, skills={{gripper_commands_new}}, final_to="MOVE_GRIPPER_DOWN",fail_to="CLEANUP_FAILED"},
    {"MOVE_GRIPPER_DOWN", SkillJumpState, skills={{gripper_commands_new}}, final_to="CLOSE_GRIPPER", fail_to="CLEANUP_FAILED"},
-   {"CLOSE_GRIPPER", SkillJumpState, skills={{gripper_commands_new}}, final_to="MOVE_GRIPPER_BACK", fail_to="CLEANUP_FAILED"},
-   {"MOVE_GRIPPER_BACK", SkillJumpState, skills={{gripper_commands_new}}, final_to = "DRIVE_BACK", fail_to="CLEANUP_FAILED"},
+   {"CLOSE_GRIPPER", SkillJumpState, skills={{gripper_commands_new}}, final_to="MOVE_GRIPPER_DOWN_SECOND", fail_to="CLEANUP_FAILED"},
+   {"MOVE_GRIPPER_DOWN_SECOND", SkillJumpState, skills={{gripper_commands_new}}, final_to="MOVE_GRIPPER_BACK", fail_to="CLEANUP_FAILED"},
+   {"MOVE_GRIPPER_BACK", SkillJumpState, skills={{gripper_commands_new}}, final_to = "HOME_GRIPPER", fail_to="CLEANUP_FAILED"},
+   {"HOME_GRIPPER", SkillJumpState, skills={{gripper_commands_new}}, final_to="DRIVE_BACK"}
    {"DRIVE_BACK", SkillJumpState, skills={{motor_move}}, final_to="CLEANUP_FINAL", fail_to="CLEANUP_FAILED"},
    {"CLEANUP_FINAL", JumpState},
    {"CLEANUP_FAILED", JumpState},
 }
 
 fsm:add_transitions{
-   {"INIT", "OPEN_GRIPPER", true, desc="Start open gripper"},
+   {"INIT", "INIT_GRIPPER", true, desc="Init gripper for product_pick"},
    {"CHECK_VISION", "CLEANUP_FAILED", timeout=20, desc="Fitness threshold wasn't reached"},
    {"CHECK_VISION", "CLEANUP_FAILED", cond=no_writer, desc="No writer for conveyor vision"},
    {"CHECK_VISION", "GRIPPER_ALIGN", cond=icp_fitness_check, desc="Fitness threshold reached"},
@@ -126,16 +136,24 @@ function INIT:init()
   if_conveyor_pose:msgq_enqueue_copy(if_conveyor_pose.SetStationMessage:new(if_conveyor_pose.BASE_STATION,if_conveyor_pose.INPUT_CONVEYOR))
 end
 
+function INIT_GRIPPER:init()
+  self.args["gripper_commands_new"].command = "MOVEABS"
+  self.args["gripper_commands_new"].x = gripper_init_x
+  self.args["gripper_commands_new"].y = gripper_init_y
+  self.args["gripper_commands_new"].z = gripper_init_z
+end
+
 function OPEN_GRIPPER:init()
   self.args["gripper_commands_new"].command = "OPEN"
 end
 
 function GRIPPER_ALIGN:init()
   local pose = pose_offset(self)
-  --self.args["gripper_commands_new"].command = "MOVEABS"
+  self.args["gripper_commands_new"].command = "MOVEABS"
   self.args["gripper_commands_new"].x = pose.x
   self.args["gripper_commands_new"].y = pose.y
   self.args["gripper_commands_new"].z = pose.z
+  self.args["gripper_commands_new"].target_frame  = align_target_frame
 end
 
 function MOVE_GRIPPER_FORWARD:init()
@@ -144,13 +162,26 @@ function MOVE_GRIPPER_FORWARD:init()
 end
 
 function MOVE_GRIPPER_DOWN:init()
-  --self.args["gripper_commands_new"].command = "MOVEABS"
+  self.args["gripper_commands_new"].command = "MOVEABS"
   self.args["gripper_commands_new"].z = gripper_down_z
 end
 
+function MOVE_GRIPPER_DOWN_SECOND:init()
+  self.args["gripper_commands_new"].z = gripper_down_second_z
+  self.args["gripper_commands_new"].command = "MOVEABS"
+end
+
 function MOVE_GRIPPER_BACK:init()
-  --self.args["gripper_commands_new"].command = "MOVEABS"
+  self.args["gripper_commands_new"].command = "MOVEABS"
   self.args["gripper_commands_new"].x = gripper_back_x
+end
+
+
+function HOME_GRIPPER:init()
+  self.args["gripper_commands_new"].x = 0
+  self.args["gripper_commands_new"].y = 0
+  self.args["gripper_commands_new"].z = 0
+  self.args["gripper_commands_new"].command = "MOVEABS"
 end
 
 function DRIVE_BACK:init()
