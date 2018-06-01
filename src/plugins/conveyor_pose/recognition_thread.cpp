ddf346024b89b01609f22dbcfea8c9e3994c66d6
Victor Mataré <matare@fh-aachen.de>
2018-06-01 20:51:48 +0200

    conveyor_pose: Set "busy" & "msgid" fields in blackboard

--- src/plugins/conveyor_pose/recognition_thread.cpp
+++ src/plugins/conveyor_pose/recognition_thread.cpp
@@ -46,6 +46,10 @@ void RecognitionThread::restart_icp()
 
   syncpoint_clouds_ready_->wait(name());
 
+  { fawkes::MutexLocker locked { &main_thread_->bb_mutex_ };
+    main_thread_->bb_set_busy(true);
+  }
+
   tf::Stamped<tf::Pose> initial_pose_cam;
 
   { fawkes::MutexLocker locked { &main_thread_->cloud_mutex_ };
@@ -116,6 +120,11 @@ void RecognitionThread::loop()
 {
   if (!enabled_) {
     logger->log_info(name(), "ICP stopped");
+
+    { MutexLocker locked { &main_thread_->bb_mutex_ };
+      main_thread_->bb_set_busy(false);
+    }
+
     while (!enabled_)
       syncpoint_clouds_ready_->wait(name());
   }
