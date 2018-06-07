5fc75919ba347c6b59b00b023398a3f7291ff9f4
Nicolas Limpert <limpert@fh-aachen.de>
2018-06-07 17:54:03 +0200

    skills/get_product_from: adapt to drive_to_machine_point

--- src/lua/skills/robotino/get_product_from.lua
+++ src/lua/skills/robotino/get_product_from.lua
@@ -25,7 +25,7 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "get_product_from"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=false}
-depends_skills     = {"product_pick", "drive_to_local", "conveyor_align"}
+depends_skills     = {"product_pick", "drive_to_machine_point", "conveyor_align"}
 depends_interfaces = {
   {v = "if_conveyor_pose", type = "ConveyorPoseInterface", id="conveyor_pose/status"},
 }
@@ -44,6 +44,7 @@ Parameters:
 -- Initialize as skill module
 skillenv.skill_module(_M)
 -- Constants
+local X_AT_MPS = 0.4
 
 function already_at_conveyor(self)
    return (self.fsm.vars.atmps == "CONVEYOR")
@@ -52,7 +53,7 @@ end
 
 fsm:define_states{ export_to=_M, closure={navgraph=navgraph},
    {"INIT", JumpState},
-   {"DRIVE_TO", SkillJumpState, skills={{drive_to_local}}, final_to="CONVEYOR_ALIGN", fail_to="FAILED"},
+   {"DRIVE_TO_MACHINE_POINT", SkillJumpState, skills={{drive_to_machine_point}}, final_to="CONVEYOR_ALIGN", fail_to="FAILED"},
    {"CONVEYOR_ALIGN", SkillJumpState, skills={{conveyor_align}}, final_to="PRODUCT_PICK", fail_to="FAILED"},
    {"PRODUCT_PICK", SkillJumpState, skills={{product_pick}}, final_to="FINAL", fail_to="FAILED"},
 }
@@ -61,7 +62,7 @@ fsm:add_transitions{
    {"INIT", "FAILED", cond="not navgraph", desc="navgraph not available"},
    {"INIT", "FAILED", cond="not vars.node:is_valid()", desc="point invalid"},
    {"INIT", "CONVEYOR_ALIGN", cond=already_at_conveyor, desc="Already in front of the mps, align"},
-   {"INIT", "DRIVE_TO", cond=true, desc="Everything OK"},
+   {"INIT", "DRIVE_TO_MACHINE_POINT", cond=true, desc="Everything OK"},
 }
 
 function INIT:init()
@@ -94,11 +95,15 @@ function INIT:init()
    end
 end
 
-function DRIVE_TO:init()
+function DRIVE_TO_MACHINE_POINT:init()
+   local option = "CONVEYOR"
+   if self.fsm.vars.shelf ~= nil then
+     option = "SHELF_" .. self.fsm.vars.shelf
+   end
    if self.fsm.vars.side == "input" or self.fsm.vars.shelf then
-      self.args["drive_to_local"] = {place = self.fsm.vars.place .. "-I"}
+      self.args["drive_to_machine_point"] = {place = self.fsm.vars.place .. "-I", option = option, x_at_mps=X_AT_MPS}
    else --if no side is given drive to output
-      self.args["drive_to_local"] = {place = self.fsm.vars.place .. "-O"}
+      self.args["drive_to_machine_point"] = {place = self.fsm.vars.place .. "-O", option = option, x_at_mps=X_AT_MPS}
    end
 end
 
