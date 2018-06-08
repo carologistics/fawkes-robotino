2e760996287e3c1105dccbfaf95b2ebec190c83d
Victor Mataré <matare@fh-aachen.de>
2018-06-08 19:33:07 +0200

    conveyor_pose.lua: make it more robust
    Wait until ICP has converged & actually check resulting pose.

--- src/lua/skills/robotino/conveyor_align.lua
+++ src/lua/skills/robotino/conveyor_align.lua
@@ -21,7 +21,7 @@ module(..., skillenv.module_init)
 
 -- Crucial skill information
 name               = "conveyor_align"
-fsm                = SkillHSM:new{name=name, start="INIT", debug=false}
+fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
 depends_skills     = {"motor_move", "gripper_commands_new"}
 depends_interfaces = {
    {v = "motor", type = "MotorInterface", id="Robotino" },
@@ -47,22 +47,26 @@ local pam = require("parse_module")
 
 -- Constants
 local euclidean_fitness_threshold = 8 -- threshold for euclidean fitness  (fitness should be higher)
-local tolerance_x = 0.5  -- x-tolerance according to conveyor pose
-local gripper_x = 0.05  -- gripper x-coordinate before the align
-local gripper_y = 0  -- gripper y-coordinate before the align
-local gripper_z = 0.035  -- gripper z-coordinate before the align
+local tolerance_trans = 0.015
+local tolerance_ori = 0.015
 local x_dist_to_mps = -0.31  -- x-distance the robot should have after the align
 local cfg_frame_ = "gripper"
+
+local GRIPPER_POSES = {
+   {x=0.05, y=0.00, z=0.035},
+   {x=0.05, y=0.02, z=0.03},
+   {x=0.05, y=-0.02, z=0.03}
+}
 
 function no_writer()
    return not if_conveyor_pose:has_writer()
 end
 
-function tolerance_check(self)
+function tolerance_ok(self)
    local pose = pose_offset(self)
-   if math.abs(pose.x) <= tolerance_x then
-      return true
-   end
+   return math.abs(pose.x) <= tolerance_trans
+      and math.abs(pose.y) <= tolerance_trans
+      and math.abs(pose.ori) <= tolerance_ori
 end
 
 function drive_ready_check(self)
@@ -77,77 +81,85 @@ function drive_ready_check(self)
   end
 
   return if_conveyor_pose:euclidean_fitness() > euclidean_fitness_threshold
+     and not if_conveyor_pose:is_busy()
+     and if_conveyor_pose:msgid() == fsm.vars.msgid
 end
 
 function pose_offset(self)
-
       local target_pos = { x = x_dist_to_mps,
                            y = 0,
                            ori = 0,
       }
 
       local transformed_pos = tfm.transform(target_pos, "conveyor_pose", "base_link")
-      print_info("target_pos is x = %f, y = %f, ori = %f", target_pos.x, target_pos.y, target_pos.ori)
       print_info("transformed_pos is x = %f, y = %f,ori = %f", transformed_pos.x, transformed_pos.y, transformed_pos.ori)
 
-      return { x = transformed_pos.x ,
-               y = transformed_pos.y,
-               ori = transformed_pos.ori,
-      }
+      return transformed_pos
 end
 
 
 fsm:define_states{ export_to=_M,
    closure={},
    {"INIT", JumpState},
-   {"MOVE_GRIPPER", SkillJumpState, skills={{gripper_commands_new}}, final_to="CHECK_VISION", failed_to="CLEANUP_FAILED"},
+   {"MOVE_GRIPPER", SkillJumpState, skills={{gripper_commands_new}}, final_to="CHECK_VISION", failed_to="FAILED"},
    {"CHECK_VISION", JumpState},
-   {"DRIVE_FORWARD", SkillJumpState, skills={{motor_move}}, final_to="CHECK_TOLERANCE", failed_to="CLEANUP_FAILED"},
+   {"DRIVE_FORWARD", SkillJumpState, skills={{motor_move}}, final_to="CHECK_TOLERANCE", failed_to="FAILED"},
    {"CHECK_TOLERANCE", JumpState},
-   {"CLEANUP_FINAL", JumpState},
-   {"CLEANUP_FAILED", JumpState},
 }
 
 fsm:add_transitions{
    {"INIT", "MOVE_GRIPPER", cond=true},
-   {"CHECK_VISION", "CLEANUP_FAILED", timeout=20, desc = "Fitness threshold wasn't reached"},
-   {"CHECK_VISION", "CLEANUP_FAILED", cond=no_writer, desc="No writer for conveyor vision"},
+   {"CHECK_VISION", "FAILED", timeout=20, desc = "Fitness threshold wasn't reached"},
+   {"CHECK_VISION", "FAILED", cond=no_writer, desc="No writer for conveyor vision"},
    {"CHECK_VISION", "DRIVE_FORWARD", cond=drive_ready_check, desc="Fitness threshold reached"},
-   {"CHECK_TOLERANCE", "CLEANUP_FINAL", cond=tolerance_check, desc="Pose tolerance ok"},
+   {"CHECK_TOLERANCE", "FINAL", cond=tolerance_ok, desc="Pose tolerance ok"},
    {"CHECK_TOLERANCE", "CHECK_VISION", cond = true, desc="Pose tolerance not ok"},
-   {"CLEANUP_FINAL", "FINAL", cond=true, desc="Cleaning up after final"},
-   {"CLEANUP_FAILED", "FAILED", cond=true, desc="Cleaning up after fail"},
 }
 
 function INIT:init()
+   self.fsm.vars.gripper_pose_idx = 1
    if_conveyor_switch:msgq_enqueue_copy(if_conveyor_switch.EnableSwitchMessage:new())
    local parse_result = pam.parse_to_type_target(if_conveyor_pose,self.fsm.vars.place,self.fsm.vars.side,self.fsm.vars.shelf,self.fsm.vars.slide)
-   if_conveyor_pose:msgq_enqueue_copy(if_conveyor_pose.SetStationMessage:new(parse_result.mps_type,parse_result.mps_target))
+   self.fsm.vars.mps_type = parse_result.mps_type
+   self.fsm.vars.mps_target = parse_result.mps_target
+end
+
+function CHECK_VISION:init()
+   local msg = if_conveyor_pose.SetStationMessage:new(self.fsm.vars.mps_type, self.fsm.vars.mps_target)
+   if_conveyor_pose:msgq_enqueue_copy(msg)
+   self.fsm.vars.msgid = msg:id()
 end
 
 function MOVE_GRIPPER:init()
-  print_info("Move gripper to %f,%f,%f", gripper_x, gripper_y, gripper_z)
+  self.args["gripper_commands_new"] = GRIPPER_POSES[self.fsm.vars.gripper_pose_idx]
   self.args["gripper_commands_new"].command = "MOVEABS"
-  self.args["gripper_commands_new"].x = gripper_x
-  self.args["gripper_commands_new"].y = gripper_y
-  self.args["gripper_commands_new"].z = gripper_z
+  self.fsm.vars.gripper_pose_idx = (self.fsm.vars.gripper_pose_idx + 1) % #GRIPPER_POSES
 end
-
 
 function DRIVE_FORWARD:init()
-  local pose = pose_offset(self)
-  print_info("Drive forward, x = %f , y = %f", pose.x, pose.y)
-  self.args["motor_move"] = pose
+   local pose = pose_offset(self)
+   print_info("Drive forward, x = %f , y = %f", pose.x, pose.y)
+   self.args["motor_move"].x = pose.x
+   self.args["motor_move"].y = pose.y
+   self.args["motor_move"].ori = pose.ori
+   self.args["motor_move"].tolerance = {
+      x = tolerance_trans * 0.9,
+      y = tolerance_trans * 0.9,
+      ori = tolerance_ori * 0.9
+   }
 end
 
-function CLEANUP_FINAL:init()
-   if (self.fsm.vars.disable_realsense_afterwards == nil or self.fsm.vars.disable_realsense_afterwards) then
+function cleanup()
+   if (fsm.vars.disable_realsense_afterwards == nil or fsm.vars.disable_realsense_afterwards) then
      if_conveyor_switch:msgq_enqueue_copy(if_conveyor_switch.DisableSwitchMessage:new())
    end
 end
 
-function CLEANUP_FAILED:init()
-   if (self.fsm.vars.disable_realsense_afterwards == nil or self.fsm.vars.disable_realsense_afterwards) then
-     if_conveyor_switch:msgq_enqueue_copy(if_conveyor_switch.DisableSwitchMessage:new())
-   end
+function FAILED:init()
+   cleanup()
 end
+
+function FINAL:init()
+   cleanup()
+end
+
