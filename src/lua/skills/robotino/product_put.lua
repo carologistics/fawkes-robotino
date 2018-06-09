a57cb4f7d7b33d541e7858f8fc6dba0e4a3f61a0
Carsten Stoffels <carsten.stoffels@rwth-aachen.de>
2018-06-09 22:54:42 +0200

    skills/product_put: tuned z-movement

--- src/lua/skills/robotino/product_put.lua
+++ src/lua/skills/robotino/product_put.lua
@@ -57,7 +57,7 @@ local gripper_pose_offset_y = 0     -- conveyor_pose offset in y direction
 local gripper_pose_offset_z = 0.065  -- conveyor_pose offset in z direction
 
 local conveyor_gripper_forward_x = 0.04 -- distance to move gripper forward after align
-local conveyor_gripper_down_z = 0.01    -- distance to move gripper down after driving over conveyor
+local conveyor_gripper_down_z = -0.01    -- distance to move gripper down after driving over conveyor
 local conveyor_gripper_back_x = -0.06   -- distance to move gripper back after closing gripper
 
 local slide_gripper_forward_x = 0.04  -- distance to move gripper forward after align if the target is slide
