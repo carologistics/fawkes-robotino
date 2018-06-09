4e561c17e132b64469d5647bcbf8a71fc7c83602
Victor Mataré <matare@fh-aachen.de>
2018-06-09 18:05:15 +0200

    skills/bring_product_to: handle slide case

--- src/lua/skills/robotino/bring_product_to.lua
+++ src/lua/skills/robotino/bring_product_to.lua
@@ -79,10 +79,14 @@ end
 
 function DRIVE_TO_MACHINE_POINT:init()
    local option = "CONVEYOR"
-   if self.fsm.vars.shelf ~= nil then
+
+   if self.fsm.vars.slide then
+      option = "SLIDE"
+   elseif self.fsm.vars.shelf then
      option = "SHELF_" .. self.fsm.vars.shelf
    end
-   if self.fsm.vars.side == "input" or self.fsm.vars.shelf then
+
+   if self.fsm.vars.side == "input" or self.fsm.vars.shelf or self.fsm.vars.slide then
       self.fsm.vars.tag_id = navgraph:node(self.fsm.vars.place):property_as_float("tag_input")
       self.args["drive_to_machine_point"] = {place = self.fsm.vars.place .. "-I", option = option, x_at_mps=X_AT_MPS, tag_id=self.fsm.vars.tag_id}
    else --if no side is given drive to output
