23a092c186667466002323db0d2468860e2b2ffc
Carsten Stoffels <carsten.stoffels@rwth-aachen.de>
2018-06-17 17:47:41 -0400

    skills/product_put: reduce align tolerance

--- src/lua/skills/robotino/product_put.lua
+++ src/lua/skills/robotino/product_put.lua
@@ -48,9 +48,9 @@ local pam = require("parse_module")
 
 -- Constants
 local euclidean_fitness_threshold = 8  --threshold for euclidean fitness  (fitness should be higher)
-local gripper_tolerance_x = 0.015-- gripper x tolerance according to conveyor pose
-local gripper_tolerance_y = 0.015 -- gripper y tolerance according to conveyor pose
-local gripper_tolerance_z = 0.015 -- gripper z tolerance according to conveyor pose
+local gripper_tolerance_x = 0.01-- gripper x tolerance according to conveyor pose
+local gripper_tolerance_y = 0.01 -- gripper y tolerance according to conveyor pose
+local gripper_tolerance_z = 0.01 -- gripper z tolerance according to conveyor pose
 
 local gripper_pose_offset_x = -0.02  -- conveyor pose offset in x direction
 local gripper_pose_offset_y = 0.00     -- conveyor_pose offset in y direction
