02d68d848cbc0dbdfc3d829fa5b805cdaa2e7139
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-10-03 16:05:14 +0200

    buildkite: modify pipeline to pass along fawkes-robotino deploy key
    Extend on-the-fly Fawkes pipeline modification to extend the passed
    deploy key env vars by one for the private repository.
    
    Slightly restructure the sed command to be clearer and easier to extend.
    Update documentation to more clearly describe what this does.

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -2,14 +2,18 @@
 
 SCRIPT_PATH=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
-# For now, we simply use the Fawkes Buildkite pipeline. We do need to adapt
-# the script paths. Therefore, we use sed to adapt the script.
+# For now, we simply use the Fawkes Buildkite pipeline. We use sed to
+# slightly modify the script:
+# - adapt the script path to point to the fawkes submodule scripts
+# - add the fawkes-robotino deploy SSH key env var to the docker call
 #
 # Additional steps can be added at the end by outputting additional steps.
 # Note that these jobs will be appended at the end, that is in the build job
 # section. Add a wait step to wait for the completion of the builds.
 
-sed -e 's|\.buildkite/|fawkes/.buildkite/|g' $SCRIPT_PATH/../fawkes/.buildkite/pipeline.yml
+sed $SCRIPT_PATH/../fawkes/.buildkite/pipeline.yml \
+	-e 's|\.buildkite/|fawkes/.buildkite/|g' \
+	-e 's/SSH_DEPLOY_PRIVKEY_COMMITTERS/SSH_DEPLOY_PRIVKEY_COMMITTERS\n          - SSH_DEPLOY_PRIVKEY_FAWKES_ROBOTINO/g'
 
 # Example to add more steps:
 # Note, that the steps must be properly indented as if it were
