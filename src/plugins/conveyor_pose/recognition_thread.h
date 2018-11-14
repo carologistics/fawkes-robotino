9d2d0d5ea6681033a784aaf462288768f5728922
Victor Mataré <matare@fh-aachen.de>
2018-11-14 15:03:11 +0100

    conveyor_pose: fix license headers

--- src/plugins/conveyor_pose/recognition_thread.h
+++ src/plugins/conveyor_pose/recognition_thread.h
@@ -1,3 +1,24 @@
+/***************************************************************************
+ *  recognition_thread.h - Align scene to model & publish the TF
+ *
+ *  Created: Tue Apr 10 15:41:58 2018 +0200
+ *  Copyright  2018 Victor Mataré
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
 #ifndef CORRESPONDENCE_GROUPING_THREAD_H
 #define CORRESPONDENCE_GROUPING_THREAD_H
 
