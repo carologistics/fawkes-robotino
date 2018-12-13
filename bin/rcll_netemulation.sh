04a6bff0261a6458f93dfc75c7dd016f11df0fa9
Christoph Gollok <christoph.gollok@alumni.fh-aachen.de>
2018-12-13 13:48:34 +0100

    netemulation: add documentation to netemulation script

--- bin/rcll_netemulation.sh
+++ bin/rcll_netemulation.sh
@@ -12,27 +12,38 @@ EOF
 
 function setup_rules() {
 
-    # prio creates 3 classes: 
-    # x:0| qdisc itself  
-    # x:1| class 1  
-    # x:2| class 2 
-    # x:3| class 3
+   # The classless netem qdisc does not allow use of filters.
+   # In order to apply filters to netem qdisc it has to be encapsulated into a classfull qdisc like prio
 
-    # netem minimum limit calculation:
-    # <bandwith> / <MTU Size> * delay * 1.5		
-    # 1 Gbps / 1500 bytes MTU * 100 ms * 1.5 = 12500.
+   # prio creates 3 classes:
+   # x:0| qdisc itself
+   # x:1| class 1 highest priority
+   # x:2| class 2
+   # x:3| class 3 lowest priority
+
+   # netem minimum limit calculation:
+   # <bandwith> / <MTU Size> * delay * 1.5
+   # 1 Gbps / 1500 bytes MTU * 100 ms * 1.5 = 12500.
 
     tc qdisc add dev $1 root handle 1: prio
-    tc qdisc add dev $1 parent 1:1 handle 10:  netem limit 100000 delay 2000ms 200ms 25% loss 20% 25% 
+    tc qdisc add dev $1 parent 1:1 handle 10:  netem limit 100000 delay 2000ms 200ms 25% loss 20% 25%
 
-    #Protocol ID's
-    #icmp 1
-    #tcp  6 
-    #udp 17
-    
-    #use "ip dport" instead of "udp dst" due to because "implicit" nexthdr  
-    tc filter add dev $1 protocol ip parent 1: prio 1 u32 match ip protocol 17 0xff match ip dport 5001 0xffff flowid 1:1
+   # Protocol ID's
+   # icmp 1
+   # tcp  6
+   # udp  17
+
+   # flowid redirects packet to the corresponding class
+
+   # Matching Rules:
+   # Each of these filters are traversed with Logical OR
+   # match each icmp package that is encapsulated by an ip packet
     tc filter add dev $1 protocol ip parent 1: prio 1 u32 match ip protocol 1 0xff flowid 1:1
+
+   # match all udp packages with destination port 5001
+   # in order to filter out udp packages use "ip dport" instead of "udp dst" due to "implicit" nexthdr not forwarded into the queue
+   # 0xffff is the mask that has to be applied in order to match the udp and tcp headers
+    tc filter add dev $1 protocol ip parent 1: prio 1 u32 match ip protocol 17 0xff match ip dport 5001 0xffff flowid 1:1
 }
 
 function clear_rules() {
@@ -40,11 +51,11 @@ function clear_rules() {
 }
 
 function show_rules() {
-    echo 'QDiscs:'	
+    echo 'QDiscs:'
     tc qdisc show dev $1
-    echo 'Classes:'	
+    echo 'Classes:'
     tc class show dev $1
-    echo 'Filters:'	
+    echo 'Filters:'
     tc filter show dev $1
 }
 
