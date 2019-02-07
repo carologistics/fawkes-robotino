0db0c5359c05afb2a382928eaa82d4b43dc73073
Daniel Habering <daniel.habering@rwth-aachen.de>
2019-02-07 17:44:23 +0100

    skills: removed left over states and interfaces

--- src/lua/skills/robotino/get_product_from.lua
+++ src/lua/skills/robotino/get_product_from.lua
@@ -25,7 +25,7 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "get_product_from"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
-depends_skills     = {"product_pick", "drive_to_machine_point", "conveyor_align","shelf_pick"}
+depends_skills     = {"gripper_commands","product_pick", "drive_to_machine_point", "conveyor_align","shelf_pick"}
 depends_interfaces = {
 }
 
