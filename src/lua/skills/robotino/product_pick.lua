907d3c3912c25cda5a8cfe27e0536c6d3404a015
Daniel Habering <daniel.habering@rwth-aachen.de>
2019-04-05 14:18:11 +0200

    product_pick: add puck check and update transforms

--- src/lua/skills/robotino/product_pick.lua
+++ src/lua/skills/robotino/product_pick.lua
@@ -26,14 +26,13 @@ name               = "product_pick"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
 depends_skills     = {"gripper_commands", "motor_move"}
 depends_interfaces = {
+   {v = "robotino_sensor", type = "RobotinoSensorInterface", id="Robotino"} -- Interface to read I/O ports
 }
 
 documentation      = [==[
 Skill to pick a product from the conveyor.
 
 Parameters:
-      @param place   the name of the MPS (see navgraph e.g.: "M-BS" for base station of team magenta)
-      @param side    optional the side of the mps ("input" or "output")
       @param shelf   optional position on shelf: ( LEFT | MIDDLE | RIGHT )
 ]==]
 
@@ -42,30 +41,38 @@ Parameters:
 skillenv.skill_module(_M)
 local tfm = require("fawkes.tfutils")
 
--- Constants
-local conveyor_gripper_forward_x = 0.05 -- distance to move gripper forward after align
-local conveyor_gripper_down_z = -0.015    -- distance to move gripper down after driving over product
-local conveyor_gripper_down_second_z = -0.005       -- distance to mover gripper down second time
-local conveyor_gripper_back_x = -0.07   -- distance to move gripper back after closing gripper
+-- Constant
+local conveyor_gripper_forward_x = 0.10 -- distance to move gripper forward after align
+local conveyor_gripper_down_z = -0.00    -- distance to move gripper down after driving over product
+
+local conveyor_gripper_back_x = -0.11   -- distance to move gripper back after closing gripper
 local conveyor_gripper_up_z = 0.05 -- distance to move gripper up after closing gripper
 
 local shelf_gripper_forward_x = 0.048  -- distance to move gripper forward after align to shelf
 local shelf_gripper_down_z = -0.01     -- distance to move gripper down after driving over shelf
-local shelf_gripper_down_second_z = 0 -- distance to move gripper down second time after driving over shelf
+
 local shelf_gripper_back_x = 0.04   -- distance to move gripper back after closing gripper over shelf
 local shelf_gripper_up_z = 0.05  -- distance to move gripper up after closing the gripper over shelf
 
 local drive_back_x = -0.1      -- distance to drive back after closing the gripper
 
-local gripper_pose_offset_x = -0.02  -- conveyor pose offset in x direction
+local gripper_pose_offset_x = 0.02  -- conveyor pose offset in x direction
 local gripper_pose_offset_y = 0.00     -- conveyor_pose offset in y direction
 local gripper_pose_offset_z = 0.02  -- conveyor_pose offset in z direction
 
-local align_target_frame = "gripper_fingers"      -- the gripper align is made relative to this frame (according to gripper_commands)
-local movement_target_frame = "gripper" -- the gripper movement is made relative to this frame (according to gripper_commands)
 
+-- function to evaluate sensor data
+function is_grabbed()
+ if robotino_sensor.digital_out(0) == false and robotino_sensor.digital_out(1) == true then -- white cable on DI1 and black on DI2
+    return true
+ else
+    return false
+ end
+end
 
-
+local x_max = 0.115
+local y_max = 0.075
+local z_max = 0.057
 
 function pose_not_exist()
   local target_pos = { x = gripper_pose_offset_x,
@@ -75,7 +82,7 @@ function pose_not_exist()
 
    }
 
-   local transformed_pos = tfm.transform6D(target_pos, "conveyor_pose", align_target_frame)
+   local transformed_pos = tfm.transform6D(target_pos, "conveyor_pose", "gripper")
    if transformed_pos == nil then
      return true
    end
@@ -83,27 +90,39 @@ function pose_not_exist()
 end
 
 
-function pose_offset()
-  local target_pos = { x = gripper_pose_offset_x,
-                        y = gripper_pose_offset_y,
-                        z = gripper_pose_offset_z,
-                        ori = { x=0, y = 0, z= 0, w= 0}
+function pose_gripper_offset(x,y,z)
+  local target_pos = { x = x,
+                        y = y,
+                        z = z,
+                        ori = { x = 0, y = 0, z = 0, w = 0}
 
    }
+   local tmp = { x = 0,
+                 y = 0,
+                 z = 0,
+                 ori = { x = 0, y = 0, z = 0, w = 0}
+   }
 
-   local transformed_pos = tfm.transform6D(target_pos, "conveyor_pose", align_target_frame)
-   print_info("product_pick: target_pos is x = %f, y = %f, z = %f", target_pos.x, target_pos.y, target_pos.z)
-   print_info("product_pick: transformed_pos is x = %f, y = %f,z = %f", transformed_pos.x, transformed_pos.y, transformed_pos.z)
+   -- Get offset from gripper axis (middle of z sledge) to gripper finger
+   local gripper_rel = tfm.transform6D(tmp,"gripper","gripper_z_dyn")
 
-   return { x = transformed_pos.x,
-            y = transformed_pos.y,
-            z = transformed_pos.z,
-  }
+   -- Shift target point to gripper axis frame
+   gripper_rel.x = target_pos.x - gripper_rel.x
+   gripper_rel.y = target_pos.y - gripper_rel.y
+   gripper_rel.z = target_pos.z - gripper_rel.z
+
+   -- Transform target to gripper home frame = absolut coordinates of the axis
+   local gripper_home_rel = tfm.transform6D(gripper_rel,"gripper","gripper_home")
+
+   -- Clip to axis limits
+   return { x = math.max(0,math.min(gripper_home_rel.x,x_max)),
+            y = math.max(0,math.min(gripper_home_rel.y,y_max)),
+            z = math.max(0,math.min(gripper_home_rel.z,z_max))}
 end
 
 
 fsm:define_states{ export_to=_M,
-   closure={arduino_if=arduino_if, pose_not_exist=pose_not_exist},
+   closure={pose_not_exist=pose_not_exist, is_grabbed=is_grabbed},
    {"INIT", JumpState},
    {"OPEN_GRIPPER", SkillJumpState, skills={{gripper_commands}},final_to="GRIPPER_ALIGN", fail_to="FAILED"},
    {"GRIPPER_ALIGN", SkillJumpState, skills={{gripper_commands}}, final_to="MOVE_GRIPPER_FORWARD",fail_to="FAILED"},
@@ -117,26 +136,23 @@ fsm:define_states{ export_to=_M,
 fsm:add_transitions{
    {"INIT", "FAILED", cond="pose_not_exist()"},
    {"INIT", "OPEN_GRIPPER", true, desc="Open gripper for product_pick"},
-   {"CHECK_PUCK", "FINAL", cond="gripper_if:gripper_closed()", desc="Hold puck"},
-   {"CHECK_PUCK", "FAILED", cond="not gripper_if:gripper_closed()", desc="Don't hold puck!"},
-   {"CLOSE_GRIPPER", "MOVE_GRIPPER_BACK", timeout=1},
+   {"CHECK_PUCK", "FAILED", cond="robotino_sensor:has_writer() and not is_grabbed()", desc="Don't hold puck!"},  -- add or not is_grabbed() 
+   {"CHECK_PUCK", "FINAL", cond=true},
+   {"CLOSE_GRIPPER", "MOVE_GRIPPER_BACK", timeout=0.5},
 }
 
 
 function INIT:init()
   -- Override values if host specific config value is set
 
-  if config:exists("/skills/product_pick/gripper_pose_offset_x") then
-      gripper_pose_offset_x = config:get_float("/skills/product_pick/gripper_pose_offset_x")
+  if config:exists("/skills/arduino/x_max") then
+      x_max = config:get_float("/skills/arduino/max_x")
   end
-  if config:exists("/skills/product_pick/gripper_pose_offset_y") then
-      gripper_pose_offset_y = config:get_float("/skills/product_pick/gripper_pose_offset_y")
+  if config:exists("/skills/arduino/y_max") then
+      y_max = config:get_float("/skills/arduino/y_max")
   end
-  if config:exists("/skills/product_pick/gripper_pose_offset_z") then
-      gripper_pose_offset_z = config:get_float("/skills/product_pick/gripper_pose_offset_z")
-  end
-  if config:exists("/skills/product_pick/conveyor_gripper_down_z") then
-      conveyor_gripper_down_z = config:get_float("/skills/product_pick/conveyor_gripper_down_z")
+  if config:exists("/skills/arduino/max_z") then
+      z_max = config:get_float("/skills/arduino/z_max")
   end
 end
 
@@ -149,35 +165,44 @@ function CLOSE_GRIPPER:init()
 end
 
 function GRIPPER_ALIGN:init()
-  local pose = pose_offset(self)
+  local target_pos = { x = gripper_pose_offset_x,
+                       y = gripper_pose_offset_y,
+                       z = gripper_pose_offset_z,
+                       ori = { x=0, y = 0, z= 0, w= 0}
+  }
+
+  local grip_pos = tfm.transform6D(target_pos, "conveyor_pose", "gripper")
+
+  local pose =  pose_gripper_offset(grip_pos.x,grip_pos.y,grip_pos.z)
   self.args["gripper_commands"] = pose
   self.args["gripper_commands"].command = "MOVEABS"
-  self.args["gripper_commands"].target_frame  = movement_target_frame
+  self.args["gripper_commands"].target_frame  = "gripper_home"
 end
 
 function MOVE_GRIPPER_FORWARD:init()
-  self.args["gripper_commands"].command = "MOVEABS"
-  self.args["gripper_commands"].target_frame = movement_target_frame
+  local pose = {}
   if self.fsm.vars.shelf ~= nil then
-    self.args["gripper_commands"].x = shelf_gripper_forward_x
-    self.args["gripper_commands"].z = shelf_gripper_down_z
-  else
-    self.args["gripper_commands"].x = conveyor_gripper_forward_x
-    self.args["gripper_commands"].z = conveyor_gripper_down_z
-  end
+    pose = pose_gripper_offset(shelf_gripper_forward_x, 0, shelf_gripper_down_z)
 
+  else
+    pose = pose_gripper_offset(conveyor_gripper_forward_x, 0, conveyor_gripper_forward_z)
+  end
+  self.args["gripper_commands"] = pose
+  self.args["gripper_commands"].command = "MOVEABS"
+  self.args["gripper_commands"].target_frame = "gripper_home"
 end
 
 function MOVE_GRIPPER_BACK:init()
-  self.args["gripper_commands"].command = "MOVEABS"
-  self.args["gripper_commands"].target_frame = movement_target_frame
-  if self.fsm.vars.helf ~= nil then
-    self.args["gripper_commands"].x = shelf_gripper_back_x
-    self.args["gripper_commands"].z = shelf_gripper_up_z
+  local pose = {}
+  if self.fsm.vars.shelf ~= nil then
+    pose = pose_gripper_offset(shelf_gripper_back_x, 0, shelf_gripper_up_z)
   else
-    self.args["gripper_commands"].x = conveyor_gripper_back_x
-    self.args["gripper_commands"].z = conveyor_gripper_up_z
+    pose = pose_gripper_offset(conveyor_gripper_back_x, 0, conveyor_gripper_up_z)
   end
+  self.args["gripper_commands"] = pose
+  self.args["gripper_commands"].command = "MOVEABS"
+  self.args["gripper_commands"].target_frame = "gripper_home"
+  self.args["gripper_commands"].wait = false
 end
 
 function DRIVE_BACK:init()
