d67e82729ebbf7fd3957bd59bcc6e0ed3e66b548
Daniel Habering <daniel.habering@rwth-aachen.de>
2019-04-11 18:34:22 +0200

    skills: move gripper to home after pick and put

--- src/lua/skills/robotino/product_pick.lua
+++ src/lua/skills/robotino/product_pick.lua
@@ -124,7 +124,8 @@ fsm:define_states{ export_to=_M,
    {"GRIPPER_ALIGN", SkillJumpState, skills={{gripper_commands}}, final_to="MOVE_GRIPPER_FORWARD",fail_to="FAILED"},
    {"MOVE_GRIPPER_FORWARD", SkillJumpState, skills={{gripper_commands}}, final_to="CLOSE_GRIPPER",fail_to="FAILED"},
    {"CLOSE_GRIPPER", SkillJumpState, skills={{gripper_commands}}, final_to="MOVE_GRIPPER_BACK", fail_to="FAILED"},
-   {"MOVE_GRIPPER_BACK", SkillJumpState, skills={{gripper_commands}}, final_to = "DRIVE_BACK", fail_to="FAILED"},
+   {"MOVE_GRIPPER_BACK", SkillJumpState, skills={{gripper_commands}}, final_to = "GRIPPER_HOME", fail_to="FAILED"},
+   {"GRIPPER_HOME", SkillJumpState, skills={{gripper_commands}}, final_to = "DRIVE_BACK", fail_to="FAILED"},
    {"DRIVE_BACK", SkillJumpState, skills={{motor_move}}, final_to="CHECK_PUCK", fail_to="FAILED"},
    {"CHECK_PUCK", JumpState},
 }
@@ -191,7 +192,14 @@ function MOVE_GRIPPER_BACK:init()
   self.args["gripper_commands"] = pose
   self.args["gripper_commands"].command = "MOVEABS"
   self.args["gripper_commands"].target_frame = "gripper_home"
+end
+
+function GRIPPER_HOME:init()
+  self.args["gripper_commands"].x = 0
+  self.args["gripper_commands"].y = 0
+  self.args["gripper_commands"].z = 0.03
   self.args["gripper_commands"].wait = false
+  self.args["gripper_commands"].command = "MOVEABS"
 end
 
 function DRIVE_BACK:init()
