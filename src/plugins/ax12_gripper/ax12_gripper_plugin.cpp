ac538be65f4a581a2a3d22b05ae50e25a678946e
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-04-15 13:16:41 +0200

    ax12_gripper: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/ax12_gripper/ax12_gripper_plugin.cpp
+++ src/plugins/ax12_gripper/ax12_gripper_plugin.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *  ax12_gripper_plugin.cpp - Plugin used to control a gripper 
+ *  ax12_gripper_plugin.cpp - Plugin used to control a gripper
  *  using Robotis AX12A - servos
  *
  *  Created: Mon Feb 09 20:28:20 2015
@@ -31,29 +31,26 @@ using namespace fawkes;
 /** Plugin to control gripper using Robotis servos
  * @author Nicolas Limpert
  */
-class AX12GripperPlugin : public fawkes::Plugin
-{
- public:
+class AX12GripperPlugin : public fawkes::Plugin {
+public:
   /** Constructor.
    * @param config Fawkes configuration
    */
-  AX12GripperPlugin(Configuration *config)
-    : Plugin(config)
-  {
+  AX12GripperPlugin(Configuration *config) : Plugin(config) {
     std::string prefix = "/hardware/ax12_gripper/";
     bool active = true;
     try {
       active = config->get_bool((prefix + "active").c_str());
-    } catch (Exception &e) {} // ignored, assume enabled
+    } catch (Exception &e) {
+    } // ignored, assume enabled
 
-    if (active)
-      {
-	// GripperAX12AThread * act_thread = new GripperAX12AThread(prefix);
-	// GripperSensorThread * sensor_thread = new GripperSensorThread();
-	// sensor_thread->add_act_thread(act_thread);
-	thread_list.push_back(new GripperAX12AThread(prefix));
-	// thread_list.push_back(sensor_thread);	
-      }
+    if (active) {
+      // GripperAX12AThread * act_thread = new GripperAX12AThread(prefix);
+      // GripperSensorThread * sensor_thread = new GripperSensorThread();
+      // sensor_thread->add_act_thread(act_thread);
+      thread_list.push_back(new GripperAX12AThread(prefix));
+      // thread_list.push_back(sensor_thread);
+    }
   }
 };
 
