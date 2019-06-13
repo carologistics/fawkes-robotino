7fa0d60c8aab633d84d3e27b64267d82a1dc7ddf
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-06-13 12:45:25 +0200

    gazsim-scripts: do not load default plugins if meta plugin was specified
    If a meta plugin has been specified on the command line with `-m
    plugin`, do not load any other plugins by default. Previously, the
    specified plugin was loaded in addition to the default simulation
    plugins. However, if we want to simulate without gazebo, this is no
    longer desired.
    
    This means that if you want to load a different meta plugin in addition
    to the gazebo plugins, you need to specify a meta plugin that includes
    all those plugins.

--- etc/scripts/gazsim-startup.bash
+++ etc/scripts/gazsim-startup.bash
@@ -177,7 +177,11 @@ case $COMMAND in
     fawkes )
 	ulimit -c unlimited
 	export ROS_MASTER_URI=http://localhost:$PORT
-	robotino_plugins=gazsim-meta-robotino$ROS$VISION$AGENT$META_PLUGIN$SKIP_EXPLORATION
+	if [ -n "$META_PLUGIN" ] ; then
+		robotino_plugins=$META_PLUGIN
+	else
+	  robotino_plugins=gazsim-meta-robotino$ROS$VISION$AGENT$SKIP_EXPLORATION
+	fi
 	$GDB $FAWKES_BIN/fawkes -c $CONF/$ROBOTINO.yaml -p $robotino_plugins $@
 	if [ -n "$GDB" ]; then
 		echo Fawkes exited, press return to close shell
