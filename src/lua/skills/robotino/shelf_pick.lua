d54583cbf3d2c3de472fc55c78caf375f079d489
Morian Sonnet <sonnet@kbsg.rwth-aachen.de>
2019-06-15 18:31:15 +0200

    skills/shelf_pick: Remove unused variable

--- src/lua/skills/robotino/shelf_pick.lua
+++ src/lua/skills/robotino/shelf_pick.lua
@@ -43,7 +43,6 @@ local tfm = require("fawkes.tfutils")
 local x_distance = 0.27
 local gripper_adjust_z_distance = 0.03
 local gripper_adjust_x_distance = 0.015
-local adjust_target_frame = "gripper_home"
 local gripper_down_to_puck = -0.025
 
 function pose_gripper_offset(x,y,z)
