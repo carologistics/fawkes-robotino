2660c518a8e9ccb8b29516f0449f774bcb32724a
Niklas Sebastian Eltester <niklas-sebastian.eltester@alumni.fh-aachen.de>
2019-06-18 13:25:01 +0200

    skills: goto now has waiting functionality
    goto now finals for waiting positions
    after a travelling a certain distance

--- src/lua/skills/robotino/goto.lua
+++ src/lua/skills/robotino/goto.lua
@@ -94,14 +94,20 @@ fsm:add_transitions{
   {"INIT",  "FAILED",         cond="not vars.target_valid",                 desc="target invalid"},
   {"INIT",  "MOVING",         cond=true},
   {"MOVING", "TIMEOUT",       timeout=2}, -- Give the interface some time to update
-  {"TIMEOUT", "FINAL",         cond=target_reached, desc="Target reached"},
-  {"TIMEOUT", "FAILED",        cond=target_unreachable, desc="Target unreachable"},
+  {"TIMEOUT", "FINAL",        cond="vars.waiting_pos and vars.travelled", desc="Going to waiting position"},
+  {"TIMEOUT", "FINAL",        cond=target_reached, desc="Target reached"},
+  {"TIMEOUT", "FAILED",       cond=target_unreachable, desc="Target unreachable"},
 }
 
 
 function INIT:init()
   self.fsm.vars.target_valid = true
-
+  -- check for waiting position
+  if string.match(self.fsm.vars.place, "[WAIT]") then 
+    self.fsm.vars.waiting_pos = true
+  else
+    self.fsm.vars.waiting_pos = false 
+  end
 
   if self.fsm.vars.place ~= nil then
     if string.match(self.fsm.vars.place, "[MC][-]Z[1-7][1-8]") then
@@ -121,6 +127,13 @@ function INIT:init()
         if node:has_property("orientation") then
           self.fsm.vars.ori = node:property_as_float("orientation");
         end
+        -- In case the navgraph place defines tolerances we'll use them.
+        if node:has_property("orientation_tolerance") then
+          self.fsm.vars.ori_tolerance = node:property_as_float("orientation_tolerance");
+        end
+        if node:has_property("target_tolerance") then
+          self.fsm.vars.trans_tolerance = node:property_as_float("target_tolerance");
+        end
       else
         self.fsm.vars.target_valid = false
       end
@@ -139,6 +152,10 @@ function WAIT_TF:loop()
    self.fsm.vars.x   = cur_pose.x
    self.fsm.vars.y   = cur_pose.y
    self.fsm.vars.ori = cur_pose.ori
+   self.fsm.vars.initial_position_x   = cur_pose.x
+   self.fsm.vars.initial_position_y   = cur_pose.y
+   self.fsm.vars.initial_position_ori = cur_pose.ori
+
 end
 
 function MOVING:init()
@@ -151,11 +168,44 @@ function MOVING:init()
       self.fsm.vars.y,
       self.fsm.vars.ori,
       "map")
+
+   -- Use a tolerance if it is defined.
+   if self.fsm.vars.trans_tolerance ~= nil and self.fsm.vars.ori_tolerance ~= nil then
+      msg = navigator.CartesianGotoWithFrameWithToleranceMessage:new(
+        self.fsm.vars.x,
+        self.fsm.vars.y,
+        self.fsm.vars.ori,
+        "map",
+        self.fsm.vars.trans_tolerance,
+        self.fsm.vars.ori_tolerance)
+   end
    fsm.vars.goto_msgid = navigator:msgq_enqueue(msg)
 end
 
+function TIMEOUT:loop()
+  local got_cur_pose = false
+  local distance_to_travel = 1.0
+ 
+  while not got_cur_pose do
+    local cur_pose = tf_mod.transform({x=0, y=0, ori=0}, "base_link", "map")
+    if cur_pose ~= nil then
+      self.fsm.vars.cur_x = cur_pose.x
+      self.fsm.vars.cur_y = cur_pose.y
+      self.fsm.vars.cur_ori = cur_pose.ori
+      got_cur_pose = true
+    end
+    local x = (self.fsm.vars.cur_x - self.fsm.vars.initial_position_x) * (self.fsm.vars.cur_x - self.fsm.vars.initial_position_x)
+    local y = (self.fsm.vars.cur_y - self.fsm.vars.initial_position_y) * (self.fsm.vars.cur_y - self.fsm.vars.initial_position_y)
+    local distance_travelled = math.sqrt(x + y)
+ 
+    if distance_travelled > distance_to_travel then
+      self.fsm.vars.travelled=true
+    end
+  end
+end
+
 function MOVING:reset()
-    if navigator:has_writer() and not navigator:is_final() then
+    if navigator:has_writer() and not navigator:is_final() and fsm.vars.waiting_pos == false then
        printf("goto: sending stop");
        navigator:msgq_enqueue(navigator.StopMessage:new(fsm.vars.msgid or 0))
     end
