5e46de8cbf2605a672695cf2b5deabfd28f3d434
Victor Mataré <matare@fh-aachen.de>
2019-07-03 18:03:30 +1000

    Revert "Merge pull request #260 from carologistics/dhabering/ignore-puck-check"
    This reverts commit ada164e159b1986836d291e452dbd715a9336efd, reversing
    changes made to 1d743c4603edbe5f35c538ed6f4ac1c7d7085294.

--- src/lua/skills/robotino/product_put.lua
+++ src/lua/skills/robotino/product_put.lua
@@ -71,8 +71,7 @@ function is_grabbed()
  if robotino_sensor:is_digital_in(0) == false and robotino_sensor:is_digital_in(1) == true then -- white cable on DI1 and black on DI2
     return true
  else
-   -- Ignore until realsense is switched of by default 
-   return true
+    return false
  end
 end
 
