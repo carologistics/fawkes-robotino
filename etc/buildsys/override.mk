3e6a7f803f6323ad6307f627b4120bf23795221f
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-09-23 11:49:11 +0200

    buildsys: do not override check targets

--- etc/buildsys/override.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: check
-check:
-check: quickdoc $(if $(subst 0,,$(YAMLLINT)),yamllint) license-check
-	$(SILENT)touch $(BASEDIR)/.check_stamp
-
-.PHONY: format-check-branch
-format-check-branch:
-	$(SILENT)true
