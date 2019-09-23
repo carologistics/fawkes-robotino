95f0d46d3b2b6a9723631d73390b4443a752f434
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-09-23 11:43:20 +0200

    laser_front_dist: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/laser_front_dist/laser_front_dist_thread.cpp
+++ src/plugins/laser_front_dist/laser_front_dist_thread.cpp
@@ -22,10 +22,12 @@
  */
 
 #include "laser_front_dist_thread.h"
-#include <cmath>
+
 #include <interfaces/Laser360Interface.h>
 #include <interfaces/Position3DInterface.h>
 #include <tf/types.h>
+
+#include <cmath>
 
 using namespace fawkes;
 
@@ -35,64 +37,68 @@ using namespace fawkes;
  */
 
 LaserFrontDistThread::LaserFrontDistThread()
-    : Thread("LaserFrontDistThread", Thread::OPMODE_WAITFORWAKEUP),
-      BlockedTimingAspect(BlockedTimingAspect::WAKEUP_HOOK_SKILL),
-      TransformAspect(TransformAspect::ONLY_PUBLISHER, "laser-front-dist") {}
-
-void LaserFrontDistThread::init() {
-  // read config values
-  beams_used_ = config->get_int("plugins/laser-front-dist/number_beams_used");
-  target_frame_ = config->get_string("plugins/laser-front-dist/target_frame");
-  // open interfaces
-  if_laser_ = blackboard->open_for_reading<Laser360Interface>(
-      config->get_string("plugins/laser-front-dist/input_laser_interface")
-          .c_str());
-  if_result_ = blackboard->open_for_writing<Position3DInterface>(
-      config->get_string("plugins/laser-front-dist/output_result_interface")
-          .c_str());
+: Thread("LaserFrontDistThread", Thread::OPMODE_WAITFORWAKEUP),
+  BlockedTimingAspect(BlockedTimingAspect::WAKEUP_HOOK_SKILL),
+  TransformAspect(TransformAspect::ONLY_PUBLISHER, "laser-front-dist")
+{
 }
 
-void LaserFrontDistThread::loop() {
-  // calculate average:
-  if_laser_->read();
-  float sum = 0.0;
-  for (int i = 0; i < beams_used_ / 2 + beams_used_ % 2; i++) {
-    if (!std::isnormal(if_laser_->distances(i))) {
-      // this is invalid
-      if_result_->set_visibility_history(-1);
-      if_result_->write();
-      return;
-    }
-    sum += if_laser_->distances(i);
-  }
-  for (int i = 0; i < beams_used_ / 2; i++) {
-    if (!std::isnormal(if_laser_->distances(359 - i))) {
-      // this is invalid
-      if_result_->set_visibility_history(-1);
-      if_result_->write();
-      return;
-    }
-    sum += if_laser_->distances(359 - i);
-  }
-  float average = sum / (float)beams_used_;
-  frame_ = if_laser_->frame();
-
-  // publish transform
-  tf::Transform transform(tf::create_quaternion_from_yaw(M_PI),
-                          tf::Vector3(average, 0, 0));
-  Time time(clock);
-  tf::StampedTransform stamped_transform(transform, time, frame_.c_str(),
-                                         target_frame_.c_str());
-  tf_publisher->send_transform(stamped_transform);
-
-  // write result
-  if_result_->set_visibility_history(1);
-  if_result_->set_translation(0, average);
-  if_result_->set_frame(frame_.c_str());
-  if_result_->write();
+void
+LaserFrontDistThread::init()
+{
+	// read config values
+	beams_used_   = config->get_int("plugins/laser-front-dist/number_beams_used");
+	target_frame_ = config->get_string("plugins/laser-front-dist/target_frame");
+	// open interfaces
+	if_laser_ = blackboard->open_for_reading<Laser360Interface>(
+	  config->get_string("plugins/laser-front-dist/input_laser_interface").c_str());
+	if_result_ = blackboard->open_for_writing<Position3DInterface>(
+	  config->get_string("plugins/laser-front-dist/output_result_interface").c_str());
 }
 
-void LaserFrontDistThread::finalize() {
-  blackboard->close(if_laser_);
-  blackboard->close(if_result_);
+void
+LaserFrontDistThread::loop()
+{
+	// calculate average:
+	if_laser_->read();
+	float sum = 0.0;
+	for (int i = 0; i < beams_used_ / 2 + beams_used_ % 2; i++) {
+		if (!std::isnormal(if_laser_->distances(i))) {
+			// this is invalid
+			if_result_->set_visibility_history(-1);
+			if_result_->write();
+			return;
+		}
+		sum += if_laser_->distances(i);
+	}
+	for (int i = 0; i < beams_used_ / 2; i++) {
+		if (!std::isnormal(if_laser_->distances(359 - i))) {
+			// this is invalid
+			if_result_->set_visibility_history(-1);
+			if_result_->write();
+			return;
+		}
+		sum += if_laser_->distances(359 - i);
+	}
+	float average = sum / (float)beams_used_;
+	frame_        = if_laser_->frame();
+
+	// publish transform
+	tf::Transform        transform(tf::create_quaternion_from_yaw(M_PI), tf::Vector3(average, 0, 0));
+	Time                 time(clock);
+	tf::StampedTransform stamped_transform(transform, time, frame_.c_str(), target_frame_.c_str());
+	tf_publisher->send_transform(stamped_transform);
+
+	// write result
+	if_result_->set_visibility_history(1);
+	if_result_->set_translation(0, average);
+	if_result_->set_frame(frame_.c_str());
+	if_result_->write();
+}
+
+void
+LaserFrontDistThread::finalize()
+{
+	blackboard->close(if_laser_);
+	blackboard->close(if_result_);
 }
