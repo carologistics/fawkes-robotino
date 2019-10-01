3746f7c9819c42a388a3c2d5d5aa22b0ad9550e4
Victor Mataré <matare@fh-aachen.de>
2019-10-01 23:51:58 +0200

    protoboard-rcll: factor out domain definition
    protoboard-rcll is effectively a new plugin, but it's based on the
    protoboard plugin. Only this time, it contains ONLY the domain-specific
    template specializations & instantiations. All of the generic (i.e.
    domain-agnostic) functionality has been moved to a plugin template in
    the core repo.
    
    This compiles, but is COMPLETELY UNTESTED yet.

---
+++ src/plugins/protoboard-rcll/plugin.cpp
@@ -0,0 +1,61 @@
+
+/***************************************************************************
+ * Protoboard plugin template instantiation for the LLSF
+ *
+ * Copyright 2019 Victor Mataré
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <interfaces/PrepareMachineInterface.h>
+#include <interfaces/SendBeaconInterface.h>
+#include <libs/protoboard/blackboard_manager.h>
+#include <libs/protoboard/plugin.h>
+
+// This is too templatey for auto-formatting ;-)
+// clang-format off
+
+
+namespace protoboard {
+
+std::vector<std::string> proto_dirs()
+{ return { BASEDIR "/src/libs/llsf_msgs" }; }
+
+}
+
+
+using ProtoboardPluginRCLL = ProtoboardPlugin <
+	protoboard::bb_iface_manager <
+		fawkes::SendBeaconInterface,
+		protoboard::type_list <
+			fawkes::SendBeaconInterface::SendBeaconMessage,
+			fawkes::SendBeaconInterface::SetPeerMessage
+		>
+	>,
+	protoboard::bb_iface_manager <
+		fawkes::PrepareMachineInterface,
+		protoboard::type_list <
+			fawkes::PrepareMachineInterface::PrepareBSMessage,
+			fawkes::PrepareMachineInterface::PrepareCSMessage,
+			fawkes::PrepareMachineInterface::PrepareDSMessage,
+			fawkes::PrepareMachineInterface::PrepareRSMessage,
+			fawkes::PrepareMachineInterface::PrepareSSMessage,
+			fawkes::PrepareMachineInterface::SetPeerMessage
+		>
+	>
+>;
+
+PLUGIN_DESCRIPTION("ProtoBoard plugin for the RoboCup Logistics League")
+EXPORT_PLUGIN(ProtoboardPluginRCLL)
+
--- src/plugins/protoboard-rcll/rcll_receiving.cpp
+++ src/plugins/protoboard-rcll/rcll_receiving.cpp
@@ -1,47 +1,77 @@
-#include "blackboard_manager.h"
+
+/***************************************************************************
+ * Protoboard plugin template instantiation for the LLSF
+ * - Mappings for received ProtoBuf messages to blackboard interfaces
+ *
+ * Copyright 2019 Victor Mataré
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
 
 #include <interfaces/OrderInterface.h>
 #include <interfaces/RCLLGameStateInterface.h>
 #include <interfaces/RecvBeaconInterface.h>
-
 #include <libs/llsf_msgs/BeaconSignal.pb.h>
-#include <libs/llsf_msgs/MachineInstructions.pb.h>
 #include <libs/llsf_msgs/GameState.pb.h>
 #include <libs/llsf_msgs/OrderInfo.pb.h>
 #include <libs/llsf_msgs/RobotInfo.pb.h>
 #include <libs/llsf_msgs/VersionInfo.pb.h>
+#include <protoboard/protobuf_to_bb.h>
 
 namespace protoboard {
 
-
 using namespace fawkes;
 using namespace std;
-using namespace boost::fusion;
+
+// clang-format off
 
 
 std::unordered_map<std::string, std::shared_ptr<pb_convert>> make_receiving_interfaces_map() {
-  return {
-    make_pair(
-          "llsf_msgs.OrderInfo",
-          make_shared<
-            pb_sequence_converter<
-              llsf_msgs::OrderInfo,
-              pb_converter<llsf_msgs::Order, OrderInterface>
-            >
-          >()),
-    make_pair("llsf_msgs.BeaconSignal", make_shared<pb_converter<llsf_msgs::BeaconSignal, RecvBeaconInterface>>()),
-    make_pair("llsf_msgs.GameState", make_shared<pb_converter<llsf_msgs::GameState, RCLLGameStateInterface>>()),
+	return {
+		{
+			"llsf_msgs.OrderInfo",
+			make_shared<
+				pb_sequence_converter<
+					llsf_msgs::OrderInfo,
+					pb_converter<llsf_msgs::Order, OrderInterface>
+				>
+			>()
+		},
+		{
+			"llsf_msgs.BeaconSignal",
+			make_shared<pb_converter<llsf_msgs::BeaconSignal, RecvBeaconInterface>>()
+		},
+		{
+			"llsf_msgs.GameState",
+			make_shared<pb_converter<llsf_msgs::GameState, RCLLGameStateInterface>>()
+		},
 
-    // Dummy handler, i.e. discard message
-    make_pair("llsf_msgs.RobotInfo", make_shared<pb_convert>()),
-    make_pair("llsf_msgs.VersionInfo", make_shared<pb_convert>())
-  };
+		// Dummy handler, i.e. discard message
+		{ "llsf_msgs.RobotInfo", make_shared<pb_convert>() },
+		{ "llsf_msgs.VersionInfo", make_shared<pb_convert>() }
+	};
 };
+
 
 
 /****************************************
  *           OrderInterface             *
  ****************************************/
+
+template<>
+std::string iface_id_for_type<OrderInterface>()
+{ return "/protoboard/order/"; }
 
 static const enum_map<llsf_msgs::BaseColor, OrderInterface::BaseColor>
 base_color_enum_order {
@@ -124,6 +154,10 @@ pb_converter<llsf_msgs::Order, OrderInterface>::handle(
  *         RecvBeaconInterface          *
  ****************************************/
 
+template<>
+std::string iface_id_for_type<RecvBeaconInterface>()
+{ return "/protoboard/beacon/"; }
+
 static const enum_map<llsf_msgs::Team, RecvBeaconInterface::TEAM_COLOR>
 team_enum_recv_beacon {
   {llsf_msgs::Team::CYAN, RecvBeaconInterface::TEAM_COLOR::CYAN},
@@ -149,19 +183,13 @@ pb_converter<llsf_msgs::BeaconSignal, RecvBeaconInterface>::handle(
 }
 
 
-template<>
-void
-BlackboardManager::handle_message(
-    SendBeaconInterface *iface,
-    SendBeaconInterface::SetPeerMessage *msg)
-{
-  iface->set_peer_id(msg->peer_id());
-}
-
-
 /****************************************
  *          GameStateInterface          *
  ****************************************/
+
+template<>
+std::string iface_id_for_type<RCLLGameStateInterface>()
+{ return "/protoboard/game_state/"; }
 
 static const enum_map<llsf_msgs::GameState::State, RCLLGameStateInterface::GameState>
 game_state_enum {
@@ -198,145 +226,5 @@ pb_converter<llsf_msgs::GameState, RCLLGameStateInterface>::handle(
 }
 
 
-/****************************************
- *          SendBeaconMessage           *
- ****************************************/
 
-static const enum_map<llsf_msgs::Team, SendBeaconInterface::TEAM_COLOR>
-team_enum_beacon {
-  {llsf_msgs::CYAN, SendBeaconInterface::TEAM_COLOR::CYAN},
-  {llsf_msgs::MAGENTA, SendBeaconInterface::TEAM_COLOR::MAGENTA}
-};
-
-
-template<>
-void
-BlackboardManager::handle_message(
-    SendBeaconInterface *iface,
-    SendBeaconInterface::SendBeaconMessage *msg)
-{
-  std::shared_ptr<llsf_msgs::BeaconSignal> m = std::make_shared<llsf_msgs::BeaconSignal>();
-
-  // Set time here to simplify blackboard interface
-  llsf_msgs::Time *time = new llsf_msgs::Time();
-  time->set_sec(clock->now().get_sec());
-  time->set_nsec(clock->now().get_nsec());
-
-  llsf_msgs::Pose2D *pose = new llsf_msgs::Pose2D();
-  pose->set_x(msg->translation(0));
-  pose->set_y(msg->translation(1));
-  pose->set_ori(msg->orientation());
-  pose->set_allocated_timestamp(time);
-
-  m->set_allocated_pose(pose);
-  m->set_allocated_team_name(new std::string(msg->team_name()));
-  m->set_number(msg->number());
-  m->set_allocated_peer_name(new std::string(msg->peer_name()));
-  m->set_team_color(team_enum_beacon.of(msg->team_color()));
-
-  m->set_allocated_time(new llsf_msgs::Time(*time));
-
-  // Same for sequence number, good idea to count it centrally, anyways
-  static google::protobuf::uint64 beacon_seq = 0;
-  m->set_seq(beacon_seq++);
-
-  message_handler_->send(iface->peer_id(), m);
 }
-
-
-/****************************************
- *           Prepare Messages           *
- ****************************************/
-
-static const enum_map<llsf_msgs::Team, PrepareMachineInterface::Team>
-team_enum_prepare {
-  {llsf_msgs::CYAN, PrepareMachineInterface::Team::CYAN},
-  {llsf_msgs::MAGENTA, PrepareMachineInterface::Team::MAGENTA}
-};
-
-static const enum_map<llsf_msgs::BaseColor, PrepareMachineInterface::BaseColor>
-base_color_enum {
-  {llsf_msgs::BaseColor::BASE_BLACK, PrepareMachineInterface::BASE_BLACK},
-  {llsf_msgs::BaseColor::BASE_RED, PrepareMachineInterface::BASE_RED},
-  {llsf_msgs::BaseColor::BASE_SILVER, PrepareMachineInterface::BASE_SILVER}
-};
-
-static const enum_map<llsf_msgs::MachineSide, PrepareMachineInterface::MachineSide>
-machine_side_enum {
-  {llsf_msgs::MachineSide::INPUT, PrepareMachineInterface::MachineSide::INPUT},
-  {llsf_msgs::MachineSide::OUTPUT, PrepareMachineInterface::MachineSide::OUTPUT}
-};
-
-static const enum_map<llsf_msgs::CSOp, PrepareMachineInterface::CSOp>
-csop_enum {
-  {llsf_msgs::CSOp::MOUNT_CAP, PrepareMachineInterface::CSOp::MOUNT_CAP},
-  {llsf_msgs::CSOp::RETRIEVE_CAP, PrepareMachineInterface::CSOp::RETRIEVE_CAP}
-};
-
-
-template<>
-void
-BlackboardManager::handle_message(
-    PrepareMachineInterface *iface,
-    PrepareMachineInterface::PrepareBSMessage *msg)
-{
-  std::shared_ptr<llsf_msgs::PrepareMachine> m = std::make_shared<llsf_msgs::PrepareMachine>();
-  llsf_msgs::PrepareInstructionBS *instr = new llsf_msgs::PrepareInstructionBS();
-  instr->set_color(base_color_enum.of(msg->color()));
-  instr->set_side(machine_side_enum.of(msg->side()));
-  m->set_allocated_instruction_bs(instr);
-  m->set_team_color(team_enum_prepare.of(msg->team_color()));
-  m->set_machine(msg->machine());
-
-  message_handler_->send(iface->peer_id(), m);
-}
-
-
-template<>
-void
-BlackboardManager::handle_message(
-    fawkes::PrepareMachineInterface *iface,
-    PrepareMachineInterface::PrepareCSMessage *msg)
-{
-  std::shared_ptr<llsf_msgs::PrepareMachine> m = std::make_shared<llsf_msgs::PrepareMachine>();
-  llsf_msgs::PrepareInstructionCS *instr = new llsf_msgs::PrepareInstructionCS();
-  instr->set_operation(csop_enum.of(msg->operation()));
-  m->set_allocated_instruction_cs(instr);
-  m->set_team_color(team_enum_prepare.of(msg->team_color()));
-  m->set_machine(msg->machine());
-
-  message_handler_->send(iface->peer_id(), m);
-}
-
-template<>
-void
-BlackboardManager::handle_message(
-    fawkes::PrepareMachineInterface *iface,
-    PrepareMachineInterface::PrepareDSMessage *msg)
-{}
-
-template<>
-void
-BlackboardManager::handle_message(
-    fawkes::PrepareMachineInterface *iface,
-    PrepareMachineInterface::PrepareRSMessage *msg)
-{}
-
-template<>
-void
-BlackboardManager::handle_message(
-    fawkes::PrepareMachineInterface *iface,
-    PrepareMachineInterface::PrepareSSMessage *msg)
-{}
-
-template<>
-void
-BlackboardManager::handle_message(
-    fawkes::PrepareMachineInterface *iface,
-    PrepareMachineInterface::SetPeerMessage *msg)
-{}
-
-
-
-} // namespace protoboard
-
