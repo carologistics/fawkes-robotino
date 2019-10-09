4821bd18006adf12fa2d3dcfdf34f10fdbcde1fe
Victor Mataré <matare@fh-aachen.de>
2019-10-09 04:46:18 +0200

    skills: add create_peer skill
    creates a ProtoBuf peer using protoboard-rcll

--- src/lua/skills/robotino/init.lua
+++ src/lua/skills/robotino/init.lua
@@ -55,3 +55,6 @@ skillenv.use_skill("skills.robotino.bring_product_to")
 skillenv.use_skill("skills.robotino.get_product_from")
 skillenv.use_skill("skills.robotino.approach_test")
 skillenv.use_skill("skills.robotino.discard")
+
+-- ProtoBuf communication
+skillenv.use_skill("skills.robotino.create_peer")
