4357f1ac6aa5b346c6e705cb65a991429901e8de
Daniel Swoboda <swoboda@kbsg.rwth-aachen.de>
2020-09-05 15:05:03 +0200

    scripts: hotfix deadlock in gazsim script

--- etc/scripts/gazsim.bash
+++ etc/scripts/gazsim.bash
@@ -433,7 +433,21 @@ if [  $COMMAND  == start ]; then
 				if [ "$(command -v rcll-refbox-instruct)" == "" ]; then
 						echo "rcll-refbox-instruct not found, not built or old version?"
 				else
-						rcll-refbox-instruct -w
+                        (rcll-refbox-instruct -w) & pid=$!
+                        # in the background, sleep for 10 secs then kill that process
+
+                        runwake=true
+                        while $runwake
+                            do sleep 2
+                            if ps -p $pid > /dev/null
+                            then kill -9 $pid 
+                                echo "Refbox wakeup stuck, retrying"
+                                (rcll-refbox-instruct -w) & pid=$!
+                            else runwake=false
+                                echo "Refbox alive"
+                            fi
+                        done
+                         
 						echo "Starting game (Phase: $START_GAME ${TEAM_CYAN:+Cyan: ${TEAM_CYAN}}${TEAM_MAGENTA:+ Magenta: ${TEAM_MAGENTA}})"
 						rcll-refbox-instruct -p SETUP -s RUNNING ${TEAM_CYAN:+-c ${TEAM_CYAN}}${TEAM_MAGENTA:+-m ${TEAM_MAGENTA}}
 						rcll-refbox-instruct -n $NUM_ROBOTINOS -W60 || (stop_simulation; exit 1)
