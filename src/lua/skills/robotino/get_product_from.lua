2c46bf39fe582ad6942eb0a03bb2ba400e636da9
Niklas Sebastian Eltester <eltester@fh-aachen.de>
2021-05-23 16:01:35 +0200

    skills/get_product_from: prep for vision based gripping

--- src/lua/skills/robotino/get_product_from.lua
+++ src/lua/skills/robotino/get_product_from.lua
@@ -25,7 +25,7 @@ module(..., skillenv.module_init)
 -- Crucial skill information
 name               = "get_product_from"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
-depends_skills     = {"product_pick", "drive_to_machine_point", "conveyor_align","shelf_pick"}
+depends_skills     = {"product_pick", "drive_to_machine_point", "conveyor_align", "detect_object"}
 depends_interfaces = {
    {v = "laserline_switch", type = "SwitchInterface", id="laser-lines"},
 }
@@ -65,20 +65,16 @@ end
 
 fsm:define_states{ export_to=_M, closure={navgraph=navgraph,shelf_set=shelf_set},
    {"INIT", JumpState},
-   {"DRIVE_TO_MACHINE_POINT", SkillJumpState, skills={{drive_to_machine_point}}, final_to="CONVEYOR_ALIGN", fail_to="FAILED"},
-   {"CONVEYOR_ALIGN", SkillJumpState, skills={{conveyor_align}}, final_to="DECIDE_ENDSKILL", fail_to="FAILED"},
-   {"DECIDE_ENDSKILL", JumpState},
+   {"DRIVE_TO_MACHINE_POINT", SkillJumpState, skills={{drive_to_machine_point}}, final_to="GET_GRIPPING_POSE", fail_to="FAILED"},
+   {"GET_GRIPPING_POSE", SkillJumpState, skills={{conveyor_align, detect_object}}, final_to="PRODUCT_PICK", fail_to="FAILED"},
    {"PRODUCT_PICK", SkillJumpState, skills={{product_pick}}, final_to="FINAL", fail_to="FAILED"},
-   {"SHELF_PICK", SkillJumpState, skills={{shelf_pick}}, final_to="FINAL", fail_to="FAILED"},
 }
 
 fsm:add_transitions{
    {"INIT", "FAILED", cond="not navgraph", desc="navgraph not available"},
    {"INIT", "FAILED", cond="not vars.node:is_valid()", desc="point invalid"},
-   {"INIT", "CONVEYOR_ALIGN", cond=already_at_conveyor, desc="Already in front of the mps, align"},
+   {"INIT", "GET_GRIPPING_POSE", cond=already_at_conveyor, desc="Already in front of the mps, align"},
    {"INIT", "DRIVE_TO_MACHINE_POINT", cond=true, desc="Everything OK"},
-   {"DECIDE_ENDSKILL","SHELF_PICK", cond=shelf_set},
-   {"DECIDE_ENDSKILL","PRODUCT_PICK", cond=true},
 }
 
 function INIT:init()
@@ -89,16 +85,15 @@ function INIT:init()
 end
 
 function DRIVE_TO_MACHINE_POINT:init()
-   local option = "CONVEYOR"
    if self.fsm.vars.shelf ~= nil then
      self.fsm.vars.side = "input"
    end
    if self.fsm.vars.side == "input" or self.fsm.vars.shelf then
       self.fsm.vars.tag_id = navgraph:node(self.fsm.vars.place):property_as_float("tag_input")
-      self.args["drive_to_machine_point"] = {place = self.fsm.vars.place .. "-I", option = option, x_at_mps=X_AT_MPS, tag_id=self.fsm.vars.tag_id}
+      self.args["drive_to_machine_point"] = {place = self.fsm.vars.place .. "-I", option = "SHELF_" + self.fsm.vars.shelf, x_at_mps=X_AT_MPS, tag_id=self.fsm.vars.tag_id}
    else --if no side is given drive to output
       self.fsm.vars.tag_id = navgraph:node(self.fsm.vars.place):property_as_float("tag_output")
-      self.args["drive_to_machine_point"] = {place = self.fsm.vars.place .. "-O", option = option, x_at_mps=X_AT_MPS, tag_id=self.fsm.vars.tag_id}
+      self.args["drive_to_machine_point"] = {place = self.fsm.vars.place .. "-O", option = "CONVEYOR", x_at_mps=X_AT_MPS, tag_id=self.fsm.vars.tag_id}
    end
 end
 
@@ -123,18 +118,7 @@ function PRODUCT_PICK:exit()
   end
 end
 
-function SHELF_PICK:init()
-  self.args["shelf_pick"].slot = self.fsm.vars.shelf
-end
-
-function SHELF_PICK:exit()
-  local sp_fsm = skillenv.get_skill_fsm("shelf_pick")
-  if sp_fsm.current == sp_fsm.states[sp_fsm.fail_state] then
-    self.fsm.vars.error = "Shelf Pick Failed"
-  end
-end
-
-function CONVEYOR_ALIGN:init()
+function GET_GRIPPING_POSE:init()
    if self.fsm.vars.shelf ~= nil then
      self.args["conveyor_align"].side = "input"
      self.args["conveyor_align"].place = self.fsm.vars.place
@@ -155,10 +139,13 @@ function FAILED:init()
   laserline_switch:msgq_enqueue(laserline_switch.DisableSwitchMessage:new())
 end
 
-function CONVEYOR_ALIGN:exit()
+function GET_GRIPPING_POSE:exit()
   local cv_fsm = skillenv.get_skill_fsm("conveyor_align")
+  local do_fsm = skillenv.get_skill_fsm("detect_object")
   if cv_fsm.current == cv_fsm.states[cv_fsm.fail_state] then
     self.fsm.vars.error = "Conveyor Align Failed"
+  elseif do_fsm.current == do_fsm.states[fail_state] then
+    self.fsm.vars.error = "Detect Object Failed"
   end
 end
 
