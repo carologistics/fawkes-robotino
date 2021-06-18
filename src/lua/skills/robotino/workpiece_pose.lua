d7a3184ba865b54c6eb606aef41322967d086b4f
Niklas Sebastian Eltester <eltester@fh-aachen.de>
2021-06-18 12:55:58 +0200

    skills/workpiece_pose: fix if name bug, improve failure behaviour

--- src/lua/skills/robotino/workpiece_pose.lua
+++ src/lua/skills/robotino/workpiece_pose.lua
@@ -33,14 +33,14 @@ module(..., skillenv.module_init)
 
 -- Crucial skill information
 name               = "workpiece_pose"
-fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
+fsm                = SkillHSM:new{name=name, start="INIT", debug=false}
 depends_skills     = {"detect_object"}
 depends_interfaces = {
    {v = "yolo_interface", type = "YoloOpenCVInterface", id = "YoloOpenCVwrite"},
    {v = "realsense_control", type = "CameraControlInterface", id = "realsense2_cam"},
    {v = "realsense_switch", type = "SwitchInterface", id = "realsense2"},
    {v = "realsense_boundingbox", type = "Realsense2Interface", id = "BoundingBoxInterface"},
-   {v = "workpiece_pose", type = "WorkpiecePoseInterface", id = "WorkpiecePose"},
+   {v = "workpiece_pose_if", type = "WorkpiecePoseInterface", id = "WorkpiecePose"},
 }
 
 documentation      = [==[
@@ -72,7 +72,8 @@ function has_writer()
    return (yolo_interface:has_writer() and
            realsense_control:has_writer() and
            realsense_switch:has_writer() and
-           realsense_boundingbox:has_writer())
+           realsense_boundingbox:has_writer() and 
+           workpiece_pose_if:has_writer())
 end
 
 function check_bounding_box_if_msgid(self)
@@ -90,10 +91,11 @@ fsm:define_states{ export_to=_M, closure={has_writer=has_writer,
 fsm:add_transitions{
    {"INIT", "FAILED", cond="not has_writer()", desc="YoloOpenCV or Realsense Interface has no writer"},
    {"INIT", "DETECT_WORKPIECE", cond=true, desc="Detecting Workpiece"},
-   {"GET_XYZ", "FAILED", cond="vars.low_conf_or_out_of_range", 
-      desc="Confindences too low or Bounding box out of range"},
+   {"GET_XYZ", "FAILED", cond="vars.xyz_retries > 2", desc="Confindences too low or Bounding box out of range"},
+   {"GET_XYZ", "DETECT_WORKPIECE", cond="vars.low_conf_or_out_of_range", desc="Taking new picture to look again"},
    {"GET_XYZ", "PUBLISH_POSE", cond="check_bounding_box_if_msgid(self)", desc="Deprojected pixel to point"},
-   {"PUBLISH_POSE", "FAILED", cond="vars.zero_pose", desc="Can't get XYZ, deprojection returned 0,0,0"},
+   {"PUBLISH_POSE", "FAILED", cond="vars.pose_retries > 2", desc="Can't get XYZ, deprojection returned 0,0,0"},
+   {"PUBLISH_POSE", "GET_XYZ", cond="vars.zero_pose", desc="Retrying deprojection"},
    {"PUBLISH_POSE", "FINAL", cond=true},
 }
 
@@ -104,6 +106,8 @@ function INIT:init()
    self.fsm.vars.low_conf_or_out_of_range = nil
    self.fsm.vars.classId = 0
    self.fsm.vars.objects = {}
+   self.fsm.vars.xyz_retries = 0
+   self.fsm.vars.pose_retries = 0
    realsense_switch:msgq_enqueue(realsense_switch.EnableSwitchMessage:new())
 end
 
@@ -115,6 +119,7 @@ function GET_XYZ:init()
    local object_nr = 0
    local index = 1024
    local found_bb =  false
+   self.fsm.vars.xyz_retries = self.fsm.vars.xyz_retries + 1
    while (yolo_interface:classId(1024 - index) ~= 1024) do
       if yolo_interface:classId(1024 - index) == self.fsm.vars.classId then
          if (tolerances[self.fsm.vars.location].x.min <= yolo_interface:centerX(1024 - index) and
@@ -142,13 +147,14 @@ function PUBLISH_POSE:init()
    local x = realsense_boundingbox:translation(0)
    local y = realsense_boundingbox:translation(1)
    local z = realsense_boundingbox:translation(2)
+   self.fsm.vars.pose_retries = self.fsm.vars.pose_retries + 1
    if x == 0 and y == 0 and z == 0 then
       self.fsm.vars.zero_pose = true
    else
-      wp_pose_msg = workpiece_pose.WPPoseMessage:new()
+      wp_pose_msg = workpiece_pose_if.WPPoseMessage:new()
       wp_pose_msg:set_translation(0, x)
       wp_pose_msg:set_translation(1, y)
       wp_pose_msg:set_translation(2, z)
-      workpiece_pose:msgq_enqueue(wp_pose_msg)
+      workpiece_pose_if:msgq_enqueue(wp_pose_msg)
    end
 end
