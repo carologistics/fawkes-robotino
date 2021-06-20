79028c8a5db3c841e51c29fdec74e56b01e02386
Niklas Sebastian Eltester <eltester@fh-aachen.de>
2021-06-20 18:55:49 +0200

    skills: add license to tsp py script

--- src/lua/skills/robotino/tsp_robotino.py
+++ src/lua/skills/robotino/tsp_robotino.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+##########################################################################
+#
+#  tsp_robotino.py: greedy tsp solver for navigation challenge
+#
+#  Copyright © 2020 Gjorgji Nikolovski  <gjorgji.nikolovski@alumni.fh-aachen.de>
+#
+##########################################################################
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Library General Public License for more details.
+#
+#  Read the full text in the LICENSE.GPL file in the doc directory.
+
 from sys import argv, maxsize
 from itertools import permutations
 from math import sqrt
