b82f7e1848765e4acbb92259eff58c3a4fd37286
Niklas Sebastian Eltester <eltester@fh-aachen.de>
2021-06-24 19:23:12 +0200

    skills/product_put: adjust product put to new transforms and model

--- src/lua/skills/robotino/product_put.lua
+++ src/lua/skills/robotino/product_put.lua
@@ -45,7 +45,7 @@ local tfm = require("fawkes.tfutils")
 -- Constants
 local gripper_pose_offset_x = 0.005  -- conveyor pose offset in x direction
 local gripper_pose_offset_y = 0.00  -- conveyor_pose offset in y direction
-local gripper_pose_offset_z = 0.03  -- conveyor_pose offset in z direction
+local gripper_pose_offset_z = 0.04  -- conveyor_pose offset in z direction
 
 local conveyor_gripper_forward_x = 0.018 -- distance to move gripper forward after align
 local conveyor_gripper_down_z = -0.015  -- distance to move gripper down after driving over conveyor
@@ -54,7 +54,7 @@ local conveyor_gripper_back_x = -0.02 -- distance to move gripper back after ope
 local conveyor_gripper_up_z = 0.03    -- distance to move gripper up after opening the gripper
 
 local slide_gripper_forward_x = 0.035  -- distance to move gripper forward after align if the target is slide
-local slide_gripper_down_z = -0.04    -- distance to move gripper down after driving over slide
+local slide_gripper_down_z = -0.0    -- distance to move gripper down after driving over slide
 
 local slide_gripper_back_x = -0.01 -- distance to move gripper back after opening the gripper if the target is slide
 local slide_gripper_up_z = 0.01    --distance to move gripper up after opening the gripper if the target is slide
