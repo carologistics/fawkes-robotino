6c0ed626069ae79140497c916dfa762a6c583bb8
Matteo Tschesche <matteo.tschesche@rwth-aachen.de>
2021-11-26 18:34:14 +0100

    RCAP: create header for RCAP_mps_detection.py

--- etc/RCAP-markerless-detection/RCAP_mps_detection.py
+++ etc/RCAP-markerless-detection/RCAP_mps_detection.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python3
+##########################################################################
+#
+#  RCAP_mps_detection.py: detect MPS machines, display, and save labels
+#
+#  Copyright © 2021 Matteo Tschesche <mtschesche@kbsg.rwth-aachen.de>
+#
+##########################################################################
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Library General Public License for more details.
+#
+#  Read the full text in the LICENSE.GPL file in the doc directory.
+
 import cv2
 import os
 import imgaug as ia
