8b7bdca779006f4c163cc9bbdd1f690c2d3a1468
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2021-12-13 19:18:26 +0100

    tests: add test for xenonite domain

---
+++ etc/scripts/cx-xenonite-check.bash
@@ -0,0 +1,51 @@
+#! /bin/bash
+#
+# cx-simtest-check.bash
+# Copyright (C) 2019-2020 Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# Read the full text in the LICENSE.GPL file in the doc directory.
+#
+
+# Check the logfiles passed to the script for the status of the CX simtests.
+# Collect and print the failures and exit with status 1 if a failure occurred.
+# Otherwise, exit with status 0.
+
+set -eu
+
+check_file () {
+  tail -n +0 -f $file | while read LINE ; do
+    if [[ "${LINE}" == *"[EXCEPTION]"* ]] ; then
+      echo $file
+      return
+    elif [[ "${LINE}" == *"Scenario fulfilled"* ]] ; then
+      return
+    fi
+  done
+}
+
+for file in $* ; do
+  if [ ! -f $file ] ; then
+    echo "File $file does not exist!"
+    exit 1
+  fi
+  failures+=($(check_file $file))
+done
+
+if [ ${#failures[@]} -gt 0 ] ; then
+  for failure in ${failures[@]} ; do
+    echo "Failure in $failure"
+  done
+  exit 1
+fi
+echo "All tests successful!"
+exit 0
---
+++ tests.d/cx-xenonite.bash
@@ -0,0 +1,48 @@
+#! /bin/bash
+#
+# cx-simtest.bash
+# Copyright (C) 2019 Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# Read the full text in the LICENSE.GPL file in the doc directory.
+#
+
+set -eu -o pipefail
+
+# Only run the simtest on Fedora.
+source /etc/os-release
+if [ "$NAME" != "Fedora" ] ; then
+  exit 0
+fi
+
+
+FAWKES_DIR=$(realpath $(dirname ${BASH_SOURCE[0]})/..)
+tmpconfig=$(mktemp $FAWKES_DIR/cfg/conf.d/simtest-XXXXXX.yaml)
+echo "---" >> $tmpconfig
+echo "/clips-executive/spec: xenonite" >> $tmpconfig
+export FAWKES_DIR
+SCRIPT_PATH=$FAWKES_DIR/bin
+WORKING_DIR=$FAWKES_DIR/tests.out.d/cx-xenonite
+mkdir -p $WORKING_DIR
+cd $WORKING_DIR
+TERMINAL=tmux
+export TERMINAL
+
+TRAP_SIGNALS="SIGINT SIGTERM SIGPIPE EXIT"
+stop_test () {
+  trap - $TRAP_SIGNALS
+  $SCRIPT_PATH/gazsim.bash -x kill >/dev/null
+  rm -f $tmpconfig
+}
+
+trap stop_test $TRAP_SIGNALS
+ulimit -c 0
+$SCRIPT_PATH/gazsim.bash -x start -k -n 3 -k -o --no-refbox --no-refbox-comm --mongodb -m m-promises $@
+sleep 10
+echo "Waiting for results..."
+timeout -k 300 240 $SCRIPT_PATH/cx-xenonite-check.bash ./robot1_latest.log ./robot2_latest.log ./robot3_latest.log
+exit $?
