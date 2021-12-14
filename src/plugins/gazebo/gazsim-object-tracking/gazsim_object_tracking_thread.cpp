a21eed4f66cb812bfdd51dcdc47d44ed97696908
Matteo Tschesche <matteo.tschesche@rwth-aachen.de>
2021-12-14 14:43:23 +0100

    gazsim_object-tracking: implement object-tracking interface
    The object-tracking interface allows to start and end object tracking by
    sending curresponding messages. The gazsim-object-tracking plugin receives
    these messages and publishes ground-truth target frames through the same
    interface.

--- src/plugins/gazebo/gazsim-object-tracking/gazsim_object_tracking_thread.cpp
+++ src/plugins/gazebo/gazsim-object-tracking/gazsim_object_tracking_thread.cpp
@@ -23,6 +23,7 @@
 #include "gazsim_object_tracking_thread.h"
 
 #include <aspect/logging.h>
+#include <interfaces/ObjectTrackingInterface.h>
 #include <interfaces/Position3DInterface.h>
 #include <tf/types.h>
 #include <utils/math/angle.h>
@@ -44,7 +45,8 @@ using namespace gazebo;
 /** Constructor. */
 ObjectTrackingThread::ObjectTrackingThread()
 : Thread("ObjectTrackingThread", Thread::OPMODE_WAITFORWAKEUP),
-  BlockedTimingAspect(BlockedTimingAspect::WAKEUP_HOOK_SENSOR_PROCESS)
+  BlockedTimingAspect(BlockedTimingAspect::WAKEUP_HOOK_SENSOR_PROCESS),
+  fawkes::TransformAspect(fawkes::TransformAspect::BOTH, "object_tracking")
 {
 }
 
@@ -53,7 +55,7 @@ ObjectTrackingThread::init()
 {
 	logger->log_info(name(), "Initializing Simulation of the Workpiece (Puck) Positions");
 
-	//read cofig values
+	//read config values
 	gps_topic_ = config->get_string("/gazsim/topics/gps");
 
 	//subscribing to gazebo publisher
@@ -66,12 +68,24 @@ ObjectTrackingThread::init()
 
 	logger->log_info(name(), "Initializing Simulation of the MPS Positions");
 
-	//read cofig values
+	//read config values
 	factory_topic_ = config->get_string("/gazsim/topics/factory");
 
 	//subscribing to gazebo publisher
 	factory_sub_ =
 	  gazebo_world_node->Subscribe(factory_topic_, &ObjectTrackingThread::on_factory_msg, this);
+
+	//read config values
+	object_tracking_if_name_ = config->get_string("/gazsim/object-tracking/if-name");
+
+	//open ObjectTrackingInterface for writing
+	object_tracking_if_ =
+	  blackboard->open_for_writing<ObjectTrackingInterface>(object_tracking_if_name_.c_str());
+	object_tracking_if_->set_current_object_type(object_tracking_if_->DEFAULT_TYPE);
+	object_tracking_if_->set_current_expected_mps(object_tracking_if_->DEFAULT_MPS);
+	object_tracking_if_->set_current_expected_side(object_tracking_if_->DEFAULT_SIDE);
+
+	object_tracking_if_->write();
 
 	//read cofig values for computing expected position and target frames
 	puck_size_   = double(config->get_float("plugins/object_tracking/puck_values/puck_size"));
@@ -98,6 +112,7 @@ ObjectTrackingThread::init()
 	  double(config->get_float("plugins/object_tracking/target_frame_offsets/gripper_offset"));
 	base_offset_ =
 	  double(config->get_float("plugins/object_tracking/target_frame_offsets/base_offset"));
+
 }
 
 //updating Puck positions:
@@ -113,14 +128,6 @@ ObjectTrackingThread::on_localization_msg(ConstPosePtr &msg)
 				puck_positions_[i][0] = msg->position().x();
 				puck_positions_[i][1] = msg->position().y();
 				puck_positions_[i][2] = msg->position().z();
-				//logger->log_info(std::string(msg->name()).c_str(),
-				//                 "updated position");
-				//logger->log_info(std::string(msg->name()).c_str(),
-				//                 std::to_string(positions_[i][0]).c_str());
-				//logger->log_info(std::string(msg->name()).c_str(),
-				//                 std::to_string(positions_[i][1]).c_str());
-				//logger->log_info(std::string(msg->name()).c_str(),
-				//                 std::to_string(positions_[i][2]).c_str());
 				return;
 			}
 			//else start tracking...
@@ -154,16 +161,6 @@ ObjectTrackingThread::on_factory_msg(ConstFactoryPtr &msg)
 				//converting wierd orientation value into Gazebo yaw value
 				mps_positions_[i][2] = 2 * std::asin(msg->pose().orientation().z());
 				logger->log_info(std::string(msg->clone_model_name()).c_str(), "updated position");
-				//testing:
-				if (!std::string(msg->clone_model_name()).compare(mps_names_[10])) {
-					start_tracking(false, "C-CS1", "SHELF_RIGHT");
-				}
-				//logger->log_info(std::string(msg->clone_model_name()).c_str(),
-				//                 std::to_string(mps_positions_[i][0]).c_str());
-				//logger->log_info(std::string(msg->clone_model_name()).c_str(),
-				//                 std::to_string(mps_positions_[i][1]).c_str());
-				//logger->log_info(std::string(msg->clone_model_name()).c_str(),
-				//                 std::to_string(mps_positions_[i][2]).c_str());
 				return;
 			}
 		}
@@ -171,17 +168,62 @@ ObjectTrackingThread::on_factory_msg(ConstFactoryPtr &msg)
 	}
 }
 
-//start tracking:
-//  grab: true (track WP), false (track conveyor belt or slide)
-//  mps_name: e.g. M-CS2
-//  mps_side: input, output, slide, shelf_left, shelf_middle, shelf_right
 void
-ObjectTrackingThread::start_tracking(bool grab, std::string mps_name, std::string mps_side)
+ObjectTrackingThread::loop()
 {
-	bool mps_found = false;
-	grab_          = grab;
+	//handle incomming messeges
+	while (!object_tracking_if_->msgq_empty()) {
+		if (object_tracking_if_->msgq_first_is<ObjectTrackingInterface::StartTrackingMessage>()) {
+			logger->log_info(name(), "Received StartTrackingMessage");
+
+			ObjectTrackingInterface::StartTrackingMessage *msg =
+			  object_tracking_if_->msgq_first<ObjectTrackingInterface::StartTrackingMessage>();
+
+			start_tracking(*msg);
+		} else if (object_tracking_if_->msgq_first_is<ObjectTrackingInterface::StopTrackingMessage>()) {
+			logger->log_info(name(), "Received StopTrackingMessage");
+
+			tracking_ = false;
+			object_tracking_if_->set_current_object_type(ObjectTrackingInterface::DEFAULT_TYPE);
+			object_tracking_if_->set_current_expected_mps(ObjectTrackingInterface::DEFAULT_MPS);
+			object_tracking_if_->set_current_expected_side(ObjectTrackingInterface::DEFAULT_SIDE);
+
+			object_tracking_if_->write();
+		} else {
+			logger->log_warn(name(), "Unknown message received");
+		}
+		object_tracking_if_->msgq_pop();
+	}
+
+	//while tracking, update target frames frequently
+	if (tracking_) {
+		fawkes::Time now(clock);
+		if (now - &last_sent_time_ > (1.0 / 10.0)) {
+			last_sent_time_.set_clock(clock);
+			last_sent_time_.stamp();
+			send_target_frames();
+		}
+	}
+}
+
+void
+ObjectTrackingThread::start_tracking(ObjectTrackingInterface::StartTrackingMessage &msg)
+{
+	current_object_type_   = msg.object_type_to_set();
+	current_expected_mps_  = msg.expected_mps_to_set();
+	current_expected_side_ = msg.expected_side_to_set();
+	logger->log_info(name(), "Start Tracking");
+
+	if (current_object_type_ == ObjectTrackingInterface::DEFAULT_TYPE
+	    || current_expected_mps_ == ObjectTrackingInterface::DEFAULT_MPS
+	    || current_expected_side_ == ObjectTrackingInterface::DEFAULT_SIDE) {
+		logger->log_warn(name(), "Default value was send");
+		return;
+	}
 
 	//find corresponding MPS
+	std::string mps_name = object_tracking_if_->enum_tostring("EXPECTED_MPS", current_expected_mps_);
+	mps_name             = mps_name.replace(1, 1, "-");
 	for (int i = 0; i < 14; i++) {
 		//find index and get MPS reference position
 		if (!mps_name.compare(mps_names_[i])) {
@@ -201,34 +243,35 @@ ObjectTrackingThread::start_tracking(bool grab, std::string mps_name, std::strin
 	}
 
 	//find expected object position as middle point
-	if (!mps_side.compare("INPUT_CONVEYOR")) {
+	if (current_expected_side_ == ObjectTrackingInterface::INPUT_CONVEYOR) {
 		middle_x_ = compute_middle_x(belt_offset_side_);
 		middle_y_ = compute_middle_y(belt_offset_side_);
 		middle_z_ = belt_height_ + puck_height_ / 2;
-	} else if (!mps_side.compare("OUTPUT_CONVEYOR")) {
+	} else if (current_expected_side_ == ObjectTrackingInterface::OUTPUT_CONVEYOR) {
 		middle_x_ =
 		  mps_x_ + belt_offset_side_ * cos(mps_ori_) + (belt_lenght_ / 2 - puck_size_) * sin(mps_ori_);
 		middle_y_ =
 		  mps_y_ + belt_offset_side_ * sin(mps_ori_) - (belt_lenght_ / 2 - puck_size_) * cos(mps_ori_);
 		middle_z_ = belt_height_ + puck_height_ / 2;
-	} else if (!mps_side.compare("SLIDE")) {
+	} else if (current_expected_side_ == ObjectTrackingInterface::SLIDE) {
 		middle_x_ = compute_middle_x(slide_offset_side_);
 		middle_y_ = compute_middle_y(slide_offset_side_);
 		middle_z_ = slide_height_ + puck_height_ / 2;
-	} else if (!mps_side.compare("SHELF_LEFT")) {
+	} else if (current_expected_side_ == ObjectTrackingInterface::SHELF_LEFT) {
 		middle_x_ = compute_middle_x(left_shelf_offset_side_);
 		middle_y_ = compute_middle_y(left_shelf_offset_side_);
 		middle_z_ = shelf_height_ + puck_height_ / 2;
-	} else if (!mps_side.compare("SHELF_MIDDLE")) {
+	} else if (current_expected_side_ == ObjectTrackingInterface::SHELF_MIDDLE) {
 		middle_x_ = compute_middle_x(middle_shelf_offset_side_);
 		middle_y_ = compute_middle_y(middle_shelf_offset_side_);
 		middle_z_ = shelf_height_ + puck_height_ / 2;
-	} else if (!mps_side.compare("SHELF_RIGHT")) {
+	} else if (current_expected_side_ == ObjectTrackingInterface::SHELF_RIGHT) {
 		middle_x_ = compute_middle_x(right_shelf_offset_side_);
 		middle_y_ = compute_middle_y(right_shelf_offset_side_);
 		middle_z_ = shelf_height_ + puck_height_ / 2;
 	} else {
-		logger->log_error(mps_side.c_str(), " is an invalid MPS-side!");
+		logger->log_error(object_tracking_if_->enum_tostring("EXPECTED_SIDE", current_expected_side_),
+		                  " is an invalid MPS-side!");
 		return;
 	}
 
@@ -237,8 +280,8 @@ ObjectTrackingThread::start_tracking(bool grab, std::string mps_name, std::strin
 	logger->log_info("middle_y: ", std::to_string(middle_y_).c_str());
 	logger->log_info("middle_z: ", std::to_string(middle_z_).c_str());
 
-	//if grab == true, find nearest workpiece (puck) to the middle point
-	if (grab_) {
+	//if workpiece is tracked, find nearest workpiece (puck) to the middle point
+	if (current_object_type_ == ObjectTrackingInterface::WORKPIECE) {
 		double min_dist = 999;
 		for (int i = 0; i < tracked_pucks_; i++) {
 			if (distance_middle_to_puck(i) < min_dist) {
@@ -246,6 +289,7 @@ ObjectTrackingThread::start_tracking(bool grab, std::string mps_name, std::strin
 				closest_puck_ind_ = i;
 			}
 		}
+
 		//update middle position to closest WP middle point
 		middle_x_ = puck_positions_[closest_puck_ind_][0];
 		middle_y_ = puck_positions_[closest_puck_ind_][1];
@@ -256,7 +300,17 @@ ObjectTrackingThread::start_tracking(bool grab, std::string mps_name, std::strin
 		logger->log_info("middle_y: ", std::to_string(middle_y_).c_str());
 		logger->log_info("middle_z: ", std::to_string(middle_z_).c_str());
 	}
+
+	//send target frames and start tracking
 	tracking_ = true;
+	msgid_    = 0;
+	object_tracking_if_->set_current_object_type(current_object_type_);
+	object_tracking_if_->set_current_expected_mps(current_expected_mps_);
+	object_tracking_if_->set_current_expected_side(current_expected_side_);
+
+	send_target_frames();
+	last_sent_time_.set_clock(clock);
+	last_sent_time_.stamp();
 }
 
 double
@@ -277,4 +331,36 @@ ObjectTrackingThread::distance_middle_to_puck(int puck_ind)
 	return sqrt(pow(middle_x_ - puck_positions_[puck_ind][0], 2)
 	            + pow(middle_y_ - puck_positions_[puck_ind][1], 2)
 	            + pow(middle_z_ - puck_positions_[puck_ind][2], 2));
+}
+
+void
+ObjectTrackingThread::send_target_frames()
+{
+	if (current_object_type_ == ObjectTrackingInterface::WORKPIECE) {
+		//update middle position to closest WP middle point
+		middle_x_ = puck_positions_[closest_puck_ind_][0];
+		middle_y_ = puck_positions_[closest_puck_ind_][1];
+		middle_z_ = puck_positions_[closest_puck_ind_][2];
+	}
+
+	object_tracking_if_->set_gripper_frame(0, middle_x_);
+	object_tracking_if_->set_gripper_frame(1, middle_y_);
+	object_tracking_if_->set_gripper_frame(2, middle_z_ + gripper_offset_);
+
+	if (current_expected_side_ == ObjectTrackingInterface::OUTPUT_CONVEYOR) {
+		object_tracking_if_->set_base_frame(0, middle_x_ - base_offset_ * sin(mps_ori_ + M_PI));
+		object_tracking_if_->set_base_frame(1, middle_y_ + base_offset_ * cos(mps_ori_ + M_PI));
+		//robotino base is created with ori = 1.57
+		object_tracking_if_->set_base_frame(5, mps_ori_ + 1.57);
+	} else {
+		object_tracking_if_->set_base_frame(0, middle_x_ - base_offset_ * sin(mps_ori_));
+		object_tracking_if_->set_base_frame(1, middle_y_ + base_offset_ * cos(mps_ori_));
+		//robotino base is created with ori = 1.57
+		object_tracking_if_->set_base_frame(5, mps_ori_ + M_PI + 1.57);
+	}
+
+	msgid_++;
+	object_tracking_if_->set_msgid(msgid_);
+	object_tracking_if_->set_detected(true);
+	object_tracking_if_->write();
 }
