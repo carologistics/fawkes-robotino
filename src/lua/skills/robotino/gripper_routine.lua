4b99defa2dd2cd625bde34e32728cb46d75e5e23
Matteo Tschesche <matteo.tschesche@rwth-aachen.de>
2022-04-05 21:27:17 +0200

    visual_servoing: start gripper routine from manipulate_wp skill
    Adjust configs in manipulate_wp to work with routine.
    Change gripper commands from MOVEREL to MOVEABS in gripper routine and clip
    them to their limits.

--- src/lua/skills/robotino/gripper_routine.lua
+++ src/lua/skills/robotino/gripper_routine.lua
@@ -25,7 +25,10 @@ module(..., skillenv.module_init)
 name               = "gripper_routine"
 fsm                = SkillHSM:new{name=name, start="INIT", debug=true}
 depends_skills     = {"gripper_commands", "motor_move"}
-depends_interfaces = {}
+depends_interfaces = {
+   {v = "object_tracking_if", type = "ObjectTrackingInterface", id="object-tracking"},
+   {v = "arduino", type = "ArduinoInterface", id="Arduino"},
+}
 
 documentation      = [==[
 Skill to pick a product and to put it down based on param pick_wp.
@@ -47,12 +50,26 @@ local gripper_down_z_put = -0.028  -- distance to move gripper down after drivin
 local gripper_up_z_pick = 0.01   -- distance to move gripper up after closing gripper
 local gripper_up_z_put = 0.045   -- distance to move gripper up after opening gripper
 
-local drive_back_x = -0.2
+local drive_back_x = -0.1
 
 local gripper_default_pose_x = 0.00   -- conveyor pose offset in x direction
 local gripper_default_pose_y = 0.00   -- conveyor_pose offset in y direction
 local gripper_default_pose_z = 0.056  -- conveyor_pose offset in z direction
 
+local x_max = 0.115  -- gripper max value in x direction
+local y_max = 0.075  -- gripper max value in y direction
+local z_max = 0.057  -- gripper max value in z direction
+
+-- read gripper config
+if config:exists("/arduino/x_max") then
+  x_max = config:get_float("/arduino/x_max")
+end
+if config:exists("/arduino/y_max") then
+  y_max = config:get_float("/arduino/y_max")
+end
+if config:exists("/arduino/max_z") then
+  z_max = config:get_float("/arduino/z_max")
+end
 
 fsm:define_states{ export_to=_M, closure={},
    {"INIT",              JumpState},
@@ -77,14 +94,31 @@ fsm:add_transitions{
 }
 
 function MOVE_GRIPPER_DOWN:init()
+  local gripper_target = tfm.transform6D(
+    {x=object_tracking_if:gripper_frame(0),
+     y=object_tracking_if:gripper_frame(1),
+     z=object_tracking_if:gripper_frame(2),
+     ori=fawkes.tf.create_quaternion_from_yaw(0)},
+    "base_link", "end_effector_home")
+
+  -- Clip to axis limits
+  local x_clipped = math.max(0, math.min(gripper_target.x, x_max))
+  local y_clipped = math.max(-y_max/2, math.min(gripper_target.y, y_max/2))
+
+  local z_given = 0
   if self.fsm.vars.pick_wp then
-    self.args["gripper_commands"].z = gripper_down_z_pick
+    z_given = gripper_target.z + gripper_down_z_pick
   else
-    self.args["gripper_commands"].z = gripper_down_z_put
+    z_given = gripper_target.z + gripper_down_z_put
   end
-  self.args["gripper_commands"].x = 0
-  self.args["gripper_commands"].y = 0
-  self.args["gripper_commands"].command = "MOVEREL"
+  local z_clipped = math.max(0, math.min(z_given, z_max))
+
+  self.args["gripper_commands"].x = x_clipped
+  self.args["gripper_commands"].y = y_clipped
+  self.args["gripper_commands"].z = z_clipped 
+  self.args["gripper_commands"].command = "MOVEABS"
+
+  fsm.vars.target_z = z_clipped
 end
 
 function CLOSE_GRIPPER:init()
@@ -96,14 +130,17 @@ function OPEN_GRIPPER:init()
 end
 
 function MOVE_GRIPPER_UP:init()
+  local z_given = 0
   if self.fsm.vars.pick_wp then
-    self.args["gripper_commands"].z = gripper_up_z_pick
+    z_given = fsm.vars.target_z + gripper_up_z_pick
   else
-    self.args["gripper_commands"].z = gripper_up_z_put
+    z_given = fsm.vars.target_z + gripper_up_z_put
   end
-  self.args["gripper_commands"].x = 0
-  self.args["gripper_commands"].y = 0
-  self.args["gripper_commands"].command = "MOVEREL"
+
+  self.args["gripper_commands"].x = arduino:x_position()
+  self.args["gripper_commands"].y = arduino:y_position() - y_max/2
+  self.args["gripper_commands"].z = math.max(0, math.min(z_given, z_max))
+  self.args["gripper_commands"].command = "MOVEABS"
 end
 
 function GRIPPER_DEFAULT:init()
