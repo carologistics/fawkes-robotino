2bc3f6acfd6948a13014731a33a40c17952ecf92
Nicolas Limpert <limpert@fh-aachen.de>
2022-05-05 16:50:34 +0200

    skills/gripper_routine: increase z distance during put

--- src/lua/skills/robotino/gripper_routine.lua
+++ src/lua/skills/robotino/gripper_routine.lua
@@ -44,13 +44,13 @@ skillenv.skill_module(_M)
 local tfm = require("fawkes.tfutils")
 
 -- Constant
-local gripper_down_z_pick = -0.05  -- distance to move gripper down after driving over product TODO: get through object tracking yaml
-local gripper_down_z_put = -0.028  -- distance to move gripper down after driving over product
+local gripper_down_z_pick = -0.02  -- distance to move gripper down after driving over product TODO: get through object tracking yaml
+local gripper_down_z_put = -0.023  -- distance to move gripper down after driving over product
 
 local gripper_up_z_pick = 0.01   -- distance to move gripper up after closing gripper
-local gripper_up_z_put = 0.045   -- distance to move gripper up after opening gripper
+local gripper_up_z_put = 0.015   -- distance to move gripper up after opening gripper
 
-local drive_back_x = -0.1
+local drive_back_x = -0.3
 
 local gripper_default_pose_x = 0.00   -- conveyor pose offset in x direction
 local gripper_default_pose_y = 0.00   -- conveyor_pose offset in y direction
@@ -113,9 +113,13 @@ function MOVE_GRIPPER_DOWN:init()
   end
   local z_clipped = math.max(0, math.min(z_given, z_max))
 
-  self.args["gripper_commands"].x = x_clipped
+  self.args["gripper_commands"].x = x_clipped + 0.005
   self.args["gripper_commands"].y = y_clipped
-  self.args["gripper_commands"].z = z_clipped 
+  if self.fsm.vars.pick_wp then
+    self.args["gripper_commands"].z = z_clipped 
+  else
+    self.args["gripper_commands"].z = z_clipped + 0.01
+  end
   self.args["gripper_commands"].command = "MOVEABS"
 
   fsm.vars.target_z = z_clipped
