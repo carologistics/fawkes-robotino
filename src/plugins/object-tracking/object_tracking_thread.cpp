0f79eba1092e1c30c9d5292ba442b7297263d8b2
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2022-10-12 12:10:02 +0200

    object-tracking: initialize box_id

--- src/plugins/object-tracking/object_tracking_thread.cpp
+++ src/plugins/object-tracking/object_tracking_thread.cpp
@@ -729,11 +729,11 @@ ObjectTrackingThread::closest_position(std::vector<std::array<float, 4>>      bo
                                        fawkes::tf::Stamped<fawkes::tf::Point> ref_pos,
                                        float                                  mps_angle,
                                        float                                  closest_pos[3],
-                                       Rect &                                 closest_box,
-                                       float &                                additional_height)
+                                       Rect                                  &closest_box,
+                                       float                                 &additional_height)
 {
 	float  min_dist = max_acceptable_dist_;
-	size_t box_id;
+	size_t box_id   = 0;
 
 	for (size_t i = 0; i < bounding_boxes.size(); ++i) {
 		float pos[3];
@@ -774,7 +774,7 @@ void
 ObjectTrackingThread::compute_3d_point(std::array<float, 4> bounding_box,
                                        float                mps_angle,
                                        float                point[3],
-                                       float &              wp_additional_height)
+                                       float               &wp_additional_height)
 {
 	//compute bounding box values
 	float bb_left    = bounding_box[0] - bounding_box[2] / 2;
