1f878258476c7df474e793665abfafaf7b1de34b
Falk Boehm <falk.boehm@rwth-aacehn.de>
2022-11-24 12:01:13 +0100

    log_fliter: add uniqueness to other filters

--- logs/log_filter.sh
+++ logs/log_filter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Generating filtered log-files..."
-grep "action-name visit" debug11_latest.log > debug11_visit.log
+grep "action-name visit" debug11_latest.log | sed 's/^.* (plan-action //g' | sed 's/(start-time.*$//g' | sort -u > debug11_visit.log
 grep "(state " debug11_latest.log > debug11_states.log
-grep "(state RUNNING)" debug11_states.log > debug11_running.log
+grep "(state RUNNING)" debug11_states.log | sed 's/^.* (plan-action //g' | sed 's/(start-time.*$//g' | sort -u > debug11_running.log
 grep "(state FINAL)" debug11_visit.log | sed 's/^.* (plan-action //g' | sed 's/(start-time.*$//g' | sort -u > debug11_final.log
 echo "Finished generating files!"
