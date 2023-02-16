f36ce9115fd65827062174079cb57b3df4bd860a
Falk Boehm <falk.boehm@rwth-aacehn.de>
2023-02-16 17:21:05 +0100

    logs: updated log_filter

--- logs/log_filter.sh
+++ logs/log_filter.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+function log_to_facts() {
+        if [ -z ${2+x} ]; then
+                sort -k 6 $1  | uniq --unique -c -f 6 | sed '/==>/!d' | sort -n
+        else
+            	head -n $2 $1 | sort -k 6 | uniq --unique -c -f 6 | sed '/==>/!d'
+        fi
+}
+
 echo "Generating filtered log-files..."
-grep "action-name visit" debug11_latest.log | sed 's/^.* (plan-action //g' | sed 's/(start-time.*$//g' | sort -u > debug11_visit.log
+grep -e "GOAL-.*mode" debug11_latest.log | sed 's/^.*goal (id //g' | sort -u > debug11_goals.log
 grep "(state " debug11_latest.log > debug11_states.log
-grep "(state RUNNING)" debug11_states.log | sed 's/^.* (plan-action //g' | sed 's/(start-time.*$//g' | sort -u > debug11_running.log
-grep "(state FINAL)" debug11_visit.log | sed 's/^.* (plan-action //g' | sed 's/(start-time.*$//g' | sort -u > debug11_final.log
+log_to_facts debug11_latest.log > debug11_facts.log
 echo "Finished generating files!"
