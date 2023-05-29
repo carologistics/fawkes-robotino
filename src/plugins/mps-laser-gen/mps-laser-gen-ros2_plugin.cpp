ec9452bde07cd7dda8625bec7818ecb7213ac416
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2023-05-29 11:17:33 +0200

    mps-laser-gen: add corresponding ros2 plugin

---
+++ src/plugins/mps-laser-gen/mps-laser-gen-ros2_plugin.cpp
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *  mps-laser-gen-ros2_plugin.cpp - mps-laser-gen
+ *
+ *  Plugin created: Thu Jun 30 21:54:46 2016
+ *  Copyright  2016  Tim Niemueller
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include "mps-laser-gen_thread.h"
+
+#include <core/plugin.h>
+
+using namespace fawkes;
+
+/** Plugin to generate virtual laser data based on known MPS positions.
+ * @author Tim Niemueller
+ */
+class MPSLaserGenROS2Plugin : public fawkes::Plugin
+{
+public:
+	/** Constructor
+   * @param config Fakwes configuration
+   */
+	MPSLaserGenROS2Plugin(Configuration *config) : Plugin(config)
+	{
+		thread_list.push_back(new MPSLaserGenThread());
+	}
+};
+
+PLUGIN_DESCRIPTION("Generate virtual laser data for known MPS for ROS 2")
+EXPORT_PLUGIN(MPSLaserGenROS2Plugin)
