fff9a49f85b57b2086a51ce1e5d381421a7ddc0a
Tim Wendt <tim.wendt@alumni.fh-aachen.de>
2023-06-07 00:00:54 +0200

    arduino: first working state of serialport

--- src/plugins/arduino/serialport.cpp
+++ src/plugins/arduino/serialport.cpp
@@ -20,8 +20,9 @@
 
 #include "serialport.h"
 
-#include <boost/thread.hpp>
 #include <boost/bind.hpp>
+#include <boost/thread.hpp>
+#include <cstddef>
 #include <cstring>
 #include <memory>
 #include <mutex>
@@ -41,9 +42,8 @@ SerialPort::SerialPort(const char                                *port,
 	mutex_ = port_mutex;
 	std::lock_guard<std::mutex> lock(*mutex_);
 
-	start_of_command_ = (char*)start_of_command;
-	end_of_command_   = (char*)end_of_command;
-
+	start_of_command_ = (char *)start_of_command;
+	end_of_command_   = (char *)end_of_command;
 
 	boost::system::error_code ec;
 
@@ -65,90 +65,91 @@ SerialPort::SerialPort(const char                                *port,
 	async_read_some_();
 }
 
-void SerialPort::async_read_some_()
+void
+SerialPort::async_read_some_()
 {
-	if (port_.get() == NULL || !port_->is_open()) return;
+	if (port_.get() == NULL || !port_->is_open())
+		return;
 
-	port_->async_read_some(
-		boost::asio::buffer(read_buf_raw_, SERIAL_PORT_READ_BUF_SIZE),
-		boost::bind(
-			&SerialPort::on_receive_,
-			this, boost::asio::placeholders::error,
-			boost::asio::placeholders::bytes_transferred));
+	port_->async_read_some(boost::asio::buffer(read_buf_raw_, SERIAL_PORT_READ_BUF_SIZE),
+	                       boost::bind(&SerialPort::on_receive_,
+	                                   this,
+	                                   boost::asio::placeholders::error,
+	                                   boost::asio::placeholders::bytes_transferred));
 }
 
-void SerialPort::on_receive_(const boost::system::error_code& ec, size_t bytes_transferred)
+bool
+start_with_pattern(const char *pattern, const char buf[], size_t bytes_transferred, size_t offset)
+{
+	for (int i = 0; i < strlen(pattern); ++i) {
+		char c = buf[i + offset];
+		if (c != pattern[i])
+			return false;
+	}
+	return true;
+}
+
+int
+SerialPort::send(const std::string &buf)
+{
+	return write_some(buf.c_str(), buf.size());
+}
+
+int
+SerialPort::send(const char *buf, const int &size)
+{
+	boost::system::error_code ec;
+
+	if (!port_)
+		return -1;
+	if (size == 0)
+		return 0;
+
+	return port_->write_some(boost::asio::buffer(buf, size), ec);
+}
+
+void
+SerialPort::on_receive_(const boost::system::error_code &ec, size_t bytes_transferred)
 {
 	boost::mutex::scoped_lock look(*mutex_);
 
-	if (port_.get() == NULL) return;
+	if (port_.get() == NULL)
+		return;
 
-	if(!port_) this->~SerialPort(); //If port is closed then just dissolve this instance
+	if (!port_)
+		this->~SerialPort(); //If port is closed then just dissolve this instance
 	if (ec) {
 		async_read_some_();
 		return;
 	}
 
-	int starts_at = -1;
-	int ends_at = -1;
+	bool has_started = false;
 	for (unsigned int i = 0; i < bytes_transferred; ++i) {
-		bool is_starting = true;
-		for(unsigned int j = 0; j < std::strlen(start_of_command_) && j + i < bytes_transferred; j++){
-			char c = read_buf_raw_[i + j];
-			if(c != start_of_command_[j]) {
-				is_starting = false;
-				break;
-			}
-		}
-		read_buf_raw_
-		if(is_starting){
-			starts_at = i;
-			break;
-		}
-	}
-	if(starts_at == -1) {
-		return;
-	}
+		bool is_starting = start_with_pattern(start_of_command_, read_buf_raw_, bytes_transferred, i);
+		bool is_ending   = start_with_pattern(start_of_command_, read_buf_raw_, bytes_transferred, i);
 
-	for (unsigned int i = starts_at + std::strlen(start_of_command_); i < bytes_transferred; ++i) {
-		bool is_ending = true;
-		for(unsigned int j = 0; j < std::strlen(end_of_command_) && j + i < bytes_transferred; j++){
-			char c = read_buf_raw_[i + j];
-			if(c != end_of_command_[i]){
-				is_ending = false;
-				break;
-			}
+		if (has_started || is_starting) {
+			has_started = true;
+			read_buf_str_ += read_buf_raw_[i];
 		}
-		if(is_ending) {
-			ends_at = i;
-			break;
-		}
-	}
-	if(starts_at == -1) {
-		return;
-	}
 
-	read_buf_str_ = std::string(read_buf_raw_);
-			this->on_receive_(read_buf_str_);
+		if (has_started && is_ending) {
+			has_started = false;
+			for (int j = 1; j < strlen(end_of_command_); ++j) {
+				read_buf_str_ += read_buf_raw_[i + j];
+			}
+			on_receive_(read_buf_str_);
 			read_buf_str_.clear();
 		}
-    else
-    {
-	    read_buf_str_ += c;
-    }
-  }
+	}
 
 	async_read_some_();
-  }
-
-
-void SerialPort::on_receive_(const std::string &data)
-{
-	//TODO
 }
 
-
-SerialPort::~SerialPort() {
+SerialPort::~SerialPort()
+{
+	port_->cancel();
 	port_->close();
+	port_.reset();
 	mutex_->unlock();
 }
--- src/plugins/arduino/serialport.h
+++ src/plugins/arduino/serialport.h
@@ -31,32 +31,35 @@ typedef boost::shared_ptr<boost::asio::serial_port> serial_port_ptr;
 
 #define SERIAL_PORT_READ_BUF_SIZE 256
 
-class SerialPort {
+class SerialPort
+{
 protected:
-	boost::asio::io_service io_service_;
-	serial_port_ptr port_;
+	boost::asio::io_service     io_service_;
+	serial_port_ptr             port_;
 	std::shared_ptr<std::mutex> mutex_;
 
 	char        read_buf_raw_[SERIAL_PORT_READ_BUF_SIZE];
 	std::string read_buf_str_;
 
-    char* start_of_command_;
-    char* end_of_command_;
+	char *start_of_command_;
+	char *end_of_command_;
 
-    boost::function<void(const std::string &)> receive_callback_;
+	boost::function<void(const std::string &)> receive_callback_;
 
-	public:
-	  SerialPort(const char                                *port,
-	             boost::function<void(const std::string &)> receive_callback,
-	             std::shared_ptr<std::mutex>                port_mutex,
-	             unsigned int                               baud_rate        = 115200,
-	             const char                                *start_of_command = "AT ",
-	             const char                                *end_of_command   = "+");
-	  ~SerialPort();
+public:
+	SerialPort(const char                                *port,
+	           boost::function<void(const std::string &)> receive_callback,
+	           std::shared_ptr<std::mutex>                port_mutex,
+	           unsigned int                               baud_rate        = 115200,
+	           const char                                *start_of_command = "AT ",
+	           const char                                *end_of_command   = "+");
+	~SerialPort();
+	int write_some(const std::string &buf);
+	int write_some(const char *buf, const int &size);
 
-	private:
-    void async_read_some_();
-	virtual void on_receive_(const boost::system::error_code& ec, size_t bytes_transferred);
+private:
+	void         async_read_some_();
+	virtual void on_receive_(const boost::system::error_code &ec, size_t bytes_transferred);
 	virtual void on_receive_(const std::string &data);
 };
 
