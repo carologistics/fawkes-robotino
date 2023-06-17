94b5f95aa752e97aabc7ceea0d49f3f7f8da62cb
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2023-06-17 12:07:49 +0200

    scripts: replace cx-simtest-check bash script by python script

--- etc/scripts/cx-simtest-check.py
+++ etc/scripts/cx-simtest-check.py
@@ -1,27 +1,25 @@
-# *****************************************************************************
-# CMake Build System for Fawkes
-# -------------------
-# Copyright (C) 2023 by Tim Wendt
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+##########################################################################
 #
-# *****************************************************************************
+#  cx-symtest-check.py Parse log files for simtest output
 #
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
+#  Copyright © 2023 Tim Wendt
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
+##########################################################################
 #
-# You should have received a copy of the GNU General Public License along with
-# this program.  If not, see <http://www.gnu.org/licenses/>.
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
 #
-# *****************************************************************************
-
-
-#!/bin/python3
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Library General Public License for more details.
+#
+#  Read the full text in the LICENSE.GPL file in the doc directory.
 
 import sys
 import subprocess
