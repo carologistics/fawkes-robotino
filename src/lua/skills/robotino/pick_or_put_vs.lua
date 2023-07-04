2d7f5d032f422be3e2d90e83331db401451d96a4
Matteo Tschesche <matteo.tschesche@rwth-aachen.de>
2023-07-04 14:33:57 +0200

    cfg: adjust to common current

--- src/lua/skills/robotino/pick_or_put_vs.lua
+++ src/lua/skills/robotino/pick_or_put_vs.lua
@@ -47,7 +47,7 @@ local tfm = require("fawkes.tfutils")
 
 -- Constant
 local gripper_down_z_pick = -0.05  -- distance to move gripper down after driving over product
-local gripper_down_z_put = -0.025  -- distance to move gripper down after driving over product
+local gripper_down_z_put = -0.033  -- distance to move gripper down after driving over product
 
 local gripper_up_z_pick = 0.0125   -- distance to move gripper up after closing gripper
 local gripper_up_z_put_slide = 0.015   -- distance to move gripper up after slide put
