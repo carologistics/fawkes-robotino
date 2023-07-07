9a35110c69d9f6095c52fbb08f1666a09663d138
Matteo Tschesche <matteo.tschesche@rwth-aachen.de>
2023-07-07 12:38:21 +0200

    vs: align more straight

--- src/lua/skills/robotino/motor_move.lua
+++ src/lua/skills/robotino/motor_move.lua
@@ -67,7 +67,7 @@ local V_MIN =         { x=0.006, y=0.006, ori=0.02 }   -- below the motor won't 
 local TOLERANCE =     { x=0.02, y=0.02, ori=0.01 } -- accuracy
 local TOLERANCE_VS =  { x=0.01, y=0.02, ori=0.01 }
 local TOL_ORI_START   = 0.1
-local TOLERANCE_EE =  { x=0.15, y=0.04, ori=0.03} -- tolerance for end_early condition
+local TOLERANCE_EE =  { x=0.15, y=0.04, ori=0.01} -- tolerance for end_early condition
 local TOLERANCE_CAM = { x=0.005, y=0.0015, ori=0.01 }
 local D_DECEL =       { x=0.035, y=0.035, ori=0.15 }    -- deceleration distance
 local ACCEL =         { x=0.06, y=0.06, ori=0.21 }   -- accelerate by this factor every loop
