08bd9674042c0497d3d851fa2956e85d02206a6e
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2023-07-07 12:23:29 +0200

    motor_move: adapt tolerances to EE tolerances

--- src/lua/skills/robotino/motor_move.lua
+++ src/lua/skills/robotino/motor_move.lua
@@ -64,7 +64,7 @@ documentation      = [==[Move on a (kind of) straight line to the given coordina
 local V_MAX =         { x=0.35, y=0.35, ori=1.4 }    -- ultimate limit
 local V_MAX_CAM =     { x=0.06, y=0.06, ori=0.3 }
 local V_MIN =         { x=0.006, y=0.006, ori=0.02 }   -- below the motor won't even start
-local TOLERANCE =     { x=0.02, y=0.02, ori=0.025 } -- accuracy
+local TOLERANCE =     { x=0.15, y=0.02, ori=0.01 } -- accuracy
 local TOLERANCE_VS =  { x=0.01, y=0.005, ori=0.01 }
 local TOLERANCE_EE =  { x=0.15, y=0.04, ori=0.03} -- tolerance for end_early condition
 local TOLERANCE_CAM = { x=0.005, y=0.0015, ori=0.01 }
