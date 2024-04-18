b2ee6cb20ce46ae2cbf10d723dba654e310d97a5
Lucas Johannsen <Lucasjohannsen@gmail.com>
2024-04-18 11:06:38 +0200

    yolov8n:  adding a better model

--- src/plugins/object-tracking/object_tracking_thread.cpp
+++ src/plugins/object-tracking/object_tracking_thread.cpp
@@ -157,7 +157,7 @@ ObjectTrackingThread::init()
 	swapRB_ = true;
 
 	//set up network
-	net_ = readNet(weights_path_, config_path_);
+	net_ = readNet(weights_path_);
 	net_.setPreferableBackend(DNN_BACKEND_DEFAULT);
 	net_.setPreferableTarget(DNN_TARGET_CPU);
 	//get name of output layer
