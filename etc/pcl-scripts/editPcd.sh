f190e7997363d7ea854327a5b5b0d709e098fafd
Tim Wendt <tim.wendt@alumni.fh-aachen.de>
2024-06-01 10:47:35 +0200

    format: added licenseheader check to pre-commit

--- etc/pcl-scripts/editPcd.sh
+++ etc/pcl-scripts/editPcd.sh
@@ -1,4 +1,15 @@
 #! /usr/bin/bash
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+
 # While recording a model for later conveyor_pose_recognition, sometimes
 # small mistakes in rotation or translation happen.
 # Further, the origin position of different models must be exactly
