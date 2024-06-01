c1b3784b844399be1e41660ed9d5ad400a6f6fdf
Tim Wendt <tim.wendt@alumni.fh-aachen.de>
2024-06-01 10:47:35 +0200

    format: added licenseheader check to pre-commit

--- etc/scripts/eval_suite/experiment.sh
+++ etc/scripts/eval_suite/experiment.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# Read the full text in the LICENSE.GPL file in the doc directory.
 # Automated experiment and evaluation on fawkes-robotino setups
 # Generates a game layout and runs n experiments of an experimental
 # setup and optionally a baseline. The resulting data is parsed and
--- etc/scripts/flash_arduino.sh
+++ etc/scripts/flash_arduino.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+
+
 # Helper script for flashing the arduino
 # args: 1. path to ArduinoSketch (in Arduino plugin)
 # args: 2. path to the Arduino (i.e. /dev/arduino on the Robotinos)
