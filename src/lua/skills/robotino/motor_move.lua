34cd8a90eab8dc89da1f73673d5953ed66c743c1
Matteo Tschesche <matteo.tschesche@rwth-aachen.de>
2024-06-08 19:50:04 +0200

    motor_move: increase tolerance

--- src/lua/skills/robotino/motor_move.lua
+++ src/lua/skills/robotino/motor_move.lua
@@ -65,7 +65,7 @@ local V_MAX = {x = 0.35, y = 0.35, ori = 1.4} -- ultimate limit
 local V_MAX_CAM = {x = 0.06, y = 0.06, ori = 0.3}
 local V_MIN = {x = 0.006, y = 0.006, ori = 0.02} -- below the motor won't even start
 local TOLERANCE = {x = 0.02, y = 0.02, ori = 0.01} -- accuracy
-local TOLERANCE_VS = {x = 0.01, y = 0.02, ori = 0.01}
+local TOLERANCE_VS = {x = 0.02, y = 0.04, ori = 0.015}
 local TOL_ORI_START = 0.1
 local TOLERANCE_EE = {x = 0.15, y = 0.04, ori = 0.01} -- tolerance for end_early condition
 local TOLERANCE_CAM = {x = 0.005, y = 0.0015, ori = 0.01}
