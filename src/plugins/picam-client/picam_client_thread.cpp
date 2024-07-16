ba708e5bb2eddda40b17094ba6128e5af68cec8e
Daniel Swoboda <daniel.swoboda@ml.rwth-aachen.de>
2024-07-16 11:10:55 +0200

    picam: make initial mode configurable

--- src/plugins/picam-client/picam_client_thread.cpp
+++ src/plugins/picam-client/picam_client_thread.cpp
@@ -509,9 +509,13 @@ PicamClientThread::connect_to_server()
 	send_configure_message();
 	send_control_message(13, config->get_float("plugins/picam_client/detection/iou"));
 	send_control_message(12, config->get_float("plugins/picam_client/detection/conf"));
-	send_control_message(4);
-	send_control_message(5);
-	send_control_message(8);
+	if(config->get_bool("plugins/picam_client/detection/workpiece")) {
+		send_control_message(5);
+		send_control_message(8);
+	}
+	if(config->get_bbol("plugins/picam_client/stream")) {
+		send_control_message(4);
+	}
 
 	return 0;
 }
